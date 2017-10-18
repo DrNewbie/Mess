4f5e746f4e4843b64f26bd7b9ae67b44695077bc
Dr_Newbie <tf2baidonation@gmail.com>
2017-10-19 04:22:00 +0800

    Spawn More Enemies ; 24

--- mods/Spawn More and Faster/lua/gamesetup.lua
+++ mods/Spawn More and Faster/lua/gamesetup.lua
@@ -4,11 +4,8 @@ Hooks:PostHook(GameSetup, "init_game", "SMF_GameSetup_init_game", function(self)
 	end
 	local FakeBigParty = managers.mutators:get_mutator(MutatorBigParty)
 	if FakeBigParty then
+		FakeBigParty:register_value("insanity_level", 2, "dm")
+		FakeBigParty:set_value("insanity_level", 2)
 		FakeBigParty:setup()
-		FakeBigParty:clear_values()
-		FakeBigParty:reset_to_default()
-		FakeBigParty:set_value("insanity_level", 2.25)
-		FakeBigParty:modify_tweak_data()
-		FakeBigParty:modify_character_tweak_data()
 	end
 end)
