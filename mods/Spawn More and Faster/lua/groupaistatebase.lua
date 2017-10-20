1ec7cc17342a8b4c73b232715603c7deb00cda21
Dr_Newbie <tf2baidonation@gmail.com>
2017-10-20 13:43:27 +0800

    Move
    https://github.com/DrNewbie/BigParty/commit/3d9e744a20980b6c8e3ebf7d40a7855e8544ad69

--- mods/Spawn More and Faster/lua/groupaistatebase.lua
+++
@@ -1,8 +0,0 @@
-function GroupAIStateBase:_get_difficulty_dependent_value(tweak_values)
-	return math.lerp(tweak_values[self._difficulty_point_index], tweak_values[self._difficulty_point_index + 1], self._difficulty_ramp)
-end
-
-function GroupAIStateBase:set_difficulty()
-	self._difficulty_value = 1
-	self:_calculate_difficulty_ratio()
-end
