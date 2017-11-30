4b8b425bb0e3d2490771ced783c91d2aec02eb1a
Dr_Newbie <tf2baidonation@gmail.com>
2017-11-30 22:53:23 +0800

    .

--- mods/Someon is not at safehouse/Base.lua
+++ mods/Someon is not at safehouse/Base.lua
@@ -2,6 +2,8 @@ _G.SomeonNotHome = _G.SomeonNotHome or {}
 SomeonNotHome.ModPath = ModPath
 SomeonNotHome.SaveFile = SavePath .. "SomeonNotHome.txt"
 SomeonNotHome.Main_Options_Menu = "SomeonNotHome_Main_Options_Menu"
+SomeonNotHome.SomeonNotHome_Options_Crew = "SomeonNotHome_Options_Crew"
+SomeonNotHome.SomeonNotHome_Options_Other = "SomeonNotHome_Options_Other"
 SomeonNotHome.Settings = SomeonNotHome.Settings or {}
 SomeonNotHome.All_Character = {}
 
@@ -17,7 +19,9 @@ Hooks:Add("LocalizationManagerPostInit", "SomeonNotHome_loc", function(loc)
 	LocalizationManager:add_localized_strings({
 		["SomeonNotHome_menu_title"] = "Someon is not at safehouse",
 		["SomeonNotHome_menu_desc"] = "Who is not at safehouse now?",
-		["SomeonNotHome_menu_empty_desc"] = " "
+		["SomeonNotHome_menu_empty_desc"] = " ",
+		["SomeonNotHome_menu_Crew_title"] = "Payday Gang",
+		["SomeonNotHome_menu_Other_title"] = "Others"
 	})
 end )
 
@@ -54,6 +58,8 @@ end
 
 Hooks:Add("MenuManagerSetupCustomMenus", "SomeonNotHomeOptionsSetup", function(...)
 	MenuHelper:NewMenu(SomeonNotHome.Main_Options_Menu)
+	MenuHelper:NewMenu(SomeonNotHome.SomeonNotHome_Options_Crew)
+	MenuHelper:NewMenu(SomeonNotHome.SomeonNotHome_Options_Other)
 	for _, _name in ipairs(SomeonNotHome.All_Character) do
 		MenuHelper:NewMenu("SomeonNotHome_".. _name .."_Options_Menu")
 	end
@@ -61,7 +67,11 @@ end )
 
 Hooks:Add("MenuManagerBuildCustomMenus", "SomeonNotHomeOptionsBuild", function(menu_manager, nodes)
 	nodes[SomeonNotHome.Main_Options_Menu] = MenuHelper:BuildMenu(SomeonNotHome.Main_Options_Menu)
-	MenuHelper:AddMenuItem(nodes["blt_options"],SomeonNotHome.Main_Options_Menu, "SomeonNotHome_menu_title", "SomeonNotHome_menu_desc")
+	nodes[SomeonNotHome.SomeonNotHome_Options_Crew] = MenuHelper:BuildMenu(SomeonNotHome.SomeonNotHome_Options_Crew)
+	nodes[SomeonNotHome.SomeonNotHome_Options_Other] = MenuHelper:BuildMenu(SomeonNotHome.SomeonNotHome_Options_Other)
+	MenuHelper:AddMenuItem(nodes["blt_options"], SomeonNotHome.Main_Options_Menu, "SomeonNotHome_menu_title", "SomeonNotHome_menu_desc")
+	MenuHelper:AddMenuItem(nodes[SomeonNotHome.Main_Options_Menu], SomeonNotHome.SomeonNotHome_Options_Crew, "SomeonNotHome_menu_Crew_title", "SomeonNotHome_menu_desc")
+	MenuHelper:AddMenuItem(nodes[SomeonNotHome.Main_Options_Menu], SomeonNotHome.SomeonNotHome_Options_Other, "SomeonNotHome_menu_Other_title", "SomeonNotHome_menu_desc")
 end)
 
 Hooks:Add("MenuManagerPopulateCustomMenus", "SomeonNotHomeOptionsPopulate", function(...)
@@ -75,7 +85,23 @@ Hooks:Add("MenuManagerPopulateCustomMenus", "SomeonNotHomeOptionsPopulate", func
 			title = "menu_".._name,
 			callback = "SomeonNotHome_".. _name.. "_callback",
 			value = SomeonNotHome.Settings[_name],
-			menu_id = SomeonNotHome.Main_Options_Menu,  
+			menu_id = SomeonNotHome.SomeonNotHome_Options_Crew,  
+		})
+	end
+	for _, _name in ipairs({"butler", "yakuza_001", "yakuza_002", "bartender", "vlad", "vlad_girl001", "vlad_girl002", "vlad_girl003", "vlad_girl004", "Max", "butler_room_001", "butler_room_002"}) do
+		LocalizationManager:add_localized_strings({
+			["SomeonNotHome_".._name] = _name
+		})
+		MenuCallbackHandler["SomeonNotHome_".. _name.. "_callback"] = function(self, item)
+			SomeonNotHome.Settings[_name] = item:value() == "on" and true or false
+			SomeonNotHome:Save()
+		end
+		MenuHelper:AddToggle({
+			id = "SomeonNotHome_".. _name.. "_callback",
+			title = "SomeonNotHome_".._name,
+			callback = "SomeonNotHome_".. _name.. "_callback",
+			value = SomeonNotHome.Settings[_name],
+			menu_id = SomeonNotHome.SomeonNotHome_Options_Other,  
 		})
 	end
 end )
--- mods/Someon is not at safehouse/lua/elementspawncivilian.lua
+++ mods/Someon is not at safehouse/lua/elementspawncivilian.lua
@@ -5,16 +5,16 @@ if not Global.game_settings or Global.game_settings.level_id ~= "chill" then
 	return
 end
 
-local SNS_CharacterFilter = ElementPlayerCharacterFilter.on_executed
+local SNS_Civilian = ElementSpawnCivilian.on_executed
 
-function ElementPlayerCharacterFilter:on_executed(...)
+function ElementSpawnCivilian:on_executed(...)
 	if not self._values.enabled then
 		return
-	end
+	end	
 	if Global.game_settings.level_id == "chill" then
-		if managers.criminals:is_not_at_home(self:value("character")) then
+		if managers.criminals:is_not_at_home(tostring(self:editor_name())) then
 			return
 		end
 	end
-	SNS_CharacterFilter(self, ...)
+	SNS_Civilian(self, ...)
 end
