cc4164326262f2c64db8f99a31ca55e6fc7efe3e
DeadneM <deadnem@gmail.com>
2017-12-06 16:04:31 +0100

    Make sure none of teammate outlines is displayed
    outlines appear after bots getting downed or other case
    using a local value to disable all teammate outlines

--- mods/Remove Teammates Outline/hook.contourext.lua
+++ mods/Remove Teammates Outline/hook.contourext.lua
@@ -1,7 +1,8 @@
 local Remove_add = ContourExt.add
+local ROL_setup = "teammate" or "teammate_downed" or "teammate_dead" or "teammate_cuffed" or "teammate_downed_selected"
 
 function ContourExt:add(type, ...)
-  if type ~= "teammate" then
+  if type ~= "ROL_setup" then
     return Remove_add(self, type, ...)
   end
 end
