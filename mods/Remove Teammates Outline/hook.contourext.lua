6629dad5d2673b5b757d9915328ea335c852d7c3
Dr_Newbie <tf2baidonation@gmail.com>
2017-12-07 20:42:36 +0800

    check more outline

--- mods/Remove Teammates Outline/hook.contourext.lua
+++ mods/Remove Teammates Outline/hook.contourext.lua
@@ -1,8 +1,7 @@
 local Remove_add = ContourExt.add
-local ROL_setup = "teammate" or "teammate_downed" or "teammate_dead" or "teammate_cuffed" or "teammate_downed_selected"
 
 function ContourExt:add(type, ...)
-  if type ~= "ROL_setup" then
+  if type ~= "teammate" and type ~= "teammate_downed" and type ~= "teammate_dead" and type ~= "teammate_cuffed" and type ~= "teammate_downed_selected" then
     return Remove_add(self, type, ...)
   end
 end
