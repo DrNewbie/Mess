c7aa1e57bf0ec27233e820a06820de62f2a39575
Dr_Newbie <tf2baidonation@gmail.com>
2017-12-07 20:45:48 +0800

    .

--- mods/Remove Teammates Outline/hook.contourext.lua
+++ mods/Remove Teammates Outline/hook.contourext.lua
@@ -1,7 +1,7 @@
 local Remove_add = ContourExt.add
 
 function ContourExt:add(type, ...)
-  if type ~= "teammate" and type ~= "teammate_downed" and type ~= "teammate_dead" and type ~= "teammate_cuffed" and type ~= "teammate_downed_selected" then
-    return Remove_add(self, type, ...)
-  end
+	if type ~= "teammate" and type ~= "teammate_downed" and type ~= "teammate_dead" and type ~= "teammate_cuffed" and type ~= "teammate_downed_selected" then
+		return Remove_add(self, type, ...)
+	end
 end
