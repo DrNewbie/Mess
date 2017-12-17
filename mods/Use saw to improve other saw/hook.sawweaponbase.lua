e7783dedc059e3af3e22cc1e2de8c62cec93f0ac
Dr_Newbie <tf2baidonation@gmail.com>
2017-12-17 15:10:31 +0800

    rvd1 saw

--- mods/Use saw to improve other saw/hook.sawweaponbase.lua
+++ mods/Use saw to improve other saw/hook.sawweaponbase.lua
@@ -1,7 +1,7 @@
 Hooks:PostHook(SawHit, "on_collision", "UseSaw2ImproveOtherSaw", function(self, col_ray, weapon_unit, user_unit, damage)
 	local hit_unit = col_ray and col_ray.unit or nil
 	if hit_unit and hit_unit:base() then
-		if hit_unit:name() == Idstring("units/pd2_dlc_jolly/equipment/gen_interactable_saw/gen_interactable_saw") then
+		if hit_unit:name():key() == "0cec8292d88a4875" or hit_unit:name() == Idstring("units/pd2_dlc_jolly/equipment/gen_interactable_saw/gen_interactable_saw") then
 			if hit_unit:timer_gui() and type(hit_unit:timer_gui()._current_timer) == "number" and hit_unit:timer_gui()._current_timer > 1 then
 				hit_unit:timer_gui()._current_timer = hit_unit:timer_gui()._current_timer - damage * 0.3
 			end
