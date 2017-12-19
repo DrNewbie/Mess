1e619a5bc7423ae826ae0169251f51df316e3b9d
Dr_Newbie <tf2baidonation@gmail.com>
2017-12-20 06:19:21 +0800

    .

--- mods/Use saw to open safe/hook.sawweaponbase.lua
+++ mods/Use saw to open safe/hook.sawweaponbase.lua
@@ -2,12 +2,12 @@ Hooks:PostHook(SawHit, "on_collision", "UseSaw2OpenSafe", function(self, col_ray
 	local hit_unit = col_ray and col_ray.unit or nil
 	if hit_unit and hit_unit:base() then
 		if type(damage) == "number" and hit_unit:base() and hit_unit:base().tweak_data then
-			local td = tostring(hit_unit:base().tweak_data)
+			local td = string.lower(tostring(hit_unit:base().tweak_data))
 			if type(hit_unit:base()._addon_saw_open_init) ~= "number" then
 				local health = 0
 				if td:find("safe") then
 					health = 100
-					if td:find("titan") then health = health + 200 end
+					if td:find("titan") then health = -1 end
 					if td:find("giga") then health = health + 100 end
 					if td:find("large") then health = health + 50 end
 					if td:find("90sec") then health = health + 45 end
