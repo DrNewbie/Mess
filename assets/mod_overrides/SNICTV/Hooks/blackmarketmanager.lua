41f022037409677bcd672f2890129be0c5bab12c
Dr_Newbie <tf2baidonation@gmail.com>
2018-02-20 14:57:17 +0800

    .

--- assets/mod_overrides/SNICTV/Hooks/blackmarketmanager.lua
+++
@@ -1,4 +0,0 @@
-Hooks:PostHook(BlackMarketManager, "on_aquired_armor", "Armor8_on_aquired_armor", function(self)	
-	self._global.armors["level_8"].unlocked = self._global.armors["level_1"].unlocked
-	self._global.armors["level_8"].owned = self._global.armors["level_1"].owned
-end)
--- assets/mod_overrides/SNICTV/Hooks/blackmarkettweakdata.lua
+++
@@ -1,8 +0,0 @@
-Hooks:PostHook(BlackMarketTweakData, "_init_armors", "Armor8_init_armors", function(self)	
-	self.armors.level_8 = {
-		name_id = "bm_armor_level_8",
-		sequence = "var_model_08",
-		upgrade_level = 8
-	}
-	self:_add_desc_from_name_macro(self.armors)
-end)
