8c78e0e36c78beca853cd6afa857ede5623dfa8e
Dr_Newbie <tf2baidonation@gmail.com>
2018-02-20 14:57:04 +0800

    .

--- mods/Fully Charged Perk Deck/Base.lua
+++
@@ -1,9 +0,0 @@
-local FullyCharged_ModPath = ModPath
-
-if ModCore then
-	ModCore:new(FullyCharged_ModPath .. "Config.xml", true, true)
-else
-	Hooks:Add("LocalizationManagerPostInit", "FullyCharged_loc", function(loc)
-		loc:load_localization_file(FullyCharged_ModPath.."Loc/EN.txt")
-	end)
-end
--- mods/Pulverizer Perk Deck/Base.lua
+++
@@ -1,9 +0,0 @@
-local PulverizerPD_ModPath = ModPath
-
-if ModCore then
-	ModCore:new(PulverizerPD_ModPath .. "Config.xml", true, true)
-else
-	Hooks:Add("LocalizationManagerPostInit", "PulverizerPD_loc", function(loc)
-		loc:load_localization_file(PulverizerPD_ModPath.."Loc/EN.txt")
-	end)
-end
