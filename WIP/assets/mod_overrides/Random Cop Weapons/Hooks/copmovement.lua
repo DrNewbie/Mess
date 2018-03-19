7ee6210d4b56a11baf9a818f5b6584e0fa99dc1b
Dr_Newbie <tf2baidonation@gmail.com>
2018-03-19 21:48:40 +0800

    Random Cop Weapons ; 21
    https://imgur.com/a/092zk

--- WIP/assets/mod_overrides/Random Cop Weapons/Hooks/copmovement.lua
+++ WIP/assets/mod_overrides/Random Cop Weapons/Hooks/copmovement.lua
@@ -2,7 +2,15 @@ local CopRandomWeapon = CopMovement.add_weapons
 
 function CopMovement:add_weapons(...)
 	local _default_weapon_id = self._ext_base._default_weapon_id or "m4"
-	if math.random() < 0.75 and self._unit and alive(self._unit) and self._unit:in_slot(managers.slot:get_mask("enemies")) and managers.weapon_factory and self._unit:inventory() and self._unit:inventory().add_unit_by_factory_name and not self._unit:inventory()._shield_unit_name and _default_weapon_id then
+	local _use_defualt_weapon = false
+	if RndCopWeapGKey and RndCopWeapGKey.Options and RndCopWeapGKey.Options:GetValue("EnableDefaultWep") then
+		if RndCopWeapGKey.Options:GetValue("DefaultWepRate") < math.random() then
+			_use_defualt_weapon = false
+		else
+			_use_defualt_weapon = true
+		end
+	end
+	if not _use_defualt_weapon and self._unit and alive(self._unit) and self._unit:in_slot(managers.slot:get_mask("enemies")) and managers.weapon_factory and self._unit:inventory() and self._unit:inventory().add_unit_by_factory_name and not self._unit:inventory()._shield_unit_name and _default_weapon_id then
 		local usage = nil
 		local crew_wep = _default_weapon_id.."_crew"
 		if tweak_data.weapon[crew_wep] and tweak_data.weapon[crew_wep].usage then
@@ -19,28 +27,34 @@ function CopMovement:add_weapons(...)
 			local weapon = table.random(RandomWeaponMap)
 			if weapon and (prim_weap_name or sec_weap_name) then
 				local cosmetics_str = ""
-				--[[
-				if RndCopWeapGKey and RndCopWeapGKey.Options and RndCopWeapGKey.Options:GetValue("EnableSkins") then
-					if math.random() < RndCopWeapGKey.Options:GetValue("SkinsRate") then
-						cosmetics_str = table.random_key(tweak_data.blackmarket.weapon_skins) .. "-" .. table.random_key(tweak_data.economy.qualities) .. "-0"
-					end
-				end
-				]]
 				local blueprint_string, blueprint = nil, {}
-				local factory_data = tweak_data.weapon.factory[weapon]
-				if math.random() < 0.45 and type(factory_data) == "table" and type(factory_data.uses_parts) == "table" then
-					local mods_type = {}
-					for i, d in pairs(factory_data.uses_parts) do 
-						if tweak_data.weapon.factory.parts[d] then
-							local mod_type = tostring(tweak_data.weapon.factory.parts[d].type)
-							mods_type[mod_type] = mods_type[mod_type] or {}
-							table.insert(mods_type[mod_type], d)
+				if RndCopWeapGKey and RndCopWeapGKey.Options then
+					if RndCopWeapGKey.Options:GetValue("EnableSkins") then
+						if math.random() < RndCopWeapGKey.Options:GetValue("SkinsRate") then
+							local weapon_id = managers.weapon_factory:get_weapon_id_by_factory_id(weapon:gsub("_npc", ""))
+							if weapon_id then
+								local skins_key = managers.blackmarket:get_weapon_skins(weapon_id)
+								cosmetics_str = table.random_key(skins_key) .. "-" .. table.random_key(tweak_data.economy.qualities) .. "-0"
+							end
 						end
 					end
-					for i, d in pairs(mods_type) do 
-						table.insert(blueprint, table.random(d))
+					if RndCopWeapGKey.Options:GetValue("EnableWepMods") then
+						local factory_data = tweak_data.weapon.factory[weapon]
+						if math.random() < RndCopWeapGKey.Options:GetValue("WepModsRate") and type(factory_data) == "table" and type(factory_data.uses_parts) == "table" then
+							local mods_type = {}
+							for i, d in pairs(factory_data.uses_parts) do 
+								if tweak_data.weapon.factory.parts[d] then
+									local mod_type = tostring(tweak_data.weapon.factory.parts[d].type)
+									mods_type[mod_type] = mods_type[mod_type] or {}
+									table.insert(mods_type[mod_type], d)
+								end
+							end
+							for i, d in pairs(mods_type) do 
+								table.insert(blueprint, table.random(d))
+							end
+							blueprint_string = managers.weapon_factory:blueprint_to_string(weapon:gsub("_npc", ""), blueprint)
+						end
 					end
-					blueprint_string = managers.weapon_factory:blueprint_to_string(weapon:gsub("_npc", ""), blueprint)
 				end
 				self._unit:inventory():add_unit_by_factory_name(weapon, true, true, blueprint_string, cosmetics_str)
 			else
---
+++ WIP/assets/mod_overrides/Random Cop Weapons/Hooks/menumanager.lua
@@ -0,0 +1,3 @@
+if ModCore then
+	RndCopWeapGKey = RndCopWeapGKey or ModCore:new("assets/mod_overrides/Random Cop Weapons/Config.xml", true, true)
+end
