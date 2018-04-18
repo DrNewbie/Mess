adadf69777f9f4b1043d895b6da8bbb639f1be34
Dr_Newbie <tf2baidonation@gmail.com>
2018-04-18 20:24:57 +0800

    .

--- mods/SwanSongSoundEffect/SwanSongEffect.lua
+++ mods/SwanSongSoundEffect/SwanSongEffect.lua
@@ -1,20 +1,23 @@
 Hooks:PostHook(HUDManager, "set_teammate_custom_radial", "PlySwanSongSoundEffect", function(self, i, data)
-	local hud = managers.hud:script(PlayerBase.PLAYER_INFO_HUD_FULLSCREEN_PD2)
-	if hud and hud.panel then
-		if data and data.current and data.total then
-			if PackageManager:has(Idstring("movie"), Idstring("SwanSongSoundEffect")) then
-				if not alive(hud.panel:child("ids_SwanSongSoundEffect")) then
-					hud.panel:video({
-						name = "ids_SwanSongSoundEffect",
-						video = "SwanSongSoundEffect",
-						visible = false,
-						loop = true,
-					}):set_volume_gain(1)
+	local peer = managers.network and managers.network:session():peer(i) or nil
+	if (peer and peer == managers.network:session():local_peer()) or Global.game_settings.single_player then
+		local hud = managers.hud:script(PlayerBase.PLAYER_INFO_HUD_FULLSCREEN_PD2)
+		if hud and hud.panel then
+			if data and data.current and data.total then
+				if PackageManager:has(Idstring("movie"), Idstring("SwanSongSoundEffect")) then
+					if not alive(hud.panel:child("ids_SwanSongSoundEffect")) then
+						hud.panel:video({
+							name = "ids_SwanSongSoundEffect",
+							video = "SwanSongSoundEffect",
+							visible = false,
+							loop = true,
+						}):set_volume_gain(1)
+					end
 				end
-			end
-			if data.current <= 0 or data.current/data.total <= 0 then
-				if alive(hud.panel:child("ids_SwanSongSoundEffect")) then
-					hud.panel:remove(hud.panel:child("ids_SwanSongSoundEffect"))
+				if data.current <= 0 or data.current/data.total <= 0 then
+					if alive(hud.panel:child("ids_SwanSongSoundEffect")) then
+						hud.panel:remove(hud.panel:child("ids_SwanSongSoundEffect"))
+					end
 				end
 			end
 		end
