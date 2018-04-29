1b34b1e62275758004149abaa4aa10842fc686bb
Dr_Newbie <tf2baidonation@gmail.com>
2018-04-29 15:48:03 +0800

    .

--- mods/Lock n' Load Sprint Animations/playerstandard.lua
+++ mods/Lock n' Load Sprint Animations/playerstandard.lua
@@ -1,5 +1,5 @@
 Hooks:PostHook(PlayerStandard, "set_running", "RunAndShootAnim_set_running", function(self)
-	if self._ext_camera and self.RUN_AND_RELOAD and self._rar_running and not self._running then
+	if self._ext_camera and self._rar_running and not self._running then
 		self._rar_running = nil
 		self._rar_hold_back = nil
 		self._rar_hold_back_t = nil
@@ -12,14 +12,14 @@ Hooks:PostHook(PlayerStandard, "set_running", "RunAndShootAnim_set_running", fun
 end)
 
 Hooks:PostHook(PlayerStandard, "_start_action_running", "RunAndShootAnim_start_action_running", function(self)
-	if self._ext_camera and self.RUN_AND_RELOAD and self._running and not self:_is_reloading() and not self._shooting then
+	if self._ext_camera and self._running and not self:_is_reloading() and not self._shooting then
 		self._rar_running = true
 		self._ext_camera:play_redirect(self:get_animation("start_running"))
 	end
 end)
 
 Hooks:PostHook(PlayerStandard, "_check_action_run", "RunAndShootAnim_check_action_run", function(self, t)
-	if self._ext_camera and self.RUN_AND_RELOAD then
+	if self._ext_camera then
 		local is_bool = (self:_is_reloading() or self._shooting) and true or false
 		if self._running then
 			if is_bool then
