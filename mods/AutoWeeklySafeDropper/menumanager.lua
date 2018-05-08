f2e03fc362f6e8207019ce47cc0689d10e3db3f6
Dr_Newbie <tf2baidonation@gmail.com>
2018-05-08 22:11:45 +0800

    .

--- mods/AutoWeeklySafeDropper/menumanager.lua
+++ mods/AutoWeeklySafeDropper/menumanager.lua
@@ -1,9 +1,5 @@
 Hooks:Add('MenuManagerOnOpenMenu', 'WeeklySafeDrop_RunInitNow', function(self, menu)
 	if (menu == 'menu_main' or menu == 'lobby') and managers.network and managers.network.account then
-		if Global._WeeklySafeDropBool then
-			return
-		end
-		Global._WeeklySafeDropBool = true
 		DelayedCalls:Add('WeeklySafeDrop_RunInitNow_Delay', 1, function()
 			_G.WeeklySafeDrop = _G.WeeklySafeDrop or {}	
 			function WeeklySafeDrop:Ask()
