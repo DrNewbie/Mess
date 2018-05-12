8997e107da0a3e894337b03c732cc1415cde6af2
Dr_Newbie <tf2baidonation@gmail.com>
2018-05-12 21:32:37 +0800

    No Body Bags Required (Lite)[3]

--- mods/No Body Bags Required (Lite)/playermanager.lua
+++ mods/No Body Bags Required (Lite)/playermanager.lua
@@ -1,3 +1,6 @@
+tweak_data.carry.person.type = "heavy"
+tweak_data.carry.special_person.type = "heavy"
+
 Hooks:PostHook(PlayerManager, "_set_body_bags_amount", "NoBodyBagsRequiredLite", function(self)
 	self._local_player_body_bags = self:max_body_bags() + 1
 	managers.hud:on_ext_inventory_changed()
