454d5dcd76b2198c1b31d352b1a1f4c272764877
Dr_Newbie <tf2baidonation@gmail.com>
2018-07-02 19:35:26 +0800

    .sav Mod[5] ; exp and version

--- WIP/mods/.sav Mod/blackmarketmanager.lua
+++ WIP/mods/.sav Mod/blackmarketmanager.lua
@@ -56,9 +56,15 @@ function BlackMarketManager:load(data, ...)
 			log("[BlkManFix] save_data is not table.")
 		elseif save_data.Empty then
 			log("[BlkManFix] save_data is 'Empty'.")
-		elseif type(data.blackmarket) ~= "table" then
-			log("[BlkManFix] data.blackmarket is not table.")
 		else
+			if type(data.blackmarket) ~= "table" then
+				log("[BlkManFix] data.blackmarket is not table.")
+				data.blackmarket = {}
+			end
+			if type(data.exp) ~= "table" then
+				log("[BlkManFix] data.exp is not table.")
+				data.exp = {}
+			end
 			if type(save_data.crafted_items) ~= "table" then
 				save_data.crafted_items = nil
 			else
@@ -136,6 +142,44 @@ function BlackMarketManager:load(data, ...)
 			data.blackmarket._preferred_character = save_data._preferred_character or BlkManFix_Default._preferred_character
 			data.blackmarket.armors = save_data.armors or BlkManFix_Default.armors
 			data.blackmarket._preferred_henchmen = save_data._preferred_henchmen or BlkManFix_Default._preferred_henchmen
+			if type(save_data.exp.level) == "number" then
+				data.ExperienceManager.level = Application:digest_value(math.max(save_data.exp.level, 1), true)
+			else
+				log("[BlkManFix] exp.level is not number")
+			end
+			if type(save_data.exp.xp_gained) == "number" then
+				data.ExperienceManager.xp_gained = Application:digest_value(math.max(save_data.exp.xp_gained, 0), true)
+			else
+				log("[BlkManFix] exp.xp_gained is not number")
+			end
+			if type(save_data.exp.total) == "number" then
+				data.ExperienceManager.total = Application:digest_value(math.max(save_data.exp.total, 0), true)
+			else
+				log("[BlkManFix] exp.total is not number")
+			end
+			if type(save_data.exp.rank) == "number" then
+				data.ExperienceManager.rank = Application:digest_value(math.max(save_data.exp.rank, 0), true)
+			else
+				log("[BlkManFix] exp.rank is not number")
+			end
+			if type(save_data.exp.next_level_data) ~= "table" then
+				save_data.exp.next_level_data = {
+					points = 1,
+					current_points = 0,
+				}
+			else
+				log("[BlkManFix] exp.next_level_data is not table")
+			end
+			if type(save_data.exp.next_level_data.points) == "number" then
+				data.ExperienceManager.next_level_data.points = Application:digest_value(math.max(save_data.exp.next_level_data.points, 1), true)
+			else
+				log("[BlkManFix] exp.next_level_data.points is not number")
+			end
+			if type(save_data.exp.next_level_data.current_points) == "number" then
+				data.ExperienceManager.next_level_data.current_points = Application:digest_value(math.max(save_data.exp.next_level_data.current_points, 0), true)
+			else
+				log("[BlkManFix] exp.next_level_data.current_points is not number")
+			end
 		end		
 	end
 	return Post_BlkManFix_Load(self, data, ...)
@@ -180,6 +224,44 @@ Hooks:PostHook(BlackMarketManager, 'save', 'Post_BlkManFix_Save', function(self,
 		if data.blackmarket._preferred_character then
 			save2files._preferred_character = data.blackmarket._preferred_character
 		end
+		if type(data.ExperienceManager) ~= "table" then
+			log("[BlkManFix] data.ExperienceManager is not table")
+		else
+			log("[BlkManFix] data.ExperienceManager")		
+			save2files.exp = {
+				level = 1,
+				xp_gained = 0,
+				total = 0,
+				rank = 0,
+				next_level_data = {
+					points = 1,
+					current_points = 0
+				}
+			}
+			local EXP_table = data.ExperienceManager
+			local digest_value_type = type(Application:digest_value(1, true))
+			if type(EXP_table.level) == digest_value_type then
+				save2files.exp.level = Application:digest_value(EXP_table.level, false)
+			end
+			if type(EXP_table.xp_gained) == digest_value_type then
+				save2files.exp.xp_gained = Application:digest_value(EXP_table.xp_gained, false)
+			end
+			if type(EXP_table.total) == digest_value_type then
+				save2files.exp.total = Application:digest_value(EXP_table.total, false)
+			end
+			if type(EXP_table.rank) == digest_value_type then
+				save2files.exp.rank = Application:digest_value(EXP_table.rank, false)
+			end
+			if type(EXP_table.next_level_data) == "table" then
+				if type(EXP_table.next_level_data.points) == digest_value_type then
+					save2files.exp.next_level_data.points = Application:digest_value(EXP_table.next_level_data.points, false)
+				end
+				if type(EXP_table.next_level_data.current_points) == digest_value_type then
+					save2files.exp.next_level_data.current_points = Application:digest_value(EXP_table.next_level_data.current_points, false)
+				end
+			end
+		end
+		save2files.Version = os.date("%m/%d/%Y - %X")
 		save_files:write(json.encode(save2files))
 		save_files:close()
 	end
