8aff260b5984fb751945918de6a9db3ae3b391fe
Dr_Newbie <tf2baidonation@gmail.com>
2018-07-04 22:15:11 +0800

    NO SA[3]

--- mods/NO SA/Nope.lua
+++ mods/NO SA/Nope.lua
@@ -1,14 +1,14 @@
-local AddSAIntoLobby2Numbers = NetworkMatchMakingSTEAM.is_server_ok
+local NOSALOBBY_is_server_ok = NetworkMatchMakingSTEAM.is_server_ok
 function NetworkMatchMakingSTEAM:is_server_ok(friends_only, room, attributes_list, ...)
 	if attributes_list.numbers[11] ~= "value_pending" then
 		return false
 	end
-	return AddSAIntoLobby2Numbers(self, friends_only, room, attributes_list, ...)
+	return NOSALOBBY_is_server_ok(self, friends_only, room, attributes_list, ...)
 end
 
-local AddSAIntoLobby2Numbers = NetworkMatchMakingSTEAM._lobby_to_numbers
+local NOSALOBBY_lobby_to_numbers = NetworkMatchMakingSTEAM._lobby_to_numbers
 function NetworkMatchMakingSTEAM._lobby_to_numbers(self, lobby, ...)
-	local numbers = AddSAIntoLobby2Numbers(self, lobby, ...)
+	local numbers = NOSALOBBY_lobby_to_numbers(self, lobby, ...)
 	table.insert(numbers, lobby:key_value("silent_assassin"))
 	return numbers
 end
