9647de125b04ae64505c7c7a1b51a1f149e7a4d9
Dr_Newbie <tf2baidonation@gmail.com>
2018-07-15 05:28:58 +0800

    Slow down time when you are not moving[2]
    http://modwork.shop/22956

--- mods/Slow down time when you are not moving/ReSet.lua
+++ mods/Slow down time when you are not moving/ReSet.lua
@@ -1,5 +1,11 @@
 if TimerManager and TimerManager.timer then
-	for _, v in pairs({"player", "game", "game_animation"}) do
-		TimerManager:timer(Idstring(v)):set_multiplier(1)
+	if TimerManager:timer(Idstring("game")) then
+		TimerManager:timer(Idstring("game")):set_multiplier(1)
+	end
+	if TimerManager:timer(Idstring("player")) then
+		TimerManager:timer(Idstring("player")):set_multiplier(1)
+	end
+	if TimerManager:timer(Idstring("game_animation")) then
+		TimerManager:timer(Idstring("game_animation")):set_multiplier(1)
 	end
 end
