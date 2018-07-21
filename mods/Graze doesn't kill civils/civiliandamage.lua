aa633419121ba7eb8a4908259ab768ac122ba34b
Dr_Newbie <tf2baidonation@gmail.com>
2018-07-21 21:38:21 +0800

    .

--- mods/Graze doesn't kill civils/civiliandamage.lua
+++ mods/Graze doesn't kill civils/civiliandamage.lua
@@ -1,7 +1,7 @@
 GrazeNoDMG2Me = GrazeNoDMG2Me or CivilianDamage.damage_simple
 
 function CivilianDamage:damage_simple(attack_data, ...)
-	if type(attack_data) == "talbe" and attack_data.variant and attack_data.variant == "graze" then
+	if type(attack_data) == "table" and attack_data.variant and attack_data.variant == "graze" then
 		return
 	end
 	return GrazeNoDMG2Me(self, attack_data, ...)
