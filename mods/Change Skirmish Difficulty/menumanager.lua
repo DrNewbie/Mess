5e61bf8fd210a944c3aa8b06840d708119f406a6
Dr_Newbie <tf2baidonation@gmail.com>
2018-08-18 06:15:39 +0800

    Change Skirmish Difficulty[2]

--- mods/Change Skirmish Difficulty/menumanager.lua
+++ mods/Change Skirmish Difficulty/menumanager.lua
@@ -36,23 +36,53 @@ end )
 
 
 function CHOD:ResetToDefaultValues()
-	self.settings.CHOD_slider_value = 1
+	CHOD.settings.CHOD_slider_value = 1
 end
 
 if CHOD.settings.CHOD_slider_value == nil then
 	self:ResetToDefaultValues()
 end
 
-function CHOD:Apply()
-	if type(SkirmishManager) == "table" and SkirmishManager:is_skirmish() and Global.game_settings and Global.game_settings.difficulty then
-		if self.settings.CHOD_slider_value == 2 then
-			Global.game_settings.difficulty = "easy_wish"
-		elseif self.settings.CHOD_slider_value == 3 then
-			Global.game_settings.difficulty = "overkill_290"
-		elseif self.settings.CHOD_slider_value == 4 then
-			Global.game_settings.difficulty = "sm_wish"
-		elseif self.settings.CHOD_slider_value == 1 then
-			Global.game_settings.difficulty = "overkill_145"
-		end
+function MenuCallbackHandler:accept_skirmish_contract(item, node)
+	managers.menu:active_menu().logic:navigate_back(true)
+	managers.menu:active_menu().logic:navigate_back(true)
+	local job_data = {
+		difficulty = "overkill_145",
+		job_id = managers.skirmish:random_skirmish_job_id()
+	}	
+	if CHOD.settings.CHOD_slider_value == 2 then
+		job_data.difficulty = "easy_wish"
+	elseif CHOD.settings.CHOD_slider_value == 3 then
+		job_data.difficulty = "overkill_290"
+	elseif CHOD.settings.CHOD_slider_value == 4 then
+		job_data.difficulty = "sm_wish"
+	end
+	if Global.game_settings.single_player then
+		MenuCallbackHandler:start_single_player_job(job_data)
+	else
+		MenuCallbackHandler:start_job(job_data)
+	end
+end
+
+function MenuCallbackHandler:accept_skirmish_weekly_contract(item, node)
+	managers.menu:active_menu().logic:navigate_back(true)
+	managers.menu:active_menu().logic:navigate_back(true)
+	local weekly_skirmish = managers.skirmish:active_weekly()
+	local job_data = {
+		difficulty = "overkill_145",
+		weekly_skirmish = true,
+		job_id = weekly_skirmish.id
+	}
+	if CHOD.settings.CHOD_slider_value == 2 then
+		job_data.difficulty = "easy_wish"
+	elseif CHOD.settings.CHOD_slider_value == 3 then
+		job_data.difficulty = "overkill_290"
+	elseif CHOD.settings.CHOD_slider_value == 4 then
+		job_data.difficulty = "sm_wish"
+	end
+	if Global.game_settings.single_player then
+		MenuCallbackHandler:start_single_player_job(job_data)
+	else
+		MenuCallbackHandler:start_job(job_data)
 	end
 end
--- mods/Change Skirmish Difficulty/skirmishmanager.lua
+++
@@ -1,3 +0,0 @@
-Hooks:PostHook(SkirmishManager, "apply_matchmake_attributes", "ChangeHOLDOUTDifficulty", function(self)
-	CHOD:Apply()
-end)
