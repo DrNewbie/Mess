9a4d4b5f6688d78c27cbcf2a4648160ed6ab0353
Dr_Newbie <tf2baidonation@gmail.com>
2018-08-19 07:13:14 +0800

    Trigger Happy Hand Effect[2]

--- mods/Trigger Happy Hand Effect/fpcameraplayerbase.lua
+++ mods/Trigger Happy Hand Effect/fpcameraplayerbase.lua
@@ -10,17 +10,19 @@ Hooks:PostHook(FPCameraPlayerBase, "update", "trigger_happy_hand_effect_update",
 	if self._is_trigger_happy_hand_effect then
 		local ans = managers.player:get_property("trigger_happy", 1)
 		if self._last_trigger_happy_property ~= ans then
-			if ans == 1 then
+			if ans <= 1 then
 				for k, v in pairs(self._trigger_happy_hand_effect_list) do
 					World:effect_manager():fade_kill(v)
 					self._trigger_happy_hand_effect_list[k] = nil
-				end
+				end			
 			else
-				for k , v in ipairs({"a_weapon_left", "a_weapon_right"}) do					
-					table.insert(self._trigger_happy_hand_effect_list, World:effect_manager():spawn({
-						effect = Idstring("effects/payday2/particles/weapons/saw/sawing"),
-						parent = self._parent_unit:camera()._camera_unit:get_object(Idstring(v))
-					}))
+				if not self._trigger_happy_hand_effect_list[1] then
+					for k , v in ipairs({"a_weapon_left", "a_weapon_right"}) do					
+						table.insert(self._trigger_happy_hand_effect_list, World:effect_manager():spawn({
+							effect = Idstring("effects/payday2/particles/explosions/molotov_grenade_enemy_on_fire_endless"),
+							parent = self._parent_unit:camera()._camera_unit:get_object(Idstring(v))
+						}))
+					end
 				end
 			end
 			self._last_trigger_happy_property = ans
