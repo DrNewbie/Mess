7edccd6c5b889ed694814314825eb075c9b9ad01
Dr_Newbie <tf2baidonation@gmail.com>
2018-08-20 21:26:43 +0800

    Sharp Dressers ALT[2]

--- mods/Sharp Dressers ALT/menuscenemanager.lua
+++ mods/Sharp Dressers ALT/menuscenemanager.lua
@@ -6,12 +6,18 @@ Hooks:PostHook(MenuSceneManager, "set_character_armor", "SharpDressers_set_chara
 	unit = unit or self._character_unit
 	if managers.menu_component and not managers.menu_component._player_inventory_gui and managers.network and managers.network:session() and unit and unit:damage() and unit:damage():has_sequence("spawn_prop_tux") then
 		unit:damage():run_sequence_simple("spawn_prop_tux")
+	else
+		unit:damage():run_sequence_simple(tweak_data.blackmarket.armors[armor_id].sequence)
 	end
 end)
 
-Hooks:PostHook(MenuSceneManager, "set_henchmen_loadout", "SharpDressers_set_henchmen_loadout", function(self, index)
+Hooks:PostHook(MenuSceneManager, "set_henchmen_loadout", "SharpDressers_set_henchmen_loadout", function(self, index, character, loadout)
 	local unit = self._henchmen_characters and self._henchmen_characters[index]
 	if managers.menu_component and not managers.menu_component._player_inventory_gui and managers.network and managers.network:session() and unit and unit:damage() and unit:damage():has_sequence("spawn_prop_tux") then
 		unit:damage():run_sequence_simple("spawn_prop_tux")
+	else
+		loadout = loadout or managers.blackmarket:henchman_loadout(index)
+		local armor_id = loadout and loadout.armor
+		unit:damage():run_sequence_simple(tweak_data.blackmarket.armors[armor_id].sequence)
 	end
 end)
