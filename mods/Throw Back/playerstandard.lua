faca8d2575bf7c915a0cabdb3e3c31c3ded7fa0e
Dr_Newbie <tf2baidonation@gmail.com>
2018-09-08 21:09:27 +0800

    Throw Back[3]

--- mods/Throw Back/playerstandard.lua
+++ mods/Throw Back/playerstandard.lua
@@ -1,3 +1,18 @@
+Hooks:PostHook(PlayerStandard, "init", "ThrowBackInit_Load_concussion_resource", function(self)
+	local data = tweak_data.blackmarket.projectiles["concussion"]
+	if data then
+		local unit_name = Idstring(not Network:is_server() and data.local_unit or data.unit)
+		if not managers.dyn_resource:is_resource_ready(Idstring("unit"), unit_name, managers.dyn_resource.DYN_RESOURCES_PACKAGE) then
+			managers.dyn_resource:load(Idstring("unit"), unit_name, managers.dyn_resource.DYN_RESOURCES_PACKAGE)
+			return
+		end
+	end	
+end)
+
+if Network:is_client() then
+	return
+end
+
 function PlayerStandard:ThrowBack(t)
 	t = t or TimerManager:game():time()
 	local data = tweak_data.blackmarket.projectiles["concussion"]
