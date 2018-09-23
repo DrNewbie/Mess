f57448416a3b6d9942b240b96f6d25f6ae6f954e
Dr_Newbie <tf2baidonation@gmail.com>
2018-09-24 00:46:19 +0800

    .

---
+++ mods/Increase your dodge while there are civilians nearby/Lua/hook.playermanager.lua
@@ -0,0 +1,24 @@
+function PlayerManager:is_less_targeted()
+	if not self:player_unit() then
+		return
+	end
+	for u_key, u_data in pairs(managers.enemy:all_civilians()) do
+		if u_data.unit then
+			local dis = math.abs(mvector3.distance(u_data.unit:position(), self:player_unit():position()))
+			if dis < 300 then
+				return true
+			end
+		end
+	end
+	return
+end
+
+LessHitMe_PlyM_dodge_chance = PlayerManager.skill_dodge_chance
+
+function PlayerManager:skill_dodge_chance(...)
+	local chance = LessHitMe_PlyM_dodge_chance(self, ...)
+	if self:is_less_targeted() then
+		chance = chance + 0.15
+	end
+	return chance
+end
