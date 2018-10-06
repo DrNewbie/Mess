d6449042be08764c0ecca0f1b334754cd9304f84
Dr_Newbie <tf2baidonation@gmail.com>
2018-10-06 14:38:49 +0800

    Kick Low Steam Levels User[2]

--- mods/Kick Low Steam Levels User/NetWork.lua
+++ mods/Kick Low Steam Levels User/NetWork.lua
@@ -13,24 +13,24 @@ Hooks:Add("NetworkManagerOnPeerAdded", "JokeSteamLevelsKicked", function(peer, p
 	if Steam and Steam:logged_on() then
 		for _, friend in ipairs(Steam:friends() or {}) do
 			if friend:id() == user_id then
-				--return
+				return
 			end
 		end
 	end
-	DelayedCalls:Add('JokeSteamLevelsKicked_Delay_'..user_id, 3, function()
-		dohttpreq("http://steamcommunity.com/profiles/"..user_id.."/?l=english",
-			function (page)
-				page = tostring(page)
-				if not page:find('friendPlayerLevelNum') then
-					return
-				end			
-				local levels = tostring(string.match(page, '<span class="friendPlayerLevelNum">(%d+)</span>'))
+	dohttpreq("http://steamcommunity.com/profiles/"..user_id.."/?l=english",
+		function (page)
+			page = tostring(page)
+			local levels = 0
+			if not page:find('friendPlayerLevelNum') then
+				
+			else
+				levels = tostring(string.match(page, '<span class="friendPlayerLevelNum">(%d+)</span>'))
 				levels = tonumber(levels) or 0
-				if levels < JokeSteamLevelsBan.Data.low_leves then			
-					session:on_peer_kicked(peer, id, 0)
-					session:send_to_peers("kick_peer", id, 0)
-					managers.chat:_receive_message(ChatManager.GAME, "[LowSteamLevels]", peer:name().." steam levels '"..levels.."' is lower than "..JokeSteamLevelsBan.Data.low_leves, Color.red)
-				end
-		end)
+			end			
+			if levels < JokeSteamLevelsBan.Data.low_leves then			
+				session:on_peer_kicked(peer, id, 0)
+				session:send_to_peers("kick_peer", id, 0)
+				managers.chat:_receive_message(ChatManager.GAME, "[LowSteamLevels]", peer:name().." steam levels '"..levels.."' is lower than "..JokeSteamLevelsBan.Data.low_leves, Color.red)
+			end
 	end)
 end)
