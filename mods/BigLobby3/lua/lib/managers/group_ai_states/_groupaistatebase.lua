737326fecdf8cf4facee4d16fc749926a706a8a0
Dr_Newbie <tf2baidonation@gmail.com>
2018-11-25 17:45:27 +0800

    .

---
+++ mods/BigLobby3/lua/lib/managers/group_ai_states/_groupaistatebase.lua
@@ -0,0 +1,24 @@
+-- Modified to support full team size instead of hardcoded 3, not hugely relevant as is a bot thing.
+function GroupAIStateBase:on_criminal_team_AI_enabled_state_changed()
+	local num_player_slots = BigLobbyGlobals:num_player_slots() - 1
+
+	-- Only code changed was replacing hardcoded 3 with variable num_player_slots
+	if Network:is_client() then
+		return
+	end
+	if managers.groupai:state():team_ai_enabled() then
+		self:fill_criminal_team_with_AI()
+	else
+		for i = 1, num_player_slots do
+			self:remove_one_teamAI()
+		end
+	end
+end
+
+Hooks:PostHook( GroupAIStateBase , "whisper_mode" , "GroupAIStateBasePostWhisperMode" , function( self )
+	for _, ai in pairs(managers.groupai:state():all_AI_criminals()) do
+	if self._whisper_mode == true and BigLobbyGlobals.auto_stop_all_bots_settings then
+		ai.unit:movement():set_should_stay(true)
+	end
+	end
+end )
