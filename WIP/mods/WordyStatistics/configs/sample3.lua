ae53b8c73ce13ebd25f0b7af28d5f0ea0e2ef0e9
Dr_Newbie <tf2baidonation@gmail.com>
2019-02-08 23:28:42 +0800

    renmae

--- WIP/mods/WordyStatistics/configs/sample3.lua
+++
@@ -1,5 +0,0 @@
-_G.RWSN2W = _G.RWSN2W or {}
-
-RWSN2W:Add(nil, function(weapon_data, stat_name, stat_data, them)
-	RWSN2W:Simple_Set_Weapon_Title(them, nil, Color(math.random(), math.random(), math.random()))
-end)
