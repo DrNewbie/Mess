1a5a14a216214af943fd2a5c5d573c980fd5dd1f
Dr_Newbie <tf2baidonation@gmail.com>
2019-02-08 15:41:40 +0800

    Text Weapon Stats[2]

--- WIP/mods/Text Weapon Stats/lua/blackmarketgui.lua
+++ WIP/mods/Text Weapon Stats/lua/blackmarketgui.lua
@@ -1,6 +1,6 @@
 _G.RWSN2W = _G.RWSN2W or {}
 
-function RWSN2W:ReplaceWepStatsNumber2Words(data)
+function RWSN2W:Run(data)
 	local weapon_data = data.weapon_data
 	local stat_name = data.stat_name
 	local stat_data = data.stat_data
@@ -9,25 +9,38 @@ function RWSN2W:ReplaceWepStatsNumber2Words(data)
 --==============================================--
 	if stat_name == "damage" then
 		local damage_e = tonumber(stat_data.equip)
-		if damage_e and damage_e > 400 then
-			them._stats_texts[stat_name].base:set_text("")
-			them._stats_texts[stat_name].mods:set_text("")
-			them._stats_texts[stat_name].skill:set_text("")
-			them._stats_texts[stat_name].equip:set_size(90, 20)
-			them._stats_texts[stat_name].equip:set_text("xxabcdefghijkzz")
-			them._stats_texts[stat_name].equip:set_color(Color(1, 0, 0))
+		if damage_e then
+			if damage_e > 9000 then
+				self:Set(them, "damage", "IT'S OVER 9000!!!!!", Color(1, 0, 0))
+			elseif damage_e > 4000 then
+				self:Set(them, "damage", "IT'S CLOSE TO 9000!!", Color(1, 1, 0))
+			end
 		end
 	end
 end
 
-function BlackMarketGui:HookReplaceWepStatsNumber2Words()
+function RWSN2W:Set(them, stat_name, txt, color)
+	if txt then
+		them._stats_texts[stat_name].base:set_text("")
+		them._stats_texts[stat_name].mods:set_text("")
+		them._stats_texts[stat_name].skill:set_text("")
+		them._stats_texts[stat_name].equip:set_text(txt)
+		them._stats_texts[stat_name].equip:set_size(200, 20)
+		them._stats_texts[stat_name].equip:parent():set_w(200)
+	end
+	if color then
+		them._stats_texts[stat_name].equip:set_color(color)
+	end
+end
+
+function BlackMarketGui:RWSN2W()
 	if type(Global.blackmarket_manager) == "table" and type(Global.blackmarket_manager.crafted_items) == "table" and type(self._stats_shown) == "table" and type(self._slot_data) == "table" and type(self._slot_data.slot) == "number" and type(self._slot_data.category) == "string" then
 		local craft = Global.blackmarket_manager.crafted_items
 		local slot = self._slot_data.slot
 		local cat = self._slot_data.category
 		if craft[cat] and craft[cat][slot] then			
 			for i, stat in pairs(self._stats_shown) do
-				RWSN2W:ReplaceWepStatsNumber2Words({
+				RWSN2W:Run({
 					weapon_data = {
 						weapon_id = craft[cat][slot].weapon_id,
 						factory_id = craft[cat][slot].factory_id,
@@ -64,17 +77,17 @@ function BlackMarketGui:HookReplaceWepStatsNumber2Words()
 end
 
 Hooks:PostHook(BlackMarketGui, "show_stats", "RWSN2W_PostHook_show_stats", function(self)
-	self:HookReplaceWepStatsNumber2Words()
+	self:RWSN2W(1)
 end)
 
 Hooks:PostHook(BlackMarketGui, "_update_borders", "RWSN2W_PostHook_update_borders", function(self)
-	self:HookReplaceWepStatsNumber2Words()
+	self:RWSN2W(2)
 end)
 
 Hooks:PostHook(BlackMarketGui, "update_info_text", "RWSN2W_PostHook_set_info_text", function(self)
-	self:HookReplaceWepStatsNumber2Words()
+	self:RWSN2W(3)
 end)
 
 Hooks:PostHook(BlackMarketGui, "_setup", "RWSN2W_PostHook_setup", function(self)
-	self:HookReplaceWepStatsNumber2Words()
+	self:RWSN2W(4)
 end)
