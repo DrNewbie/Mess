79c45fb38cee1f9b0713987e97e635767984f3fe
Dr_Newbie <tf2baidonation@gmail.com>
2019-07-20 21:52:15 +0800

    Jump and Duck in Casing Mode[2]

--- mods/Jump and Duck in Casing Mode/Lua/hook.playermaskoff.lua
+++ mods/Jump and Duck in Casing Mode/Lua/hook.playermaskoff.lua
@@ -13,36 +13,3 @@ function PlayerMaskOff:_check_action_duck(t, input)
 		end
 	end
 end
-
-function PlayerMaskOff:_update_check_actions(t, dt)
-	local input = self:_get_input(t, dt)
-	self._stick_move = self._controller:get_input_axis("move")
-
-	if mvector3.length(self._stick_move) < 0.1 or self:_interacting() then
-		self._move_dir = nil
-	else
-		self._move_dir = mvector3.copy(self._stick_move)
-		local cam_flat_rot = Rotation(self._cam_fwd_flat, math.UP)
-		mvector3.rotate_with(self._move_dir, cam_flat_rot)
-	end
-
-	self:_update_interaction_timers(t)
-	self:_update_start_standard_timers(t)
-
-	if input.btn_stats_screen_press then
-		self._unit:base():set_stats_screen_visible(true)
-	elseif input.btn_stats_screen_release then
-		self._unit:base():set_stats_screen_visible(false)
-	end
-
-	self:_update_foley(t, input)
-
-	local new_action = nil
-	new_action = new_action or self:_check_use_item(t, input)
-	new_action = new_action or self:_check_action_interact(t, input)
-
-	self:_check_action_jump(t, input)
-	self:_check_action_duck(t, input)
-	self:_check_action_run(t, input)
-	self:_check_action_change_equipment(t, input)
-end
