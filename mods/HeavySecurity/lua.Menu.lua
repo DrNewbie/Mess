a88039d23e0d4dfd1ab258c594fc45ab2bfa4097
Dr_Newbie <tf2baidonation@gmail.com>
2019-08-17 15:29:37 +0800

    HeavySecurity[27]
    https://tinyurl.com/y4k6v85f ;
    https://youtu.be/fqYi8quNTLo ;

--- mods/HeavySecurity/lua.Menu.lua
+++ mods/HeavySecurity/lua.Menu.lua
@@ -9,22 +9,30 @@ HeavySecurity = HeavySecurity or {
 	settings = {}	
 }
 
-function HeavySecurity:Reset()
-	self.settings = {
+function HeavySecurity:Default()
+	return {
 		Level = 1,
 		Enemy_Type = 1,
 		Enable = true,
 		AllEnemies = false,
-		AllCivilians = false
+		AllCivilians = false,
+		AddonFollower = 0
 	}
+end
+
+function HeavySecurity:Reset()
+	self.settings = self:Default()
 	self:Save()
 end
 
 function HeavySecurity:Load()
 	local file = io.open(self.SaveFile, "r")
 	if file then
+		self.settings = self:Default()
 		for key, value in pairs(json.decode(file:read("*all"))) do
-			self.settings[key] = value
+			if type(value) == type(self:Default()[key]) then
+				self.settings[key] = value
+			end
 		end
 		file:close()
 	else
@@ -42,7 +50,7 @@ function HeavySecurity:Save()
 	end
 end
 
-function HeavySecurity:Spawn(_cop, them)
+function HeavySecurity:Spawn(_cop, them, special_target)
 	local _spawn_list = {
 		"units/payday2/characters/ene_bulldozer_1/ene_bulldozer_1",
 		"units/payday2/characters/ene_bulldozer_2/ene_bulldozer_2",
@@ -75,40 +83,50 @@ function HeavySecurity:Spawn(_cop, them)
 	if self.settings.Enemy_Type == 12 then
 		_spawn_select = _cop:name()
 	end
-	for i = 1, self.settings.Level do
-		if self.settings.Enemy_Type == 9 then
-			if math.random() > 0.5 then
-				_spawn_select = Idstring("units/payday2/characters/ene_sniper_1/ene_sniper_1")
-			else
-				_spawn_select = Idstring("units/payday2/characters/ene_sniper_2/ene_sniper_2")			
-			end
-		elseif self.settings.Enemy_Type == 5 then
-			if math.random() > 0.5 then
-				_spawn_select = Idstring("units/payday2/characters/ene_shield_1/ene_shield_1")
-			else
-				_spawn_select = Idstring("units/payday2/characters/ene_shield_2/ene_shield_2")			
-			end
-		elseif self.settings.Enemy_Type == 11 then
-			_spawn_select = Idstring(_spawn_list[math.random(#_spawn_list)])
+	if self.settings.Enemy_Type == 9 then
+		if math.random() > 0.5 then
+			_spawn_select = Idstring("units/payday2/characters/ene_sniper_1/ene_sniper_1")
+		else
+			_spawn_select = Idstring("units/payday2/characters/ene_sniper_2/ene_sniper_2")			
 		end
-		_u = safe_spawn_unit(_spawn_select, them:get_orientation())
-		if _u then
-			if not team_id then
-				team_id = tweak_data.levels:get_default_team_ID(_u:base():char_tweak().access == "gangster" and "gangster" or "combatant")
-			end
-			if them._values.participate_to_group_ai then
-				managers.groupai:state():assign_enemy_to_group_ai(_u, team_id)
-			else
-				managers.groupai:state():set_char_team(_u, team_id)
-			end
-			_u:brain():set_spawn_ai( { init_state = "idle", params = { scan = true }, objective = new_objective } )
-			_u:brain():on_reload()
-			_u:brain():terminate_all_suspicion()
-			_u:movement():set_cool(true)
-			_u:brain():on_cool_state_changed(true)
-			managers.groupai:state():_clear_criminal_suspicion_data()
+	elseif self.settings.Enemy_Type == 5 then
+		if math.random() > 0.5 then
+			_spawn_select = Idstring("units/payday2/characters/ene_shield_1/ene_shield_1")
+		else
+			_spawn_select = Idstring("units/payday2/characters/ene_shield_2/ene_shield_2")			
 		end
+	elseif self.settings.Enemy_Type == 11 then
+		_spawn_select = Idstring(_spawn_list[math.random(#_spawn_list)])
 	end
+	if special_target then
+		local _pos_offset = function ()
+			local ang = math.random() * 360 * math.pi
+			local rad = math.random(3, 5)
+			rad = rad * 800
+			return Vector3(math.cos(ang) * rad, math.sin(ang) * rad, 0)
+		end
+		_u = safe_spawn_unit(_spawn_select, special_target:position() + _pos_offset(), special_target:rotation())
+		new_objective.follow_unit = special_target
+	else
+		_u = safe_spawn_unit(_spawn_select, them:get_orientation())
+	end	
+	if _u then
+		if not team_id then
+			team_id = tweak_data.levels:get_default_team_ID(_u:base():char_tweak().access == "gangster" and "gangster" or "combatant")
+		end
+		if them._values.participate_to_group_ai then
+			managers.groupai:state():assign_enemy_to_group_ai(_u, team_id)
+		else
+			managers.groupai:state():set_char_team(_u, team_id)
+		end
+		_u:brain():set_spawn_ai( { init_state = "idle", params = { scan = true }, objective = new_objective } )
+		_u:brain():on_reload()
+		_u:brain():terminate_all_suspicion()
+		_u:movement():set_cool(true)
+		_u:brain():on_cool_state_changed(true)
+		managers.groupai:state():_clear_criminal_suspicion_data()
+	end
+	return _u
 end
 
 Hooks:Add("LocalizationManagerPostInit", "HeavySecurity_loc", function(Loc)
@@ -133,6 +151,9 @@ Hooks:Add("MenuManagerInitialize", "MenuManagerInitialize_HeavySecurityOptions",
 	end
 	MenuCallbackHandler.HeavySecurity_menu_apply2allcivilians_callback = function(_, item)
 		HeavySecurity.settings.AllCivilians = tostring(item:value()) == 'on' and true or false
+	end
+	MenuCallbackHandler.HeavySecurity_menu_addonfollower_callback = function(_, item)
+		HeavySecurity.settings.AddonFollower = math.floor(item:value())
 	end
 	MenuHelper:LoadFromJsonFile(HeavySecurity.ModPath.."menu.Main.json", HeavySecurity, HeavySecurity.settings)
 end)
--- mods/HeavySecurity/lua.elementspawncivilian.lua
+++ mods/HeavySecurity/lua.elementspawncivilian.lua
@@ -1,15 +1,15 @@
 core:import("CoreMissionScriptElement")
 ElementSpawnCivilian = ElementSpawnCivilian or class(CoreMissionScriptElement.MissionScriptElement)
 
-if not HeavySecurity or not HeavySecurity.settings or not HeavySecurity.settings.Level or not HeavySecurity.settings.Enable then
-	return
-end
-
 Hooks:PostHook(ElementSpawnCivilian, "produce", "F_"..Idstring("Post.ElementSpawnCivilian.produce.HeavySecurity"):key(), function(self, params)
-	if not Network:is_client() and managers.groupai:state():whisper_mode() and self._units and self._units[#self._units] then
-		_cop = self._units[#self._units]
-		if HeavySecurity.settings.AllCivilians and managers.enemy:is_civilian(_cop) then
-			HeavySecurity:Spawn(_cop, self)
+	if not HeavySecurity or not HeavySecurity.settings or not HeavySecurity.settings.Level or not HeavySecurity.settings.Enable then
+	
+	else
+		if not Network:is_client() and managers.groupai:state():whisper_mode() and self._units and self._units[#self._units] then
+			_cop = self._units[#self._units]
+			if HeavySecurity.settings.AllCivilians and managers.enemy:is_civilian(_cop) then
+				HeavySecurity:Spawn(_cop, self)
+			end
 		end
 	end
 end)
