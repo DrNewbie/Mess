4be3ec0050256c098ae00c2169b2afe4838b3ed1
Dr_Newbie <tf2baidonation@gmail.com>
2019-08-25 01:41:20 +0800

    Bain warn you about dead people
    http://modwork.shop/25533

---
+++ mods/Bain warn you about dead people/playermanager.lua
@@ -0,0 +1,3 @@
+Hooks:PostHook(PlayerManager, "on_killshot", "F_"..Idstring("PlayerManager:on_killshot:OhNOOOOOOOOOOOOXDDDDDDDDDDDDDDD"):key(), function(self)
+	managers.dialog:queue_narrator_dialog("c01", {delay = (math.random()*3)})
+end)
