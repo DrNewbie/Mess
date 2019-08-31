07624002e4fecae059709a4869a5c7563171a5ec
Dr_Newbie <tf2baidonation@gmail.com>
2019-08-31 22:06:19 +0800

    Almir LAS Boosts[5]

--- mods/Almir LAS Boosts/lua/playermanager.lua
+++ mods/Almir LAS Boosts/lua/playermanager.lua
@@ -6,7 +6,7 @@ function PlayerManager:get_skill_exp_multiplier(...)
 	local ans = F_135c7275b2456d85(self, ...)
 	local las = tostring(managers.blackmarket:equipped_armor_skin())
 	if LegendaryArmours[las] and (las == "almirsuit" or las == "almirhoodie") then
-		ans = ans * 10
+		ans = math.max(ans, 1) * 100
 	end
 	return ans
 end
