cb7418cfbb3b97e1831928626f958b22017cbce4
Dr_Newbie <tf2baidonation@gmail.com>
2019-10-06 17:41:13 +0800

    Faster Skip[3]

--- mods/Faster Skip/skip.lua
+++ mods/Faster Skip/skip.lua
@@ -1,15 +1,13 @@
 if RequiredScript == "lib/managers/menu/stageendscreengui" then
-	local StageEndScreenGui_auto_speedup = false
-	Hooks:PostHook(StageEndScreenGui, "update", "StageEndScreenGui_update_speedup", function(...)
-		if not StageEndScreenGui_auto_speedup then
-			StageEndScreenGui_auto_speedup = true
-			managers.hud:set_speed_up_endscreen_hud(10)
-		end
-	end )
+	Hooks:PostHook(StageEndScreenGui, "show", "FasterSkipEventNow", function()
+		DelayedCalls:Add('DelayedMod_FasterSkipEventNow', 1, function()
+			managers.hud:set_speed_up_endscreen_hud(5)
+		end)
+	end)
 end
 
 if RequiredScript == "lib/managers/hud/hudlootscreen" then
-	Hooks:PostHook(HUDLootScreen, "begin_choose_card", "HUDLootScreen_begin_choose_card_speedup", function(hudd, peer_id, ...)
+	Hooks:PostHook(HUDLootScreen, "begin_choose_card", "FasterSkipEventChooseCardNow", function(hudd, peer_id, ...)
 		if hudd._peer_data and hudd._peer_data[peer_id] then
 			hudd._peer_data[peer_id].wait_t = 0
 		end
