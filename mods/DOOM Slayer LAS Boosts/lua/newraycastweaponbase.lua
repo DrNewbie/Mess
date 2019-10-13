71e4612727c5f44432f73b7bb09ba1fefb225c8d
Dr_Newbie <tf2baidonation@gmail.com>
2019-10-13 14:27:20 +0800

    DOOM Slayer LAS Boosts[3]

--- mods/DOOM Slayer LAS Boosts/lua/newraycastweaponbase.lua
+++ mods/DOOM Slayer LAS Boosts/lua/newraycastweaponbase.lua
@@ -2,14 +2,22 @@ Hooks:PostHook(NewRaycastWeaponBase, "replenish", "F_"..Idstring("NewRaycastWeap
 	if managers.player:Is_LAS_DewmSlaya() then
 		local ammax = self:get_ammo_max() * 2
 		self:set_ammo_max(ammax)
-		self:set_ammo_max_per_clip(ammax)
+		if self:weapon_tweak_data().CLIP_AMMO_MAX == 2 and table.contains(self:weapon_tweak_data().categories, "shotgun") and self:weapon_tweak_data().upgrade_blocks and self:weapon_tweak_data().upgrade_blocks.weapon and table.contains(self:weapon_tweak_data().upgrade_blocks.weapon, "clip_ammo_increase") then
+			self._is_super_shotgun_type = true
+		else
+			self:set_ammo_max_per_clip(ammax)
+		end
 		self:set_ammo_total(ammax)
 		self._is_dewmslaya = true
 	end
 end)
 
 Hooks:PostHook(NewRaycastWeaponBase, "add_ammo", "F_"..Idstring("NewRaycastWeaponBase:add_ammo:DewmSlayaLASBoosts"):key(), function(self)
-	if self._unit and alive(self._unit) and managers.player:Is_LAS_DewmSlaya() and self:can_reload() then
-		self:on_reload()
+	if self._unit and alive(self._unit) and not self._is_super_shotgun_type and managers.player:Is_LAS_DewmSlaya() and self:can_reload() then
+		if self:clip_empty() then
+			self:on_reload()
+		else
+			self._ask_forced_reload = TimerManager:game():time() + 0.5
+		end
 	end
 end)
--- mods/DOOM Slayer LAS Boosts/lua/playerstandard.lua
+++ mods/DOOM Slayer LAS Boosts/lua/playerstandard.lua
@@ -1,7 +1,18 @@
 Hooks:PostHook(PlayerStandard, "_update_equip_weapon_timers", "F_"..Idstring("PlayerStandard:_update_equip_weapon_timers:DewmSlayaLASBoosts"):key(), function(self)
-	if managers.player:Is_LAS_DewmSlaya() and self._equipped_unit and alive(self._equipped_unit) and self._equipped_unit:base() and self._equipped_unit:base()._is_dewmslaya and not self._equipped_unit:base()._is_dewmslaya_init then
-		self._equipped_unit:base()._is_dewmslaya_init = true
-		self._equipped_unit:base():on_reload()
-		managers.hud:set_ammo_amount(self._equipped_unit:base():selection_index(), self._equipped_unit:base():ammo_info())
+	if managers.player:Is_LAS_DewmSlaya() and self._equipped_unit and alive(self._equipped_unit) and self._equipped_unit:base() and self._equipped_unit:base()._is_dewmslaya then
+		if not self._equipped_unit:base()._is_dewmslaya_init then
+			self._equipped_unit:base()._is_dewmslaya_init = true
+			self._equipped_unit:base():on_reload()
+			managers.hud:set_ammo_amount(self._equipped_unit:base():selection_index(), self._equipped_unit:base():ammo_info())
+		end
+		if self._equipped_unit:base()._ask_forced_reload then
+			if self._equipped_unit:base()._ask_forced_reload < TimerManager:game():time() then
+				self._equipped_unit:base()._ask_forced_reload = nil
+				if self._equipped_unit:base():can_reload() then
+					self._equipped_unit:base():on_reload()
+					managers.hud:set_ammo_amount(self._equipped_unit:base():selection_index(), self._equipped_unit:base():ammo_info())
+				end
+			end
+		end
 	end
 end)
