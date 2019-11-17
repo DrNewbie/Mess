c21b42e054da3ee9e43b8edbb5b6bd039088dae6
Dr_Newbie <tf2baidonation@gmail.com>
2019-11-17 21:07:34 +0800

    mod that automatically refill minigun ammo

--- mods/mod that automatically refill minigun ammo/lua/newraycastweaponbase.lua
+++ mods/mod that automatically refill minigun ammo/lua/newraycastweaponbase.lua
@@ -1,10 +1,12 @@
-Hooks:PostHook(NewRaycastWeaponBase, "replenish", "F_"..Idstring("NewRaycastWeaponBase:replenish:mod that automatically refill minigun gun ammo"):key(), function(self)
+Hooks:PostHook(NewRaycastWeaponBase, "replenish", "F_"..Idstring("NewRaycastWeaponBase:replenish:mod that automatically refill minigun ammo"):key(), function(self)
 	if table.contains(self:weapon_tweak_data().categories, "minigun") then
 		self._is_auto_refill_minigun = true
 	end
 end)
 
-Hooks:PostHook(NewRaycastWeaponBase, "add_ammo", "F_"..Idstring("NewRaycastWeaponBase:add_ammo:mod that automatically refill minigun gun ammo"):key(), function(self)
+local old_add_ammo = NewRaycastWeaponBase.add_ammo
+
+function NewRaycastWeaponBase:add_ammo(...)
 	if self._unit and alive(self._unit) and self._is_auto_refill_minigun and self:can_reload() then
 		if self:clip_empty() then
 			self:on_reload()
@@ -12,4 +14,5 @@ Hooks:PostHook(NewRaycastWeaponBase, "add_ammo", "F_"..Idstring("NewRaycastWeapo
 			self._auto_refill_minigun_ask_forced_reload = TimerManager:game():time() + 0.3
 		end
 	end
-end)
+	return old_add_ammo(self, ...)
+end
--- mods/mod that automatically refill minigun ammo/lua/playerstandard.lua
+++ mods/mod that automatically refill minigun ammo/lua/playerstandard.lua
@@ -1,4 +1,4 @@
-Hooks:PostHook(PlayerStandard, "_update_equip_weapon_timers", "F_"..Idstring("PlayerStandard:_update_equip_weapon_timers:mod that automatically refill minigun gun amm"):key(), function(self)
+Hooks:PostHook(PlayerStandard, "_update_equip_weapon_timers", "F_"..Idstring("PlayerStandard:_update_equip_weapon_timers:mod that automatically refill minigun ammo"):key(), function(self)
 	if self._equipped_unit and alive(self._equipped_unit) and self._equipped_unit:base() and self._equipped_unit:base()._is_auto_refill_minigun and self._equipped_unit:base()._auto_refill_minigun_ask_forced_reload and self._equipped_unit:base()._auto_refill_minigun_ask_forced_reload < TimerManager:game():time() then
 		self._equipped_unit:base()._auto_refill_minigun_ask_forced_reload = nil
 		if self._equipped_unit:base():can_reload() then
