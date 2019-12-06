f8a15820056383eb74f40a0b9d563c67edb9c174
Dr_Newbie <tf2baidonation@gmail.com>
2019-12-06 22:11:02 +0800

    .

--- mods/Recently played with/Lua/Base.lua
+++ mods/Recently played with/Lua/Base.lua
@@ -1,7 +1,33 @@
 _G.RPwith = _G.RPwith or {}
 
-RPwith.Ban_list_SteamID64 = RPwith.Ban_list_SteamID64 or {}
 RPwith.ModPath = RPwith.ModPath or ModPath
+
+function RPwith:format_time(timestamp, format, tzoffset, tzname)
+	if tzoffset == "local" then  -- calculate local time zone (for the server)
+		local now = os.time()
+		local local_t = os.date("*t", now)
+		local utc_t = os.date("!*t", now)
+		local delta = (local_t.hour - utc_t.hour)*60 + (local_t.min - utc_t.min)
+		local h, m = math.modf( delta / 60)
+		tzoffset = string.format("%+.4d", 100 * h + 60 * m)
+	end
+	tzoffset = tzoffset or "GMT"
+	format = format:gsub("%%z", tzname or tzoffset)
+	if tzoffset == "GMT" then
+		tzoffset = "+0000"
+	end
+	tzoffset = tzoffset:gsub(":", "")
+
+	local sign = 1
+	if tzoffset:sub(1,1) == "-" then
+		sign = -1
+		tzoffset = tzoffset:sub(2)
+	elseif tzoffset:sub(1,1) == "+" then
+		tzoffset = tzoffset:sub(2)
+	end
+	tzoffset = sign * (tonumber(tzoffset:sub(1,2))*60 + tonumber(tzoffset:sub(3,4)))*60
+	return os.date(format, timestamp + tzoffset)
+end
 
 function RPwith:Record_History(peer_id)
 	if not managers.network or not managers.network:session() then
