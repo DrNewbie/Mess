f705c662ea25e167648ea0fda42010acedcfe7cc
Dr_Newbie <tf2baidonation@gmail.com>
2019-12-19 02:02:55 +0800

    OverBurn , 4 , use addon spawn
    fix some bugs

--- mods/OverBurnFWB/hook.coreworlddefinition.lua
+++ mods/OverBurnFWB/hook.coreworlddefinition.lua
@@ -14,30 +14,20 @@ local OverBurn_WorldDefinition_make_unit = WorldDefinition.make_unit
 local OverBurnMoneyAreaCenter = Vector3(7726, 1274, -300)
 local OverBurnMoneyAreaRange = 1000
 
-local is_editor = Application:editor()
-
-function WorldDefinition:make_unit(data, offset, ...)
+Hooks:PostHook(WorldDefinition, "make_unit", "F_"..Idstring("PostHook:WorldDefinition:make_unit:OverBurn"):key(), function(self, data, offset, ...)
 	if not Global.game_settings or Global.game_settings.level_id ~= "red2" then
 	
 	else
 		local old_name = tostring(data.name)
 		if old_name == "units/payday2/pickups/gen_pku_money_multi/gen_pku_money_multi" or old_name == "units/world/props/bank/money_wrap/money_wrap_single_bundle" then
 			if data.position and mvector3.distance(data.position, OverBurnMoneyAreaCenter) < OverBurnMoneyAreaRange then
-				data.name = "units/payday2/pickups/gen_pku_gold/gen_pku_gold"
-				if old_name == "units/world/props/bank/money_wrap/money_wrap_single_bundle" then
-					local s_unit = nil
-					if MassUnitManager:can_spawn_unit(Idstring(data.name)) and not is_editor then
-						s_unit = MassUnitManager:spawn_unit(Idstring(data.name), data.position + offset, data.rotation)
-					else
-						s_unit = CoreUnit.safe_spawn_unit(data.name, data.position, data.rotation)
-					end
-					if s_unit and s_unit.interaction and s_unit:interaction() then
-						s_unit:interaction():set_active(true, true)
-					end
-					data.name = old_name
+				local s_unit = nil
+				local _rotation = Rotation(math.random(-180, 180), 0, 0)
+				s_unit = CoreUnit.safe_spawn_unit(Idstring("units/payday2/pickups/gen_pku_gold/gen_pku_gold"), data.position, _rotation)
+				if s_unit then
+					s_unit:interaction():set_active(true, true)
 				end
 			end
 		end
 	end
-	return OverBurn_WorldDefinition_make_unit(self, data, offset, ...)
-end
+end)
