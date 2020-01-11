e585e81f8d84d1c2e67640060bb4415440dcd6c0
Dr_Newbie <tf2baidonation@gmail.com>
2020-01-11 14:48:41 +0800

    .

--- mods/Hoxton Breakout Xmas Lite/levelstweakdata.lua
+++ mods/Hoxton Breakout Xmas Lite/levelstweakdata.lua
@@ -3,5 +3,11 @@ Hooks:PostHook(LevelsTweakData, "init", "F_"..Idstring("PostHook:LevelsTweakData
 	self.hox_1.load_screen = "guis/dlcs/xmn/textures/loading/job_hox_1_xmn_df"
 	self.hox_1.music_overrides = {
 		track_20 = "track_66"
+	}	
+	self.hox_2.load_screen = "guis/dlcs/pic/textures/loading/job_breakout_02"
+	self.hox_2.world_name = "narratives/dentist/hox/stage_2_xmn"
+	self.hox_2.load_screen = "guis/dlcs/xmn/textures/loading/job_hox_2_xmn_df"
+	self.hox_2.music_overrides = {
+		track_21 = "track_67"
 	}
 end)
