7d06b02a07e04f90c05e3b99ba72a72a860a5035
Dr_Newbie <tf2baidonation@gmail.com>
2020-01-22 21:24:54 +0800

    Meth Failure, less run

--- mods/Meth Failure (Host Only)/hostnetworksession.lua
+++ mods/Meth Failure (Host Only)/hostnetworksession.lua
@@ -21,7 +21,7 @@ local dialog_ids = {
 	}
 }
 local _possible_ids = {
-	_cids = nil,
+	_cids = {},
 	_init = nil,
 	_pids = {}
 }
@@ -29,25 +29,23 @@ local _possible_ids = {
 function HostNetworkSession:send_to_peers_synched(v1, v2, ...)
 	if managers.mission and type(v1) == "string" and match_name[v1] then
 		local _dialogue_id
-		_possible_ids._cids = nil
-		for _, script in pairs(managers.mission:scripts()) do
-			for _id, element in pairs(script:elements()) do
-				if element and element._values and element._values.dialogue and _id == v2 then
-					_dialogue_id = element._values.dialogue
-					if dialog_ids[1][_dialogue_id] then
-						_possible_ids._cids = 1
-					elseif dialog_ids[2][_dialogue_id] then
-						_possible_ids._cids = 2
-					elseif dialog_ids[3][_dialogue_id] then
-						_possible_ids._cids = 3
-					else
-						_possible_ids._cids = nil
+		local _v2_ids = Idstring(v2)
+		if not _possible_ids._cids[_v2_ids] then
+			for _, script in pairs(managers.mission:scripts()) do
+				for _id, element in pairs(script:elements()) do
+					if element and element._values and element._values.dialogue and _id == v2 then
+						_dialogue_id = element._values.dialogue
+						if dialog_ids[1][_dialogue_id] or dialog_ids[2][_dialogue_id] or dialog_ids[3][_dialogue_id] then
+							_possible_ids._cids[_v2_ids] = 1
+						else
+							_possible_ids._cids[_v2_ids] = -1
+						end
+						break
 					end
-					break
 				end
 			end
 		end
-		if _possible_ids._cids then
+		if type(_possible_ids._cids[_v2_ids]) == "number" and _possible_ids._cids[_v2_ids] == 1 then
 			if not _possible_ids._init then
 				_possible_ids._init = true
 				for _, script in pairs(managers.mission:scripts()) do
