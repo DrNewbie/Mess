fccf4a8d89946ac291148db1bc82e00d1a0e2d88
Dr_Newbie <tf2baidonation@gmail.com>
2020-01-23 10:56:57 +0800

    Meth Failure (Meth Hinderer) (Host Only)
    https://modworkshop.net/mod/26446

--- mods/Meth Failure (Meth Hinderer) (Host Only)/hostnetworksession.lua
+++ mods/Meth Failure (Meth Hinderer) (Host Only)/hostnetworksession.lua
@@ -4,21 +4,17 @@ local match_name = {
 	["run_mission_element_no_instigator"] = true
 }
 local dialog_ids = {
-	{
-		["pln_rt1_20"] = "mu",
-		["pln_rt1_22"] = "cs",
-		["pln_rt1_24"] = "hcl"
-	},
-	{
-		["pln_rat_stage1_20"] = "mu",
-		["pln_rat_stage1_22"] = "cs",
-		["pln_rat_stage1_24"] = "hcl"
-	},
-	{
-		["Play_loc_mex_cook_03"] = "mu",
-		["Play_loc_mex_cook_04"] = "cs",
-		["Play_loc_mex_cook_05"] = "hcl"
-	}
+	["pln_rt1_20"] = "mu",
+	["pln_rt1_22"] = "cs",
+	["pln_rt1_24"] = "hcl",
+
+	["pln_rat_stage1_20"] = "mu",
+	["pln_rat_stage1_22"] = "cs",
+	["pln_rat_stage1_24"] = "hcl",
+
+	["Play_loc_mex_cook_03"] = "mu",
+	["Play_loc_mex_cook_04"] = "cs",
+	["Play_loc_mex_cook_05"] = "hcl"
 }
 local _possible_ids = {
 	_cids = {},
@@ -28,14 +24,13 @@ local _possible_ids = {
 
 function HostNetworkSession:send_to_peers_synched(v1, v2, ...)
 	if managers.mission and type(v1) == "string" and match_name[v1] then
-		local _dialogue_id
-		local _v2_ids = Idstring(v2)
+		local _v2_ids = 'ID_'..Idstring(tostring(v2)):key()
 		if not _possible_ids._cids[_v2_ids] then
 			for _, script in pairs(managers.mission:scripts()) do
 				for _id, element in pairs(script:elements()) do
-					if element and element._values and element._values.dialogue and _id == v2 then
-						_dialogue_id = element._values.dialogue
-						if dialog_ids[1][_dialogue_id] or dialog_ids[2][_dialogue_id] or dialog_ids[3][_dialogue_id] then
+					if element and element._values and element._values.dialogue and tostring(_id) == tostring(v2) then
+						local _dialogue_id = element._values.dialogue
+						if dialog_ids[_dialogue_id] then
 							_possible_ids._cids[_v2_ids] = 1
 						else
 							_possible_ids._cids[_v2_ids] = -1
@@ -51,8 +46,8 @@ function HostNetworkSession:send_to_peers_synched(v1, v2, ...)
 				for _, script in pairs(managers.mission:scripts()) do
 					for _id, element in pairs(script:elements()) do
 						if element and element._values and element._values.dialogue then
-							_dialogue_id = element._values.dialogue
-							if dialog_ids[1][_dialogue_id] or dialog_ids[2][_dialogue_id] or dialog_ids[3][_dialogue_id] then
+							local _dialogue_id = 'ID_'..Idstring(tostring(element._values.dialogue)):key()
+							if dialog_ids[_dialogue_id] then
 								_possible_ids._pids[_dialogue_id] = _id
 							end
 						end
