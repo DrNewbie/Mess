933bd634e6f20b6ad30593b46874c38aff90f226
Dr_Newbie <tf2baidonation@gmail.com>
2020-02-28 01:06:37 +0800

    .

--- mods/Block Spawn Turret/sentrygunbase.lua
+++ mods/Block Spawn Turret/sentrygunbase.lua
@@ -1,8 +1,5 @@
-local STOPTURRETFROMSPAWNPRODUCE = SentryGunBase.activate_as_module
-
-function SentryGunBase:activate_as_module(team_type, tweak_table_id, ...)
-	STOPTURRETFROMSPAWNPRODUCE(self, team_type, tweak_table_id, ...)
+Hooks:PostHook(SentryGunBase, "activate_as_module", 'F_'..Idstring("PostHook:SentryGunBase:activate_as_module:MOD THAT REMOVES TURRETS ACTUALLY"):key(), function(self, team_type, tweak_table_id)
 	if tostring(team_type) == "combatant" or tostring(tweak_table_id) == "swat_van_turret_module" then
 		self._unit:character_damage():disable(nil, "explosion")
 	end	
-end
+end)
