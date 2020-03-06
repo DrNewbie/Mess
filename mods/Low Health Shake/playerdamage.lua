4cb1894b8362b2a813e16d8e2d5fa4989949b5df
Dr_Newbie <tf2baidonation@gmail.com>
2020-03-06 22:31:59 +0800

    .

--- mods/Low Health Shake/playerdamage.lua
+++ mods/Low Health Shake/playerdamage.lua
@@ -1,13 +1,9 @@
-Hooks:PostHook(PlayerDamage, "update", "F_"..Idstring("PostHook:PlayerDamage:update:Low Health Shake"):key(), function(self, unit, t, dt)
+Hooks:PostHook(PlayerDamage, "update", "F_"..Idstring("PostHook:PlayerDamage:update:Low Health Shake"):key(), function(self)
 	if not self.__ext_camera then
 		self.__ext_camera = self._unit:camera()
-		if self.__ext_camera then
-			self.__hp_shake_number = self:health_ratio()
-		end
 	else
-		if self.__hp_shake_number ~= self:health_ratio() then
-			self.__hp_shake_number = self:health_ratio()
+		if self:health_ratio() then
+			self.__ext_camera:play_shaker("player_taser_shock", math.clamp((1 - self:health_ratio()), 0.001, 0.995)*3.5, 10)
 		end
-		self.__ext_camera:play_shaker("player_taser_shock", math.clamp((1 - self.__hp_shake_number), 0.001, 0.995)*3.5, 10)
 	end
 end)
