d5e47dde55849234d14e2bd3f64d54a6dd2c24ea
Dr_Newbie <tf2baidonation@gmail.com>
2020-03-09 00:22:16 +0800

    Low Health Shake ; 3

--- mods/Low Health Shake/playerdamage.lua
+++ mods/Low Health Shake/playerdamage.lua
@@ -2,10 +2,11 @@ Hooks:PostHook(PlayerDamage, "update", "F_"..Idstring("PostHook:PlayerDamage:upd
 	if not self.__ext_camera then
 		self.__ext_camera = self._unit:camera()
 	else
-		if self:health_ratio() < 0.3 and managers.player:upgrade_value("player", "max_health_reduction", 1) then
-			self.__ext_camera:play_shaker("player_taser_shock", math.clamp((0.3 - self:health_ratio()), 0.001, 0.295)*3.5, 10)
-		elseif self:health_ratio() < 1 and not managers.player:upgrade_value("player", "max_health_reduction", 1) then
-			self.__ext_camera:play_shaker("player_taser_shock", math.clamp((1 - self:health_ratio()), 0.001, 0.995)*3.5, 10)
+		if self:health_ratio() < self._max_health_reduction then
+			self.__ext_camera:play_shaker("player_taser_shock", 
+				math.clamp(
+					1 - (self:health_ratio()/self._max_health_reduction), 0.001, 0.995
+				)*3.5, 10)
 		end
 	end
 end)
