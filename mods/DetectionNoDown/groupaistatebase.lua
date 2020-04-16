82134e872897c3e99a27932066255f2f0d9e6061
Dr_Newbie <tf2baidonation@gmail.com>
2020-04-16 23:35:18 +0800

    Detection No Down

---
+++ mods/DetectionNoDown/groupaistatebase.lua
@@ -0,0 +1,58 @@
+Hooks:PostHook(GroupAIStateBase, "init", "F_"..Idstring("PostHook:GroupAIStateBase:init:Detection No Down"):key(), function(self)
+	if (Network and Network:is_server()) or Global.game_settings.single_player then
+		self.__suspicion_peek_dt = 0
+		self.__suspicion_peek_last = nil
+		self.__add_suspicion_amount = 0
+		self.__hold_suspicion_amount = 0
+	end
+end)
+
+function GroupAIStateBase:__set_add_suspicion_amount(__var)
+	self.__add_suspicion_amount = __var
+end
+
+function GroupAIStateBase:__get_add_suspicion_amount()
+	return self.__add_suspicion_amount
+end
+
+function GroupAIStateBase:__set_hold_suspicion_amount(__var)
+	self.__hold_suspicion_amount = __var
+end
+
+function GroupAIStateBase:__get_hold_suspicion_amount()
+	return self.__hold_suspicion_amount
+end
+
+Hooks:PostHook(GroupAIStateBase, "on_criminal_suspicion_progress", "F_"..Idstring("PostHook:GroupAIStateBase:on_criminal_suspicion_progress:Detection No Down"):key(), function(self, u_suspect, u_observer, status)
+	if (Network and Network:is_server()) or Global.game_settings.single_player then
+		local local_player = managers.player:local_player()
+		local __now = TimerManager:game():time()
+		local __is_suspicion = false
+		if type(u_suspect) == "userdata" and type(u_observer) == "userdata" and type(status) == "number" then
+			__is_suspicion = true
+		end
+		if __now - self.__suspicion_peek_dt > 0.5 then
+			if __is_suspicion then
+				if u_observer:base().is_security_camera then
+					self.__add_suspicion_amount = self.__add_suspicion_amount + 0.1
+				else
+					self.__add_suspicion_amount = self.__add_suspicion_amount + 0.25
+				end
+			end
+		end
+		if __is_suspicion and (not self.__suspicion_peek_last or self.__suspicion_peek_last ~= u_observer) then
+			self.__suspicion_peek_last = u_observer
+			self.__suspicion_peek_dt = -1
+		else
+			self.__suspicion_peek_dt = __now
+		end
+		local max_suspicion = 0
+		if local_player and local_player:movement() and local_player:movement()._suspicion then
+			for u_key, val in pairs(local_player:movement()._suspicion) do
+				if max_suspicion < val then
+					max_suspicion = val
+				end
+			end
+		end
+	end
+end)
---
+++ mods/DetectionNoDown/playermovement.lua
@@ -0,0 +1,33 @@
+Hooks:PostHook(PlayerMovement, "_calc_suspicion_ratio_and_sync", "F_"..Idstring("PostHook:PlayerMovement:_calc_suspicion_ratio_and_sync:Detection No Down"):key(), function(self, observer_unit, status)
+	if self._suspicion and type(self._suspicion_ratio) == "number" then
+		local max_suspicion = nil
+		for u_key, val in pairs(self._suspicion) do
+			if not max_suspicion or max_suspicion < val then
+				max_suspicion = val
+			end
+		end
+		if max_suspicion then
+			local __add_suspicion_amount = managers.groupai:state():__get_add_suspicion_amount()
+			local __hold_suspicion_amount = managers.groupai:state():__get_hold_suspicion_amount()
+			if __hold_suspicion_amount > max_suspicion then
+				max_suspicion = __hold_suspicion_amount
+			else
+				managers.groupai:state():__set_hold_suspicion_amount(max_suspicion)
+			end
+			max_suspicion = max_suspicion + __add_suspicion_amount
+			if max_suspicion >= 1.0 then
+				self:on_suspicion(observer_unit, true)
+				managers.groupai:state():on_criminal_suspicion_progress(self._unit, observer_unit, true)
+				managers.groupai:state():criminal_spotted(self._unit)
+				managers.groupai:state():__set_add_suspicion_amount(0)
+				managers.groupai:state():__set_hold_suspicion_amount(0)
+				if observer_unit.movement and observer_unit:movement() and observer_unit:movement().set_cool then
+					observer_unit:movement():set_cool(false)
+				elseif observer_unit:base() and observer_unit:base().is_security_camera and observer_unit:base()._sound_the_alarm then
+					observer_unit:base():_sound_the_alarm(self._unit)
+				end
+			end
+			self._suspicion_ratio = max_suspicion
+		end
+	end
+end)
