8213a39213e3b2f065468113167c0b54ee454d09
Dr_Newbie <tf2baidonation@gmail.com>
2020-04-28 00:31:51 +0800

    Sicario Perk Deck Buff ; 2

--- mods/Sicario Perk Deck Buff/Hooks/SmokeScreenEffectAlt.lua
+++ mods/Sicario Perk Deck Buff/Hooks/SmokeScreenEffectAlt.lua
@@ -1,7 +1,7 @@
 SmokeScreenEffectAlt = SmokeScreenEffectAlt or class(SmokeScreenEffect)
 
-function SmokeScreenEffectAlt:init(position, normal, time, has_dodge_bonus)
-	self._timer = time
+function SmokeScreenEffectAlt:init(position, normal, __time, has_dodge_bonus)
+	self._timer = TimerManager:game():time() + __time
 	self._position = position
 	self._radius = 400
 	self._unit_list = {}
@@ -21,21 +21,14 @@ function SmokeScreenEffectAlt:init(position, normal, time, has_dodge_bonus)
 end
 
 function SmokeScreenEffectAlt:update(t, dt)
-	if self._timer then
-		self._timer = self._timer - dt
-		if self._timer <= 2 then
-			World:effect_manager():fade_kill(self._effect)
-			if not self._sound_killed then
-				self._sound_source:post_event("lung_loop_end")
-				managers.enemy:add_delayed_clbk("SmokeScreenEffectAlt", callback(ProjectileBase, ProjectileBase, "_dispose_of_sound", {
-					sound_source = self._sound_source
-				}), TimerManager:game():time() + 4)
-				self._sound_killed = true
-			end
-		end
-		if self._timer <= 0 then
-			self._timer = nil
-		end
+	if self._timer and t > self._timer then
+		self._timer = nil
+		self._sound_killed = true
+		World:effect_manager():fade_kill(self._effect)
+		self._sound_source:post_event("lung_loop_end")
+		managers.enemy:add_delayed_clbk("SmokeScreenEffectAlt", callback(ProjectileBase, ProjectileBase, "_dispose_of_sound", {
+			sound_source = self._sound_source
+		}), t + 4)
 	end
 	self._unit_list = {}
 	local nearby_units = World:find_units_quick("sphere", self._position, self._radius, managers.slot:get_mask("persons"))
