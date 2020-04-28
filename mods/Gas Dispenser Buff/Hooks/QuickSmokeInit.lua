7f935558b21fe0f6a97d7191a438730c82e86aa9
Dr_Newbie <tf2baidonation@gmail.com>
2020-04-28 18:12:53 +0800

    Gas Dispenser Buff ; 3

--- mods/Gas Dispenser Buff/Hooks/QuickSmokeInit.lua
+++ mods/Gas Dispenser Buff/Hooks/QuickSmokeInit.lua
@@ -1,23 +1,3 @@
-Hooks:PostHook(PlayerManager, "add_grenade_amount", "F_"..Idstring("PostHook:PlayerManager:add_grenade_amount:Gas Dispenser Buff"):key(), function(self, __var)
-	if managers.blackmarket:equipped_grenade() ~= "tag_team" or type(__var) ~= "number" or __var >= 0 then
-	
-	else
-		local base_values = managers.player:upgrade_value("player", "tag_team_base")
-		if type(base_values) == "table" and type(base_values.duration) == "number" then
-			if self.__QuickSmokeHuskAlt and self.__QuickSmokeHuskAlt._timer then
-				self.__QuickSmokeHuskAlt._timer = -1
-				self.__QuickSmokeHuskAlt:update(TimerManager:game():time(), 0.1)
-				self.__QuickSmokeHuskAlt = nil
-			end
-			self.__QuickSmokeHuskAlt = SmokeScreenEffectAltAlt:new(self:player_unit():position(), math.UP, base_values.duration * 0.75)
-			if self.__QuickSmokeHuskAlt then
-				self._smoke_screen_effects = self._smoke_screen_effects or {}
-				table.insert(self._smoke_screen_effects, self.__QuickSmokeHuskAlt)
-			end
-		end
-	end
-end)
-
 Hooks:PostHook(PlayerManager, "update", "F_"..Idstring("PostHook:PlayerManager:update:Gas Dispenser Buff"):key(), function(self, t, dt)
 	if self.__QuickSmokeHuskAlt and self.__QuickSmokeHuskAlt.update then
 		if self:player_unit() then
@@ -37,6 +17,19 @@ function PlayerManager:_attempt_tag_team(...)
 		
 		else
 			self:add_coroutine("tag_team", PlayerAction.TagTeam, self:player_unit(), self:player_unit())
+			local base_values = managers.player:upgrade_value("player", "tag_team_base")
+			if type(base_values) == "table" and type(base_values.duration) == "number" then
+				if self.__QuickSmokeHuskAlt and self.__QuickSmokeHuskAlt._timer then
+					self.__QuickSmokeHuskAlt._timer = -1
+					self.__QuickSmokeHuskAlt:update(TimerManager:game():time(), 0.1)
+					self.__QuickSmokeHuskAlt = nil
+				end
+				self.__QuickSmokeHuskAlt = SmokeScreenEffectAltAlt:new(self:player_unit():position(), math.UP, base_values.duration * 0.75)
+				if self.__QuickSmokeHuskAlt then
+					self._smoke_screen_effects = self._smoke_screen_effects or {}
+					table.insert(self._smoke_screen_effects, self.__QuickSmokeHuskAlt)
+				end
+			end
 			return true
 		end
 	end
