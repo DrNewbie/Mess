ed18412a992d39d1c13476e5deba34e7c978b27c
Dr_Newbie <tf2baidonation@gmail.com>
2020-05-03 15:09:19 +0800

    Slot Machine Cheat: skm_cas

--- mods/Slot Machine Cheat/coreelementlogicchance.lua
+++ mods/Slot Machine Cheat/coreelementlogicchance.lua
@@ -2,9 +2,9 @@ core:module("CoreElementLogicChance")
 core:import("CoreMissionScriptElement")
 ElementLogicChance = ElementLogicChance or class(CoreMissionScriptElement.MissionScriptElement)
 
-if Global.level_data and Global.level_data.level_id and Global.level_data.level_id == "kenaz" then
-	Hooks:PreHook(ElementLogicChance, "on_executed", "fc53214492d3d608", function(self)
-		if self._editor_name == "1_chance" and (Global.level_data.level_id == "kenaz" or Global.level_data.level_id == "chill") then
+if Global.level_data and Global.level_data.level_id and (Global.level_data.level_id == "kenaz" or Global.level_data.level_id == "skm_cas") then
+	Hooks:PreHook(ElementLogicChance, "on_executed", "F_"..Idstring("PreHook:ElementLogicChance:on_executed:Slot Machine Cheat"):key(), function(self)
+		if self._editor_name == "1_chance" and (Global.level_data.level_id == "kenaz" or Global.level_data.level_id == "skm_cas") then
 			self._chance = 999
 		end
 	end)
