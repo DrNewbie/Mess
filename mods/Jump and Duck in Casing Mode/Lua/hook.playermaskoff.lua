10db2d5e71f1f0925c32bf5b06d24696de51ca47
Dr_Newbie <tf2baidonation@gmail.com>
2020-08-15 22:00:19 +0800

    Jump and Duck in Casing Mode [3]

--- mods/Jump and Duck in Casing Mode/Lua/hook.playermaskoff.lua
+++ mods/Jump and Duck in Casing Mode/Lua/hook.playermaskoff.lua
@@ -1,15 +1,23 @@
-function PlayerMaskOff:_check_action_jump(t, input)
+local mod_ids = Idstring('Jump and Duck in Casing Mode'):key()
+
+Hooks:PostHook(PlayerMaskOff, "_check_action_jump", 'F_'..Idstring("PostHook:PlayerMaskOff:_check_action_jump:"..mod_ids):key(), function(self, t, input)
 	if input.btn_jump_press then
 		PlayerStandard._check_action_jump(self, t, input)
 	end
-end
+end)
 
-function PlayerMaskOff:_check_action_duck(t, input)
-	if input.btn_duck_press then
-		if not self._state_data.ducking then
-			PlayerStandard._start_action_ducking(self, t)
-		elseif self._state_data.ducking then
-			PlayerStandard._end_action_ducking(self, t)
-		end
+Hooks:PostHook(PlayerMaskOff, "_check_action_duck", 'F_'..Idstring("PostHook:PlayerMaskOff:_check_action_duck:"..mod_ids):key(), function(self, t, input)
+	if input.btn_duck_release or input.btn_duck_press then
+		PlayerStandard._check_action_duck(self, t, input)
 	end
-end
+end)
+
+Hooks:PostHook(PlayerMaskOff, "_check_action_interact", 'F_'..Idstring("PostHook:PlayerMaskOff:_check_action_interact:"..mod_ids):key(), function(self)
+	return true
+end)
+
+Hooks:PostHook(PlayerMaskOff, "_upd_attention", 'F_'..Idstring("PostHook:PlayerMaskOff:_upd_attention:"..mod_ids):key(), function(self)
+	if self._state_data.ducking then
+		PlayerStandard._upd_attention(self)
+	end
+end)
