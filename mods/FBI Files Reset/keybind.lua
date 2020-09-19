245fe140dd4006ecffd1bc0c590187c00a82c042
Dr_Newbie <tf2baidonation@gmail.com>
2020-09-19 21:46:11 +0800

    FBIFilesReset[2]

--- mods/FBI Files Reset/keybind.lua
+++ mods/FBI Files Reset/keybind.lua
@@ -1,6 +1,8 @@
-local __list = {
-}
+local __list = {}
+local __bo_list = {}
+
 if Steam and Steam:sa_handler() then
+	local sa_handler = Steam:sa_handler()
 	local level_list, job_list, mask_list, weapon_list, melee_list, grenade_list, enemy_list, armor_list, character_list, deployable_list, suit_list, weapon_color_list = tweak_data.statistics:statistics_table()
 	table.insert(__list, "gadget_used_ammo_bag")
 	table.insert(__list, "gadget_used_trip_mine")
@@ -33,18 +35,13 @@ if Steam and Steam:sa_handler() then
 	for _, __name in pairs(character_list) do
 		table.insert(__list, "character_used_"..__name)
 	end
-	--[[
 	for _, __name in pairs(suit_list) do
 		table.insert(__list, "suit_used_"..__name)
 	end
+	--[[
 	for _, __name in pairs(glove_list) do
 		table.insert(__list, "gloves_used_"..__name)
 	end
-	]]
-	--[[
-	table.insert(__list, "heist_dropin")
-	table.insert(__list, "heist_failed")
-	table.insert(__list, "heist_success")
 	]]
 	for _, __name in pairs(enemy_list) do
 		table.insert(__list, "enemy_kills_"..__name)
@@ -61,21 +58,26 @@ if Steam and Steam:sa_handler() then
 		table.insert(__list, "contract_"..__name.."_win_dropin")
 		table.insert(__list, "contract_"..__name.."_fail")
 	end
+	table.insert(__list, "heist_dropin")
+	table.insert(__list, "heist_failed")
+	table.insert(__list, "heist_success")
 	]]
 	for _, __api_name in pairs(__list) do
-		if Steam:sa_handler():has_stat(__api_name) then
-			Steam:sa_handler():set_stat(__api_name, 0)
-			log("[FBI Files Reset]: Clean.API.Name: "..tostring(__api_name))
-		else
-			log("[FBI Files Reset]: Error.API.Name: "..tostring(__api_name))
+		if not __bo_list[__api_name] then
+			__bo_list[__api_name] = true
+			if sa_handler:has_stat(__api_name) then
+				if sa_handler:get_stat(__api_name) > 0 then
+					local __res = sa_handler:set_stats(__api_name, 0)
+					log("[FBI Files Reset]: Clean.API.Name: "..tostring(__api_name).." ; Server.Respond: "..tostring(__res))
+				else
+					log("[FBI Files Reset]: Zero.API.Name: "..tostring(__api_name))
+				end
+			else
+				log("[FBI Files Reset]: Error.API.Name: "..tostring(__api_name))
+			end
 		end
 	end
-	Steam:sa_handler():store_data()
-	--[[
-	if SystemInfo:distribution() == Idstring("STEAM") then
-		managers.statistics:publish_level_to_steam()
-	end
-	]]
+	sa_handler:store_data()
 	log("[FBI Files Reset]: Set.Done: "..tostring(math.random()))
 	QuickMenu:new(
 		"[FBI Files Reset]",
