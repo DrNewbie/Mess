f64e5c476365d9efeed08b748475c147f749034f
Dr_Newbie <tf2baidonation@gmail.com>
2020-10-16 23:56:12 +0800

    Hell's Island Less Light
    https://modworkshop.net/mod/29454

---
+++ mods/Hells Island Less Light/Core.lua
@@ -0,0 +1,18 @@
+core:module("CoreWorldDefinition")
+core:import("CoreUnit")
+core:import("CoreMath")
+core:import("CoreEditorUtils")
+core:import("CoreEngineAccess")
+WorldDefinition = WorldDefinition or class()
+
+if Global.load_level == true and Global.level_data.level_id == "bph" then
+	local old_make_unit = WorldDefinition.make_unit
+	function WorldDefinition:make_unit(data, ...)
+		if Global.level_data.level_id == "bph" and tostring(data.continent) == "lights2" then			
+			if tostring(__name):find("dlc") or data.projection_textures then
+				return nil
+			end
+		end
+		return old_make_unit(self, data, ...)
+	end
+end
