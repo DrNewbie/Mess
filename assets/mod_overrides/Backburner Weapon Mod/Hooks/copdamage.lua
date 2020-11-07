e9be3b869e17cb33c3ae1536973691bfa33ce257
Dr_Newbie <tf2baidonation@gmail.com>
2020-11-07 20:37:24 +0800

    Backburner Weapon Mod[4]

--- assets/mod_overrides/Backburner Weapon Mod/Hooks/copdamage.lua
+++ assets/mod_overrides/Backburner Weapon Mod/Hooks/copdamage.lua
@@ -1,17 +1,14 @@
-local BackBurnerFunc = BackBurnerFunc or CopDamage.roll_critical_hit
-
-function CopDamage:roll_critical_hit(attack_data, ...)
-	if attack_data.damage and attack_data.variant and attack_data.variant == "fire" and attack_data.attacker_unit and attack_data.attacker_unit == managers.player:player_unit() and attack_data.weapon_unit then
-		if attack_data.weapon_unit.base and attack_data.weapon_unit:base()._is_backburner then
-			local damage = attack_data.damage
-			local mvec3_dot = mvector3.dot			
-			local hit_dir = self._unit:position() - attack_data.attacker_unit:position()
-			local from_behind = mvec3_dot(hit_dir, self._unit:rotation():y()) >= 0
-			if from_behind then
-				damage =  damage * self._char_tweak.headshot_dmg_mul * 1.5
-				return true, damage
-			end
+Hooks:PreHook(CopDamage, "damage_fire", "F_"..Idstring("BackBurnerFunc_damage_fire"):key(), function(self, attack_data)
+	if not attack_data.__backburner_bonus and attack_data.weapon_unit and attack_data.weapon_unit.base and attack_data.weapon_unit:base()._is_backburner then
+		local mvec3_dot = mvector3.dot			
+		local hit_dir = self._unit:position() - attack_data.attacker_unit:position()
+		local from_behind = mvec3_dot(hit_dir, self._unit:rotation():y()) >= 0
+		if from_behind then
+			local critical_hits = self._char_tweak.critical_hits or {}
+			local critical_damage_mul = critical_hits.damage_mul or 1.5
+			attack_data.damage = attack_data.damage * critical_damage_mul
+			attack_data.__backburner_bonus = true
+			managers.hud:on_crit_confirmed()
 		end
 	end
-	return BackBurnerFunc(self, attack_data, ...)
-end
+end)
--- assets/mod_overrides/Backburner Weapon Mod/Hooks/newflamethrowerbase.lua
+++ assets/mod_overrides/Backburner Weapon Mod/Hooks/newflamethrowerbase.lua
@@ -1,4 +1,4 @@
-Hooks:PostHook(NewFlamethrowerBase, "setup_default", "BackBurnerFuncInit", function(self)
+Hooks:PostHook(NewFlamethrowerBase, "setup_default", "F_"..Idstring("BackBurnerFuncInit_setup_default"):key(), function(self)
 	if type(self._blueprint) == "table" and table.contains(self._blueprint, "wpn_fps_backburner") then
 		self._is_backburner = true
 	end
