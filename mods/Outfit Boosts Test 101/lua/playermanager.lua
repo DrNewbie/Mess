b8e860463df12063bea81fc546840708151e7314
Dr_Newbie <tf2baidonation@gmail.com>
2020-11-14 16:57:27 +0800

    Outfit Boosts Test 101[6]

--- mods/Outfit Boosts Test 101/lua/playermanager.lua
+++ mods/Outfit Boosts Test 101/lua/playermanager.lua
@@ -1,10 +1,12 @@
-local old_get_skill_exp_multiplier = PlayerManager.get_skill_exp_multiplier
+local old_get_infamy_exp_multiplier = PlayerManager.get_infamy_exp_multiplier
 
-function PlayerManager:get_skill_exp_multiplier(...)
-	local ans1 = old_get_skill_exp_multiplier(self, ...)
+function PlayerManager:get_infamy_exp_multiplier(...)
+	local ans1 = old_get_infamy_exp_multiplier(self, ...)
 	local las = tostring(managers.blackmarket:equipped_player_style())
-	if type(ans1) == "number" and Global.level_data and Global.level_data.level_id and Global.level_data.level_id == "dinner" and las == "slaughterhouse" then
-		ans1 = math.max(ans1, 1) * 10
+	if type(ans1) == "number" and Global.level_data and Global.level_data.level_id then
+		if Global.level_data.level_id == "dinner" and las == "slaughterhouse" then
+			ans1 = math.max(ans1, 1) + 100
+		end
 	end
 	return ans1
 end
--- mods/Outfit Boosts Test 101/lua/playerstyletweakdata.lua
+++ mods/Outfit Boosts Test 101/lua/playerstyletweakdata.lua
@@ -1,5 +1,5 @@
 Hooks:Add("LocalizationManagerPostInit", "Loc_"..Idstring("Outfit Boosts Test 101"):key(), function(loc)
 	loc:add_localized_strings({
-		["bm_suit_slaughterhouse_desc"] = loc:text("bm_suit_slaughterhouse_desc").."\n\n".."[+] You gain 10 times more XP from Slaughterhouse Hesit."
+		["bm_suit_slaughterhouse_desc"] = loc:text("bm_suit_slaughterhouse_desc").."\n\n".."[+] You gain 10 times more XP from Slaughterhouse Heist."
 	})
 end)
