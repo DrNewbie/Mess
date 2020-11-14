32e0cfceaea06ec2b0e3b717c53a5de4312204af
Dr_Newbie <tf2baidonation@gmail.com>
2020-11-14 16:59:32 +0800

    Update playermanager.lua

--- mods/Outfit Boosts Test 101/lua/playermanager.lua
+++ mods/Outfit Boosts Test 101/lua/playermanager.lua
@@ -5,7 +5,7 @@ function PlayerManager:get_infamy_exp_multiplier(...)
 	local las = tostring(managers.blackmarket:equipped_player_style())
 	if type(ans1) == "number" and Global.level_data and Global.level_data.level_id then
 		if Global.level_data.level_id == "dinner" and las == "slaughterhouse" then
-			ans1 = math.max(ans1, 1) + 100
+			ans1 = math.max(ans1, 1) + 10
 		end
 	end
 	return ans1
