9c17f05fc7759063c48148a0909790965d1d52ab
Dr_Newbie <tf2baidonation@gmail.com>
2020-12-29 01:47:02 +0800

    Symmetra[5]

--- mods/Symmetra/lua/playerequipment.lua
+++ mods/Symmetra/lua/playerequipment.lua
@@ -1,36 +1,82 @@
-function PlayerEquipment:valid_look_at_placement(equipment_data)
-	local from = self._unit:movement():m_head_pos()
-	local to = from + self._unit:movement():m_head_rot():y() * 200
-	local ray = self._unit:raycast("ray", from, to, "slot_mask", managers.slot:get_mask("trip_mine_placeables"), "ignore_unit", {}, "ray_type", "equipment_placement")
+local old_func1 = "F_"..Idstring("Symmetra:valid_look_at_placement"):key()
+local old_func2 = "F_"..Idstring("Symmetra:use_sentry_gun"):key()
 
-	if ray and equipment_data and equipment_data.dummy_unit then
-		local pos = ray.position
-		local rot = self._unit:movement():m_head_rot()
-		if equipment_data.use_function_name == "use_sentry_gun" then
-			local rot_pitch = math.round(rot:pitch())
-			if rot_pitch < 66 and rot_pitch > -66 then
-				rot_pitch = 90
-			elseif rot_pitch >= 66 then
-				rot_pitch = 180
-			else
-				rot_pitch = 0
+PlayerEquipment[old_func1] = PlayerEquipment[old_func1] or PlayerEquipment.valid_look_at_placement
+PlayerEquipment[old_func2] = PlayerEquipment[old_func2] or PlayerEquipment.use_sentry_gun
+
+function PlayerEquipment:valid_look_at_placement(equipment_data, ...)
+	equipment_data = equipment_data or managers.player:selected_equipment()
+	local __ray = self[old_func1](self, equipment_data, ...)
+	if tostring(equipment_data.use_function) == "use_sentry_gun" then
+		if __ray and equipment_data and self._dummy_unit then
+			local pos = __ray.position
+			local rot = self._unit:movement():m_head_rot()
+			rot = Rotation(__ray.normal, math.UP)
+			self._dummy_unit:set_position(pos)
+			self._dummy_unit:set_rotation(rot)
+			if alive(self._dummy_unit) then
+				self._dummy_unit:set_enabled(true)
 			end
-			rot = Rotation(ray.normal, math.UP)
 		end
+	end
+	return __ray
+end
 
-		if not alive(self._dummy_unit) then
-			self._dummy_unit = World:spawn_unit(Idstring(equipment_data.dummy_unit), pos, rot)
-
-			self:_disable_contour(self._dummy_unit)
+function PlayerEquipment:use_sentry_gun(selected_index, unit_idstring_index, ...)
+	local __ans = self[old_func2](self, selected_index, unit_idstring_index, ...)
+	if __ans then
+		return __ans
+	else
+		if self._sentrygun_placement_requested then
+			return
 		end
-
-		self._dummy_unit:set_position(pos)
-		self._dummy_unit:set_rotation(rot)
+		local _valid_look_at_placement = false
+		local ray = self:valid_shape_placement()	
+		if not ray then
+			_valid_look_at_placement = true
+			ray = self:valid_look_at_placement()
+		end
+		if ray and self:_can_place("sentry_gun") then
+			local pos = ray.position
+			local rot = self:_m_deploy_rot()
+			if _valid_look_at_placement then
+				local rot_pitch = math.round(rot:pitch())
+				if rot_pitch < 66 and rot_pitch > -66 then
+					rot_pitch = 90
+				elseif rot_pitch >= 66 then
+					rot_pitch = 180
+				else
+					rot_pitch = 0
+				end
+				rot = Rotation(rot:yaw(), rot_pitch, 0)
+			else
+				rot = Rotation(rot:yaw(), 0, 0)
+			end
+			managers.statistics:use_sentry_gun()
+			local ammo_level = managers.player:upgrade_value("sentry_gun", "extra_ammo_multiplier", 1)
+			local armor_multiplier = 1 + managers.player:upgrade_value("sentry_gun", "armor_multiplier", 1) - 1 + managers.player:upgrade_value("sentry_gun", "armor_multiplier2", 1) - 1
+			local can_switch_fire_mode = managers.player:has_category_upgrade("sentry_gun", "ap_bullets")
+			local equipment_name = managers.player:equipment_in_slot(selected_index)
+			local fire_mode_index = can_switch_fire_mode and managers.player:get_equipment_setting(equipment_name, "fire_mode") or 1
+			if Network:is_client() then
+				managers.network:session():send_to_host("place_sentry_gun", pos, rot, selected_index, self._unit, unit_idstring_index, ammo_level, fire_mode_index)
+				self._sentrygun_placement_requested = true
+				return false
+			else
+				local shield = managers.player:has_category_upgrade("sentry_gun", "shield")
+				local sentry_gun_unit, spread_level, rot_level = SentryGunBase.spawn(self._unit, pos, rot, managers.network:session():local_peer():id(), false, unit_idstring_index)
+				if sentry_gun_unit then
+					self:_sentry_gun_ammo_cost(sentry_gun_unit:id())
+					local fire_rate_reduction = managers.player:upgrade_value("sentry_gun", "fire_rate_reduction", 1)
+					managers.network:session():send_to_peers_synched("from_server_sentry_gun_place_result", managers.network:session():local_peer():id(), selected_index, sentry_gun_unit, rot_level, spread_level, shield, ammo_level, fire_mode_index)
+					sentry_gun_unit:event_listener():call("on_setup", true)
+					sentry_gun_unit:base():post_setup(fire_mode_index)
+				else
+					return false
+				end
+			end
+			return true
+		end
+		return false
 	end
-
-	if alive(self._dummy_unit) then
-		self._dummy_unit:set_enabled(ray and true or false)
-	end
-
-	return ray
 end
--- mods/Symmetra/lua/playermanager.lua
+++ mods/Symmetra/lua/playermanager.lua
@@ -1,17 +1,14 @@
-function PlayerManager:check_selected_equipment_placement_valid(player)
-	local equipment_data = managers.player:selected_equipment()
+local old_func = "F_"..Idstring("Symmetra:check_equipment_placement_valid"):key()
 
+PlayerManager[old_func] = PlayerManager[old_func] or PlayerManager.check_equipment_placement_valid
+
+function PlayerManager:check_equipment_placement_valid(player, equipment, ...)
+	local equipment_data = managers.player:equipment_data_by_name(equipment)
 	if not equipment_data then
 		return false
 	end
-
-	if equipment_data.equipment == "sentry_gun_silent" or equipment_data.equipment == "sentry_gun" or equipment_data.equipment == "trip_mine" or equipment_data.equipment == "ecm_jammer" then
+	if equipment_data.equipment == "sentry_gun_silent" or equipment_data.equipment == "sentry_gun" then
 		return player:equipment():valid_look_at_placement(tweak_data.equipments[equipment_data.equipment]) and true or false
-	elseif equipment_data.equipment == "ammo_bag" or equipment_data.equipment == "doctor_bag" or equipment_data.equipment == "first_aid_kit" or equipment_data.equipment == "bodybags_bag" then
-		return player:equipment():valid_shape_placement(equipment_data.equipment, tweak_data.equipments[equipment_data.equipment]) and true or false
-	elseif equipment_data.equipment == "armor_kit" then
-		return true
 	end
-
-	return player:equipment():valid_placement(tweak_data.equipments[equipment_data.equipment]) and true or false
+	return self[old_func](self, player, equipment, ...)
 end
