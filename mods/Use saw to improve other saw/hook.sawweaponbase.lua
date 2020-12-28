731353660b6be1a0af06a0ad7c77411910c370de
Dr_Newbie <tf2baidonation@gmail.com>
2020-12-29 00:43:28 +0800

    Use saw to improve other saw[5]

--- mods/Use saw to improve other saw/hook.sawweaponbase.lua
+++ mods/Use saw to improve other saw/hook.sawweaponbase.lua
@@ -1,4 +1,8 @@
-Hooks:PostHook(SawHit, "on_collision", "UseSaw2ImproveOtherSaw", function(self, col_ray, weapon_unit, user_unit, damage)
+if Network and not Network:is_server() then
+	return
+end
+
+Hooks:PostHook(SawHit, "on_collision", "F_"..Idstring("SawHit:on_collision:UseSaw2ImproveOtherSaw"):key(), function(self, col_ray, weapon_unit, user_unit, damage)
 	local hit_unit = col_ray and col_ray.unit or nil
 	if hit_unit and hit_unit:base() then
 		if hit_unit:name():key() == "0cec8292d88a4875" or hit_unit:name() == Idstring("units/pd2_dlc_jolly/equipment/gen_interactable_saw/gen_interactable_saw") then
