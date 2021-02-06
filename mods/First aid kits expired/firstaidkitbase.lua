0b29358fdc083198a08a9c07dea1552a8b1c8981
Dr_Newbie <tf2baidonation@gmail.com>
2021-02-06 20:23:52 +0800

    .

--- mods/First aid kits expired/firstaidkitbase.lua
+++ mods/First aid kits expired/firstaidkitbase.lua
@@ -1,4 +1,4 @@
-Hooks:PreHook(FirstAidKitBase, 'take', 'F_'..Idstring('take:First aid kits expire'):key(), function(self, unit)
+Hooks:PreHook(FirstAidKitBase, 'take', 'F_'..Idstring('take:First aid kits expired'):key(), function(self, unit)
 	if math.random() < 0.3 and not self._empty and unit and unit:character_damage() then
 		unit:character_damage():delay_damage(100, 10)
 		self:_set_empty()
