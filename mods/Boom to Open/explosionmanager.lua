15dce63fcead1938a63cede2e9e524201db5ee29
Dr_Newbie <tf2baidonation@gmail.com>
2021-03-14 23:17:35 +0800

    Boom to Open[7]

--- mods/Boom to Open/explosionmanager.lua
+++ mods/Boom to Open/explosionmanager.lua
@@ -9,15 +9,17 @@ Hooks:PostHook(ExplosionManager, "detect_and_give_dmg", Idstring("Boom to Open:E
 			local units = World:find_units("sphere", hit_pos, 300*__fix_from_dmg, __slot_mask, "ray_type", "body bullet lock")
 			if type(units) == "table" and units[1] then
 				for id, hit_unit in pairs(units) do
-					if hit_unit:damage() and type(hit_unit:num_bodies()) == "number" then
+					if hit_unit:damage() and type(hit_unit:num_bodies()) == "number" and hit_unit.interaction and hit_unit:interaction() and not hit_unit:interaction():disabled() and hit_unit:interaction():active() then
 						for i = 0, hit_unit:num_bodies() - 1 do
 							local body = hit_unit:body(i)
 							if body:extension() and body:extension().damage and body:extension().damage:endurance_exists("lock") then
+								--[[
 								if hit_unit:damage():has_sequence("state_spawn_nothing") then
 									if math.random() > 0.3 then
 										hit_unit:damage():run_sequence_simple("state_spawn_nothing")
 									end
 								end
+								]]
 								body:extension().damage:damage_lock(
 									managers.player:local_player(), 
 									math.UP, 
