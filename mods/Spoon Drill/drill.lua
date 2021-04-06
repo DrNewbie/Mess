5c1173aeed0460f190f94c8fb0c82150ed05bdb1
Dr_Newbie <tf2baidonation@gmail.com>
2021-04-06 18:50:35 +0800

    .

--- mods/Spoon Drill/drill.lua
+++ mods/Spoon Drill/drill.lua
@@ -7,16 +7,20 @@ local func6 = "F_"..Idstring("func5::"..mod_ids):key()
 local spoon_gold_ids = Idstring("units/pd2_dlc_apfo/weapons/wpn_fps_mel_spoon/wpn_third_mel_spoon_gold")
 
 Drill[func5] = function(self)
-	if self[func3] then
-		self[func3]:unlink()
-		World:delete_unit(self[func3])
-		self[func3] = nil	
+	if self._unit then
+		if self[func3] then
+			self[func3]:unlink()
+			World:delete_unit(self[func3])
+			self[func3] = nil	
+		end
+		local drill_align = self._unit:get_object(Idstring("e_drill_particles"))
+		if drill_align then
+			local spoon_unit = World:spawn_unit(spoon_gold_ids, drill_align:position(), drill_align:rotation())
+			self._unit:link(drill_align:name(), spoon_unit, spoon_unit:orientation_object():name())
+			self[func3] = spoon_unit
+			self._unit:set_visible(false)
+		end
 	end
-	local drill_align = self._unit:get_object(Idstring("e_drill_particles"))
-	local spoon_unit = World:spawn_unit(spoon_gold_ids, drill_align:position(), drill_align:rotation())
-	self._unit:link(drill_align:name(), spoon_unit, spoon_unit:orientation_object():name())
-	self[func3] = spoon_unit
-	self._unit:set_visible(false)
 end
 
 Hooks:PostHook(Drill, "_start_drill_effect", func1, function(self)
