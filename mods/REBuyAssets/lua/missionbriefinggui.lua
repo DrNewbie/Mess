e6c4eebace3c78be9d7fb582b8b14cbb3bb8f111
Berk-Rider <38303655+Berk-Rider@users.noreply.github.com>
2021-04-17 14:51:12 -0300

    Public Lobby Preplanning Fix

--- mods/REBuyAssets/lua/missionbriefinggui.lua
+++ mods/REBuyAssets/lua/missionbriefinggui.lua
@@ -1,5 +1,9 @@
 _G.REBuyAssets = _G.REBuyAssets or {}
 
+if not REBuyAssets or Network:is_client() then
+	return
+end
+
 Hooks:PostHook(AssetsItem, "chk_preplanning_textures_done", "REBuyAssets_DoPreplanning", function(...)
 	REBuyAssets:Load()
 	local current_stage = managers.job:current_level_id()
@@ -15,20 +19,6 @@ Hooks:PostHook(AssetsItem, "chk_preplanning_textures_done", "REBuyAssets_DoPrepl
 				else
 					managers.preplanning:server_unreserve_mission_element(_data.id, 1)
 				end
-			end
-		end
-	end
-end )
-
-Hooks:PostHook(MissionBriefingGui, "create_asset_tab", "REBuyAssets_DoAssets", function(...)
-	REBuyAssets.settings = REBuyAssets.settings or {}
-	REBuyAssets.settings["AutoBuyAllAssets"] = REBuyAssets.settings["AutoBuyAllAssets"] or 0
-	if REBuyAssets.settings["AutoBuyAllAssets"] == 1 then
-		local asset_ids = managers.assets:get_all_asset_ids(true) or {}
-		for _, asset_id in pairs(asset_ids) do
-			local asset = managers.assets:_get_asset_by_id(asset_id)
-			if asset.can_unlock then
-				managers.assets:unlock_asset(asset_id)
 			end
 		end
 	end
