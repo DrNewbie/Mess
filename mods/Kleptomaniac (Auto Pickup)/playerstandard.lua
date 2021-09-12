9baea11be67def11319ade4b75b43f46b4e4e770
Dr_Newbie <tf2baidonation@gmail.com>
2021-09-12 11:35:20 +0800

    Kleptomaniac (Auto Pickup)[2]

--- mods/Kleptomaniac (Auto Pickup)/playerstandard.lua
+++ mods/Kleptomaniac (Auto Pickup)/playerstandard.lua
@@ -1,21 +1,44 @@
-function PlayerStandard:_find_pickups_kleptomaniac(list)
-	local pickups = World:find_units_quick("sphere", self._unit:movement():m_pos(), 200) or {}
-	if pickups and #pickups > 0 then
+local ThisModPath = ModPath
+local mod_ids = Idstring(ThisModPath):key()
+local func1 = "F_"..Idstring("func1::"..mod_ids):key()
+local func2 = "F_"..Idstring("func2::"..mod_ids):key()
+local func3 = "F_"..Idstring("func3::"..mod_ids):key()
+local func4 = "F_"..Idstring("func4::"..mod_ids):key()
+
+local function __Kleptomaniac(them, list)
+	local pickups = World:find_units_quick("sphere", them._unit:movement():m_pos(), 200, 1) or {}
+	if type(pickups) == "table" and #pickups > 0 then
 		for _, pickup in pairs(pickups) do
-			if pickup:interaction() and type(pickup:interaction().tweak_data) == "string" and list[pickup:interaction().tweak_data] then
-				if pickup:interaction():can_select(self._unit) and not pickup:interaction():_interact_blocked(self._unit) then
-					pickup:interaction():interact(self._unit)
+			if type(pickup.interaction) == "function" and pickup:interaction() and type(pickup:interaction().tweak_data) == "string" and (not list or (type(list) == "table" and list[pickup:interaction().tweak_data])) then
+				local pick_inter = pickup:interaction()
+				local pick_tweak = pick_inter.tweak_data
+				if not tweak_data.interaction[pick_tweak] or tweak_data.interaction[pick_tweak].timer then
+				
+				else
+					if pick_inter:can_select(them._unit) and pick_inter:can_interact(them._unit) and not pick_inter:_interact_blocked(them._unit) and pick_inter:active() and not pick_inter:disabled() then
+						pick_inter:interact(them._unit)
+						return
+					end
 				end
 			end
 		end
 	end
 end
 
-Hooks:PostHook(PlayerStandard, "_update_check_actions", "F_"..Idstring("Post.PlyStand._update_check_actions.Kleptomaniac"):key(), function(self)
-	if self._find_pickups_kleptomaniac_dt then
-		self._find_pickups_kleptomaniac_dt = nil
+Hooks:PostHook(PlayerStandard, "_update_check_actions", func1, function(self, t, dt)
+	if self[func3] then
+		self[func3] = false
 	else
-		self._find_pickups_kleptomaniac_dt = true
-		self:_find_pickups_kleptomaniac({pickup_keycard = true})
+		self[func3] = true
+		__Kleptomaniac(self)
+	end
+end)
+
+Hooks:PostHook(PlayerCarry, "_update_check_actions", func2, function(self, t, dt)
+	if self[func4] then
+		self[func4] = false
+	else
+		self[func4] = true
+		__Kleptomaniac(self)
 	end
 end)
