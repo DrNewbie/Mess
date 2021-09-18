839f7f12ab88bfaf9245d06f7aacd9ff6233f3b2
Dr_Newbie <tf2baidonation@gmail.com>
2021-09-18 14:36:58 +0800

    Bullet Dismemberment (Alt Mod)
    https://modworkshop.net/mod/34203

---
+++ mods/Bullet Dismemberment Mod/lua/CopDamage.lua
@@ -0,0 +1,209 @@
+if not _G.BulletDecapitations then
+	BulletDecapitations = {}
+end
+ 
+if not BulletDecapitations.tweak_data then
+	BulletDecapitations.tweak_data = {}
+end
+ 
+-- [[ BD Settings ]]
+-- Change which weapons can decapitate enemies and stuff...
+-- Set to either: 'true' or 'nil' (false).
+BulletDecapitations.tweak_data.allowed_weapons = {
+	["assault_rifle"] = true, -- Assault Rifle
+	["pistol"] = true, -- Pistol
+	["smg"] = true, -- SMG
+	["shotgun"] = true, -- Shotgun
+	["saw"] = true, -- OVE9000 Saw
+	["lmg"] = true, -- LMG
+	["snp"] = true, -- Sniper Rifle
+	["akimbo"] = true, -- Akimbo Pistols
+	["minigun"] = true -- Minigun
+}
+
+BulletDecapitations.tweak_data.enable_bullet_cops = true -- Enable decapitations for all allowed weapons, disable if you want want just want tasers or cloakers
+BulletDecapitations.tweak_data.twitch = true -- Enable body twitching on decapitations. Value 'true' by default.
+BulletDecapitations.tweak_data.blood_time = 20 -- Determines how long a decapitated body will bleed.
+BulletDecapitations.tweak_data.twitch_rate = 1 -- The interval at which a body twitches.
+
+-- "all" - Cloakers can be cut from head to torso. | "head" - Cloakers can be dismembered in the head only. | "body" - Cloakers can be dismembered in the body only. | "none" - Cloakers do not use the 'true' dismemberment system.
+BulletDecapitations.tweak_data.cloaker_decapitations = "all"
+BulletDecapitations.tweak_data.taser_decapitations = true -- Enables the special decapitation for tasers when headshotted.
+
+-- [[ End of BD Settings ]]
+BulletDecapitations.cop_decapitation = {
+	t = {},
+	interval = {},
+	attack_data = {},
+	vfx = {},
+	ragdoll = {},
+	parts = {}
+}
+
+Hooks:PostHook(CopDamage, "damage_bullet", "BD_CopDamagePostDamageBullet", function(self, attack_data)
+	if not attack_data.attacker_unit then
+		attack_data.attacker_unit = managers.player:player_unit()
+	end
+	if not self._dead or (attack_data.attacker_unit:inventory() and BulletDecapitations.tweak_data.allowed_weapons[tweak_data.weapon[attack_data.attacker_unit:inventory():equipped_unit():base():get_name_id()].category] ~= true) then
+	
+	else
+		if self._unit:base()._tweak_table == "spooc" then
+			local body = attack_data.body_name or attack_data.col_ray.body:name()
+			if (body:key() == Idstring("head"):key() or body:key() == Idstring("hit_Head"):key() or body:key() == Idstring("rag_Head"):key()) and (BulletDecapitations.tweak_data.cloaker_decapitations == "all" or BulletDecapitations.tweak_data.cloaker_decapitations == "head") or body:key() == Idstring("hit_Head"):key() or body:key() == Idstring("rag_Head"):key() and (BulletDecapitations.tweak_data.cloaker_decapitations == "all" or BulletDecapitations.tweak_data.cloaker_decapitations == "head") then
+				self._unit:sound():play("split_gen_head")
+				self._unit:damage():run_sequence_simple("dismember_head")
+			else
+				if (BulletDecapitations.tweak_data.cloaker_decapitations == "all" or BulletDecapitations.tweak_data.cloaker_decapitations == "body") then
+					self._unit:sound():play("split_gen_body")
+					self._unit:damage():run_sequence_simple("dismember_body_top")
+				end
+            end
+        end
+		if self._unit:base()._tweak_table == "taser" then
+			local body = attack_data.body_name or attack_data.col_ray.body:name()
+			if (body:key() == Idstring("head"):key() or body:key() == Idstring("hit_Head"):key() or body:key() == Idstring("rag_Head"):key()) and BulletDecapitations.tweak_data.taser_decapitations then
+				self._unit:sound():play("split_gen_head")
+				self._unit:damage():run_sequence_simple("kill_tazer_headshot")
+				return
+            end
+        end
+		if BulletDecapitations.tweak_data.enable_bullet_cops then
+			local body = attack_data.body_name or attack_data.col_ray.body:name()
+			if body:key() then
+				if not BulletDecapitations.cop_decapitation.parts[self._unit] then
+					BulletDecapitations.cop_decapitation.parts[self._unit] = {}
+				end			
+				self._unit:movement():enable_update()
+				self._unit:movement()._frozen = nil
+				if self._unit:movement()._active_actions[1] and self._unit:movement()._active_actions[1]:type() == "hurt" then
+					self._unit:movement()._active_actions[1]:force_ragdoll(true)
+				end				
+				local bone_head = self._unit:get_object(Idstring("Head"))
+				local bone_body = self._unit:get_object(Idstring("Spine1"))				
+				if body:key() == Idstring("head"):key() or body:key() == Idstring("hit_Head"):key() or body:key() == Idstring("rag_Head"):key() then
+					--self._unit:body(self._unit:get_object(Idstring("Head"))):set_enabled( false )					
+					BulletDecapitations.cop_decapitation.vfx[self._unit] = World:effect_manager():spawn({
+						effect = Idstring("effects/payday2/particles/impacts/blood/blood_tendrils"),
+						position = self._unit:get_object(Idstring("Neck")):position(),
+						rotation = self._unit:get_object(Idstring("Neck")):rotation()
+					})					
+					BulletDecapitations.cop_decapitation.parts[self._unit].Head = "Head"
+					self:_spawn_head_gadget({
+						position = bone_head:position(),
+						rotation = bone_head:rotation(),
+						dir = -self._unit:movement():m_head_rot():y()
+					})
+				elseif body:key() == Idstring("hit_LeftArm"):key() or body:key() == Idstring("hit_LeftForeArm"):key() or body:key() == Idstring("rag_LeftArm"):key() or body:key() == Idstring("rag_LeftForeArm"):key() then
+					--self._unit:body(self._unit:get_object(Idstring("LeftArm"))):set_enabled( false )
+					--self._unit:body(self._unit:get_object(Idstring("LeftForeArm"))):set_enabled( false )
+					--self._unit:body(self._unit:get_object(Idstring("LeftHand"))):set_enabled( false )
+					BulletDecapitations.cop_decapitation.parts[self._unit].LeftArm = "LeftArm"
+				elseif body:key() == Idstring("hit_RightArm"):key() or body:key() == Idstring("hit_RightForeArm"):key() or body:key() == Idstring("rag_RightArm"):key() or body:key() == Idstring("rag_RightForeArm"):key() then
+					--self._unit:body(self._unit:get_object(Idstring("RightArm"))):set_enabled( false )
+					--self._unit:body(self._unit:get_object(Idstring("RightForeArm"))):set_enabled( false )
+					--self._unit:body(self._unit:get_object(Idstring("RightHand"))):set_enabled( false )
+					BulletDecapitations.cop_decapitation.parts[self._unit].RightArm = "RightArm"
+				elseif body:key() == Idstring("hit_LeftUpLeg"):key() or body:key() == Idstring("hit_LeftLeg"):key() or body:key() == Idstring("rag_LeftUpLeg"):key() or body:key() == Idstring("rag_LeftLeg"):key() then
+					--self._unit:body(self._unit:get_object(Idstring("LeftLeg"))):set_enabled( false )
+					--self._unit:body(self._unit:get_object(Idstring("LeftFoot"))):set_enabled( false )
+					BulletDecapitations.cop_decapitation.parts[self._unit].LeftLeg = "LeftLeg"
+				elseif body:key() == Idstring("hit_RightUpLeg"):key() or body:key() == Idstring("hit_RightLeg"):key() or body:key() == Idstring("rag_RightUpLeg"):key() or body:key() == Idstring("rag_RightLeg"):key() then
+					--self._unit:body(self._unit:get_object(Idstring("RightLeg"))):set_enabled( false )
+					--self._unit:body(self._unit:get_object(Idstring("RightFoot"))):set_enabled( false )
+					BulletDecapitations.cop_decapitation.parts[self._unit].RightLeg = "RightLeg"
+				else
+				
+				end
+				BulletDecapitations.cop_decapitation.attack_data[self._unit] = attack_data
+				BulletDecapitations.cop_decapitation.ragdoll[self._unit] = self._unit
+				BulletDecapitations.cop_decapitation.t[self._unit] = Application:time() + BulletDecapitations.tweak_data.blood_time
+				BulletDecapitations.cop_decapitation.interval[self._unit] = Application:time() + BulletDecapitations.tweak_data.twitch_rate
+			end
+		end
+	end
+end )
+
+Hooks:PostHook(PlayerManager, "update", "BD_CopDecapitationUpdate", function(self, t, dt)	
+	if not CopDamage then
+	
+	else
+		if BulletDecapitations.cop_decapitation then
+			local __spawn_BD = function(__unit, __object)
+				if __unit and __unit:get_object(Idstring(__object)) then
+					BulletDecapitations.cop_decapitation.vfx[__unit] = World:effect_manager():spawn({
+						effect = Idstring("effects/payday2/particles/impacts/blood/blood_tendrils"),
+						position = __unit:get_object(Idstring(__object)):position(),
+						rotation = __unit:get_object(Idstring(__object)):rotation()
+					})
+				end
+				return
+			end
+			for unit, val in pairs(BulletDecapitations.cop_decapitation.ragdoll) do
+				if alive(unit) then
+				
+				else
+					BulletDecapitations.cop_decapitation.ragdoll[unit] = nil
+					BulletDecapitations.cop_decapitation.parts[unit] = nil
+				end
+			end
+			for unit, val in pairs(BulletDecapitations.cop_decapitation.t) do
+				if alive(unit) then
+					for part , _ in pairs(BulletDecapitations.cop_decapitation.parts[unit]) do
+						if part == "Head" then
+							__spawn_BD(unit, "Neck")
+						elseif part == "LeftArm" then
+							__spawn_BD(unit, "LeftShoulder")
+						elseif part == "LeftLeg" then
+							__spawn_BD(unit, "LeftUpLeg")
+						elseif part == "RightArm" then
+							__spawn_BD(unit, "RightShoulder")
+						elseif part == "RightLeg" then
+							__spawn_BD(unit, "RightUpLeg")
+						else
+							
+						end
+						if part == "Head" then
+							if Application:time() < val then
+								if Application:time() >= BulletDecapitations.cop_decapitation.interval[unit] then
+									BulletDecapitations.cop_decapitation.interval[unit] = Application:time() + BulletDecapitations.tweak_data.twitch_rate
+									local splatter_from = unit:get_object(Idstring("Neck")):position()
+									local splatter_to = splatter_from + unit:get_object(Idstring("Neck")):rotation():y() * 100
+									local splatter_ray = unit:raycast("ray", splatter_from, splatter_to, "slot_mask", managers.slot:get_mask("world_geometry"))
+									if splatter_ray then
+										World:project_decal(Idstring("blood_spatter"), splatter_ray.position, splatter_ray.ray, splatter_ray.unit, nil, splatter_ray.normal)
+									end
+									if unit:movement()._active_actions[1] and unit:movement()._active_actions[1]:type() == "hurt" then
+										unit:movement()._active_actions[1]:force_ragdoll(true)
+									end
+									local scale = BulletDecapitations.tweak_data.twitch and 0.075 or 0
+									local height = 1
+									local twist_dir = math.random(2) == 1 and 1 or -1
+									local rot_acc = (math.UP * (0.5 * twist_dir)) * -0.5
+									local rot_time = 1 + math.rand(2)
+									local nr_u_bodies = unit:num_bodies()
+									local i_u_body = 0
+									while nr_u_bodies > i_u_body do
+										local u_body = unit:body(i_u_body)
+										if u_body:enabled() and u_body:dynamic() then
+											local body_mass = u_body:mass()
+											World:play_physic_effect(Idstring("physic_effects/body_explosion"), u_body, math.UP * 600 * scale, 4 * body_mass / math.random(2), rot_acc, rot_time)
+										end
+										i_u_body = i_u_body + 1
+									end
+								end
+							else
+								BulletDecapitations.cop_decapitation.t[unit] = nil
+								BulletDecapitations.cop_decapitation.interval[unit] = nil
+								BulletDecapitations.cop_decapitation.attack_data[unit] = nil
+							end
+						end
+					end
+				else
+					BulletDecapitations.cop_decapitation.t[unit] = nil
+					BulletDecapitations.cop_decapitation.interval[unit] = nil
+					BulletDecapitations.cop_decapitation.attack_data[unit] = nil
+				end
+			end
+		end	
+	end	
+end)
---
+++ mods/Bullet Dismemberment Mod/lua/CopMovement.lua
@@ -0,0 +1,58 @@
+Hooks:PreHook(CopMovement, "update", "BulletDecapitationsPreCopMovementUpdate", function(self, unit, t, dt)
+	if not BulletDecapitations then
+	
+	else
+		local __set_BD = function(__unit, __object, __body)
+			if __unit and __unit:get_object(Idstring(__object)) then
+				__unit:get_object(Idstring(__object)):m_position(__body:position())
+				__unit:get_object(Idstring(__object)):m_rotation(__body:rotation())
+				__unit:get_object(Idstring(__object)):set_position(__body:position())
+				__unit:get_object(Idstring(__object)):set_rotation(__body:rotation())
+				__unit:get_object(Idstring(__object)):set_visibility(false)
+			end
+			return
+		end
+		for decap_unit, _ in pairs(BulletDecapitations.cop_decapitation.ragdoll) do
+			if decap_unit == self._unit then
+				local bone_body = self._unit:get_object(Idstring("LeftUpLeg"))
+				local bone_body2 = self._unit:get_object(Idstring("RightUpLeg"))
+				local bone_body3 = self._unit:get_object(Idstring("LeftShoulder"))
+				local bone_body4 = self._unit:get_object(Idstring("RightShoulder"))
+				local bone_body5 = self._unit:get_object(Idstring("Spine1"))
+				self._need_upd = false
+				self._force_head_upd = nil
+				self:upd_ground_ray()
+				--[[self._unit:movement():enable_update()
+				self._unit:movement()._frozen = nil
+				if self._unit:movement()._active_actions[1] then
+					self._unit:movement()._active_actions[1]:force_ragdoll()
+				end]]
+				for part , _ in pairs(BulletDecapitations.cop_decapitation.parts[unit]) do
+					if part == "Head" then
+						__set_BD(self._unit, "Head", bone_body5)
+					elseif part == "LeftArm" then
+						__set_BD(self._unit, "LeftShoulder", bone_body3)
+						__set_BD(self._unit, "LeftArm", bone_body3)
+						__set_BD(self._unit, "LeftForeArm", bone_body3)
+						__set_BD(self._unit, "LeftHand", bone_body3)
+					elseif part == "LeftLeg" then
+						__set_BD(self._unit, "LeftUpLeg", bone_body)
+						__set_BD(self._unit, "LeftLeg", bone_body)
+						__set_BD(self._unit, "LeftFoot", bone_body)
+					elseif part == "RightArm" then
+						__set_BD(self._unit, "RightShoulder", bone_body4)
+						__set_BD(self._unit, "RightArm", bone_body4)
+						__set_BD(self._unit, "RightForeArm", bone_body4)
+						__set_BD(self._unit, "RightHand", bone_body4)
+					elseif part == "RightLeg" then
+						__set_BD(self._unit, "RightUpLeg", bone_body2)
+						__set_BD(self._unit, "RightLeg", bone_body2)
+						__set_BD(self._unit, "RightFoot", bone_body2)
+					else
+						
+					end
+				end
+			end
+		end
+	end
+end)
