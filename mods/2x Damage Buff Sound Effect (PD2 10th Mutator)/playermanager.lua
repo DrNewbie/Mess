7b47f01f099c1ec20ceeef450806f97127819798
Dr_Newbie <tf2baidonation@gmail.com>
2021-10-22 19:41:42 +0800

    2x Damage Buff Sound Effect (PD2 10th Mutator)
    https://modworkshop.net/mod/34748

--- mods/2x Damage Buff Sound Effect (PD2 10th Mutator)/playermanager.lua
+++ mods/2x Damage Buff Sound Effect (PD2 10th Mutator)/playermanager.lua
@@ -1,16 +1,16 @@
 local ThisModPath = ModPath
 local ThisModIds = Idstring(ThisModPath):key()
-local ThisOGGAssets = ModPath.."Assets/sounds/birthday_buff/buff_birthday_2x_damage.ogg"
+local ThisOGGPath = ModPath.."Assets/sounds/birthday_buff/buff_birthday_2x_damage.ogg"
 local Hook1 = "F_"..Idstring("Hook1::"..ThisModIds):key()
 local IsOK = false
 
-if blt.xaudio and io.file_is_readable(ThisOGGAssets) then
+if blt.xaudio and io.file_is_readable(ThisOGGPath) then
 	blt.xaudio.setup()
 	IsOK = true
 end
 
 Hooks:PostHook(PlayerManager, "activate_temporary_property", Hook1, function(self, __name, __time, __value)
 	if IsOK and __name and __name == "birthday_multiplier" and __time and __time > 0 and __value and __value > 1 then
-		XAudio.Source:new(XAudio.Buffer:new(ThisOGGAssets))
+		XAudio.UnitSource:new(XAudio.PLAYER, XAudio.Buffer:new(ThisOGGPath)):set_volume(1)
 	end
 end)
