1cfacf5964ce04e3913138bd33f05b4b990c87f9
Dr_Newbie <tf2baidonation@gmail.com>
2021-10-23 22:18:33 +0800

    Jammed Drill Rollin[2]

--- mods/Jammed Drill Rollin/timergui.lua
+++ mods/Jammed Drill Rollin/timergui.lua
@@ -1,11 +1,9 @@
 local ThisModPath = ModPath
-local mod_ids = Idstring(ThisModPath):key()
-local func1 = "F_"..Idstring("func1::"..mod_ids):key()
-local func3 = "F_"..Idstring("func3::"..mod_ids):key()
-local func4 = "F_"..Idstring("func4::"..mod_ids):key()
+local ThisModPathIds = Idstring(ThisModPath):key()
+local func1 = "F_"..Idstring("func1::"..ThisModPathIds):key()
 
-Hooks:PostHook(TimerGui, "update", func4, function(self, unit, t, dt)
-	if self._unit and self._unit:name():key() == "584bea03f3b5d712" and self._unit:base() and (self._jammed or not self._powered) then
-		self._unit:set_local_rotation(Rotation(0, 0, (t*1000)%255))
+Hooks:PostHook(TimerGui, "update", func1, function(self, __unit, __t, __dt)
+	if self._unit and self._unit:name():key() == "584bea03f3b5d712" and (self._jammed or not self._powered) then
+		self._unit:set_local_rotation(self._unit:local_rotation() * Rotation(Vector3(0, 1, 0), __dt * 1000))
 	end
 end)
