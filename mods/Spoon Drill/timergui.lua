216bb8ab5fec23a4f07a60287abf0285237a8352
Dr_Newbie <tf2baidonation@gmail.com>
2021-10-23 23:17:03 +0800

    Spoon Drill[5]

--- mods/Spoon Drill/timergui.lua
+++ mods/Spoon Drill/timergui.lua
@@ -1,12 +1,12 @@
 local mod_ids = Idstring("Spoon Drill"):key()
-local func1 = "F_"..Idstring("func1::"..mod_ids):key()
 local func3 = "F_"..Idstring("func3::"..mod_ids):key()
 local func4 = "F_"..Idstring("func4::"..mod_ids):key()
 
-Hooks:PostHook(TimerGui, "update", func4, function(self, unit, t, dt)
+Hooks:PostHook(TimerGui, "update", func4, function(self, __unit, __t, __dt)
 	if self._unit:base() and self._unit:base()[func3] then
+		local __spoon = self._unit:base()[func3]
 		if not self._jammed and self._powered then
-			self._unit:base()[func3]:set_local_rotation(Rotation(0, 0, (t*1000)%255))
+			__spoon:set_local_rotation(__spoon:local_rotation() * Rotation(Vector3(0, 1, 0), __dt * 1000))
 		end
 	end
 end)
