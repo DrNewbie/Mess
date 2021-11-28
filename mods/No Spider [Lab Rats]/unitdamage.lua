92f68f97d67bccd624bb28b70b97128e2fddaf7f
Dr_Newbie <tf2baidonation@gmail.com>
2021-11-28 20:36:59 +0800

    No Spider [Lab Rats] ; test 101

---
+++ mods/No Spider [Lab Rats]/unitdamage.lua
@@ -0,0 +1,15 @@
+local ThisModPath = ModPath
+
+BLTAssetManager:CreateEntry( 
+	Idstring("units/pd2_dlc_nails/props/nls_prop_methlab_spider_med/nls_prop_methlab_spider_med"), 
+	Idstring("object"), 
+	ThisModPath.."assets/nls_prop_methlab_spider_med.object",
+	nil 
+)
+
+BLTAssetManager:CreateEntry( 
+	Idstring("units/pd2_dlc_nails/props/nls_prop_methlab_spider/nls_prop_methlab_spider"), 
+	Idstring("object"), 
+	ThisModPath.."assets/nls_prop_methlab_spider.object",
+	nil 
+)
