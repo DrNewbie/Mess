0f07b4f496feef64242bc1d47fd4dc1c8434f69a
Dr_Newbie <tf2baidonation@gmail.com>
2022-01-09 18:43:06 +0800

    Block and Replace Enemies
    https://modworkshop.net/mod/35781

---
+++ mods/Block and Replace Enemies/elementspawnenemydummy.lua
@@ -0,0 +1,71 @@
+core:import("CoreMissionScriptElement")
+
+_G.BoR_Enemy = _G.BoR_Enemy or {}
+BoR_Enemy = BoR_Enemy or {}
+
+local func1 = BoR_Enemy:Name("ElementSpawnEnemyDummy:produce")
+local func2 = BoR_Enemy:Name("old:unit_name")
+
+ElementSpawnEnemyDummy[func1] = ElementSpawnEnemyDummy[func1] or ElementSpawnEnemyDummy.produce
+
+function ElementSpawnEnemyDummy:produce(params, ...)
+	local unit_name = nil
+	local which_one
+	if params and params.name then
+		unit_name = params.name
+		which_one = true
+	else
+		unit_name = self:value("enemy") or self._enemy_name
+		which_one = false
+	end
+	if unit_name then
+		--[[
+			Save unit name for next time
+		]]
+		self[func2] = self[func2] or unit_name
+		unit_name = self[func2]
+		--[[
+			Run BoR_E
+		]]
+		local unit_type = BoR_Enemy:Get_EnemyType(unit_name)
+		if type(unit_type) == "number" and unit_type > 0 and unit_type < 999 then
+			local BoR_type = BoR_Enemy:Get_Value("value::"..unit_type) or 1
+			if BoR_type == 1 then
+				--Default, do nothing
+			elseif BoR_type >= 2 and BoR_type <= 11 then
+				local r_unit = table.random({
+					Idstring("units/payday2/characters/ene_city_heavy_r870/ene_city_heavy_r870"),
+					Idstring("units/payday2/characters/ene_city_heavy_g36/ene_city_heavy_g36")
+				})
+				if BoR_type ~= 2 then
+					--Replace to special
+					if BoR_type == 11 then
+						--Random
+						local new_BoR_type = math.random(3, 9)
+						while new_BoR_type == unit_type do
+							new_BoR_type = math.random(3, 9)
+						end
+						BoR_type = new_BoR_type
+					elseif BoR_type == 10 then
+						--Bulldozer+
+						BoR_type = table.random({3, 3, 3, 8, 9})
+					end
+					local r_list = BoR_Enemy:Get_TypeEnemy(BoR_type)
+					if type(r_list) == "table" and table.size(r_list) > 0 then
+						r_unit = r_list[table.random_key(r_list)]
+					end
+				end
+				if which_one then
+					params.name = r_unit
+				else
+					if self:value("enemy") then
+						self._values["enemy"] = r_unit
+					elseif self._enemy_name then
+						self._enemy_name = r_unit
+					end
+				end
+			end
+		end
+	end
+	return self[func1](self, params, ...)
+end
---
+++ mods/Block and Replace Enemies/menumanagerpd2.lua
@@ -0,0 +1,179 @@
+_G.BoR_Enemy = _G.BoR_Enemy or {}
+BoR_Enemy = BoR_Enemy or {}
+BoR_Enemy.ModPath = BoR_Enemy.ModPath or ModPath
+BoR_Enemy.settings = BoR_Enemy.settings or {}
+BoR_Enemy.enemy_type_list = BoR_Enemy.enemy_type_list or {}
+BoR_Enemy.type_enemy_list = BoR_Enemy.type_enemy_list or {}
+
+function BoR_Enemy:Name(__name)
+	return "BoR_E_"..Idstring(__name.."::"..self.ModPath):key()
+end
+
+local function __Name(__name)
+	return BoR_Enemy:Name(__name)
+end
+
+BoR_Enemy.menu_id = __Name("menu_id")
+BoR_Enemy.SaveFile = BoR_Enemy.SaveFile or SavePath..__Name("SaveFile")..".txt"
+
+function BoR_Enemy:Reset()
+	self.settings = {
+		init = true
+	}
+	self:Save()
+	return
+end
+
+function BoR_Enemy:Load()
+	local __file = io.open(self.SaveFile, "r")
+	if __file then
+		for key, value in pairs(json.decode(__file:read("*all"))) do
+			self.settings[key] = value
+		end
+		__file:close()
+	else
+		self:Reset()
+	end
+	return
+end
+
+function BoR_Enemy:Save()
+	local __file = io.open(self.SaveFile, "w+")
+	if __file then
+		__file:write(json.encode(self.settings))
+		__file:close()
+	end
+	return
+end
+
+function BoR_Enemy:Get_Value(__name)
+	return self.settings[self:Name(__name)] or nil
+end
+
+function BoR_Enemy:Set_Value(__name, __var)
+	self.settings[self:Name(__name)] = __var
+	self:Save()
+	return
+end
+
+local function __Get(__name)
+	return BoR_Enemy:Get_Value(__name)
+end
+
+local function __Set(__name, __var)
+	return BoR_Enemy:Set_Value(__name, __var)
+end
+
+BoR_Enemy:Load()
+
+Hooks:Add("LocalizationManagerPostInit", __Name("Loc"), function(loc)
+	loc:add_localized_strings({
+		["BoR_Enemy_menu_title"] = "Block or Replace Enemies",
+		["BoR_Enemy_menu_desc"] = " ",
+		["BoR_Enemy_menu_opts_1"] = "Default",
+		["BoR_Enemy_menu_opts_2"] = "Block",
+		["BoR_Enemy_menu_opts_3"] = "Bulldozer",
+		["BoR_Enemy_menu_opts_4"] = "Cloaker",
+		["BoR_Enemy_menu_opts_5"] = "Shield",
+		["BoR_Enemy_menu_opts_6"] = "Taser",
+		["BoR_Enemy_menu_opts_7"] = "Medic",
+		["BoR_Enemy_menu_opts_8"] = "Minigun-dozer",
+		["BoR_Enemy_menu_opts_9"] = "Medic-dozer",
+		["BoR_Enemy_menu_opts_10"] = "Bulldozer+",
+		["BoR_Enemy_menu_opts_11"] = "Random"
+	})
+end)
+
+Hooks:Add("MenuManagerSetupCustomMenus", __Name("NewMenu"), function()
+	MenuHelper:NewMenu(BoR_Enemy.menu_id)
+end)
+
+Hooks:Add("MenuManagerPopulateCustomMenus", __Name("CustomMenus"), function(menu_manager, nodes)
+	local __items = {
+		"BoR_Enemy_menu_opts_1",
+		"BoR_Enemy_menu_opts_2",
+		"BoR_Enemy_menu_opts_3",
+		"BoR_Enemy_menu_opts_4",
+		"BoR_Enemy_menu_opts_5",
+		"BoR_Enemy_menu_opts_6",
+		"BoR_Enemy_menu_opts_7",
+		"BoR_Enemy_menu_opts_8",
+		"BoR_Enemy_menu_opts_9",
+		"BoR_Enemy_menu_opts_10",
+		"BoR_Enemy_menu_opts_11"
+	}
+	for __i = 3, 9 do
+		MenuCallbackHandler[__Name("callback::"..__i)] = function(self, item)
+			__Set("value::"..__i, math.floor(item:value()))
+		end
+		MenuHelper:AddMultipleChoice({
+			id = __Name("id::"..__i),
+			title = "BoR_Enemy_menu_opts_"..__i,
+			callback = __Name("callback::"..__i),
+			items = __items,
+			value = __Get("value::"..__i) or 1,
+			priority = 100-__i,
+			menu_id = BoR_Enemy.menu_id,  
+		})
+	end
+end)
+
+Hooks:Add("MenuManagerBuildCustomMenus", __Name("BuildMenu"), function(menu_manager, nodes)
+	nodes[BoR_Enemy.menu_id] = MenuHelper:BuildMenu(BoR_Enemy.menu_id)
+	MenuHelper:AddMenuItem(nodes["blt_options"], BoR_Enemy.menu_id, "BoR_Enemy_menu_title", "BoR_Enemy_menu_desc")
+end)
+
+function BoR_Enemy:Get_TypeEnemy(__type)
+	return self.type_enemy_list[__type]
+end
+
+function BoR_Enemy:Set_EnemyType(unit_name_ids, __type)
+	self.enemy_type_list[unit_name_ids:key()] = __type
+	self.type_enemy_list[__type] = self.type_enemy_list[__type] or {}
+	self.type_enemy_list[__type][unit_name_ids:key()] = unit_name_ids
+	return
+end
+
+function BoR_Enemy:Get_EnemyType(unit_name_ids)
+	if self.enemy_type_list[unit_name_ids:key()] then
+		return self.enemy_type_list[unit_name_ids:key()]
+	end
+	if DB:has(Idstring("unit"), unit_name_ids) then
+		local unit_node = DB:load_node(Idstring("unit"), unit_name_ids)
+		if unit_node and type(unit_node) == "userdata" then
+			local unit_node_string = tostring(unit_node)
+			local type_list = {
+				["tank"] = 3,
+				["spooc"] = 4,
+				["shield"] = 5,
+				["taser"] = 6,
+				["medic"] = 7,
+				["tank_medic"] = 8,
+				["tank_mini"] = 9
+			}
+			for type_key, type_id in pairs(type_list) do
+				if unit_node_string:find('<var name="_tweak_table" value="'..type_key..'"/>') then
+					self:Set_EnemyType(unit_name_ids, type_id)
+					return type_id
+				end
+			end
+			self:Set_EnemyType(unit_name_ids, "other")
+			return 999
+		end
+	end
+	return -1
+end
+
+BoR_Enemy:Get_EnemyType("units/payday2/characters/ene_medic_r870/ene_medic_r870")
+BoR_Enemy:Get_EnemyType("units/payday2/characters/ene_medic_m4/ene_medic_m4")
+BoR_Enemy:Get_EnemyType("units/payday2/characters/ene_spook_1/ene_spook_1")
+BoR_Enemy:Get_EnemyType("units/payday2/characters/ene_tazer_1/ene_tazer_1")
+BoR_Enemy:Get_EnemyType("units/payday2/characters/ene_sniper_1/ene_sniper_1")
+BoR_Enemy:Get_EnemyType("units/payday2/characters/ene_shield_1/ene_shield_1")
+BoR_Enemy:Get_EnemyType("units/payday2/characters/ene_shield_2/ene_shield_2")
+BoR_Enemy:Get_EnemyType("units/payday2/characters/ene_bulldozer_1/ene_bulldozer_1")
+BoR_Enemy:Get_EnemyType("units/payday2/characters/ene_bulldozer_2/ene_bulldozer_2")
+BoR_Enemy:Get_EnemyType("units/payday2/characters/ene_bulldozer_3/ene_bulldozer_3")
+BoR_Enemy:Get_EnemyType("units/pd2_dlc_drm/characters/ene_bulldozer_medic/ene_bulldozer_medic")
+BoR_Enemy:Get_EnemyType("units/pd2_dlc_drm/characters/ene_bulldozer_minigun/ene_bulldozer_minigun")
+BoR_Enemy:Get_EnemyType("units/pd2_dlc_drm/characters/ene_bulldozer_minigun_classic/ene_bulldozer_minigun_classic")
