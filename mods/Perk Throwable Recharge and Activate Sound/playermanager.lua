b22980a1dd180a01dadd1892ba82f33a3e8a36f2
Dr_Newbie <tf2baidonation@gmail.com>
2022-01-30 17:53:51 +0800

    .

--- mods/Perk Throwable Recharge and Activate Sound/playermanager.lua
+++ mods/Perk Throwable Recharge and Activate Sound/playermanager.lua
@@ -1,17 +1,21 @@
 local ThisModPath = ModPath or tostring(math.random())
 
 Hooks:PostHook(PlayerManager, "_on_grenade_cooldown_end", "MOD2_"..Idstring(ThisModPath):key(), function(self)
-	local __var1, __var2 = managers.blackmarket:equipped_grenade()
-	local tweak = tweak_data.blackmarket.projectiles[__var1]
-	if tweak and tweak.sounds and tweak.sounds.cooldown then
-		self:player_unit():sound():play(tweak.sounds.cooldown)
+	if self:player_unit() then
+		local __var1, __var2 = managers.blackmarket:equipped_grenade()
+		local tweak = tweak_data.blackmarket.projectiles[__var1]
+		if tweak and tweak.sounds and tweak.sounds.cooldown then
+			self:player_unit():sound():play(tweak.sounds.cooldown)
+		end
 	end
 end)
 
 Hooks:PostHook(PlayerManager, "on_throw_grenade", "MOD3_"..Idstring(ThisModPath):key(), function(self)
-	local __var1, __var2 = managers.blackmarket:equipped_grenade()
-	local tweak = tweak_data.blackmarket.projectiles[__var1]
-	if tweak and tweak.sounds and tweak.sounds.activate then
-		self:player_unit():sound():play(tweak.sounds.activate)
+	if self:player_unit() then
+		local __var1, __var2 = managers.blackmarket:equipped_grenade()
+		local tweak = tweak_data.blackmarket.projectiles[__var1]
+		if tweak and tweak.sounds and tweak.sounds.activate then
+			self:player_unit():sound():play(tweak.sounds.activate)
+		end
 	end
 end)
