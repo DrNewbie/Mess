5144632a4f3e8ed228eec6454d322fad576ce4ae
Dr_Newbie <tf2baidonation@gmail.com>
2022-02-18 21:30:52 +0800

    Player Event Sounds[1]
    https://modworkshop.net/mod/36336

--- mods/Message Sounds Event/cfgs/test_OnPlayerDodge.lua
+++
@@ -1,8 +0,0 @@
-_G.MessageSoundsEventt = _G.MessageSoundsEventt or {}
-
-_G.MessageSoundsEventt.AddMsgFunc("OnPlayerDodge", "a_test_OnPlayerDodge_000", function()
-	if managers.player and managers.player:player_unit() then
-		_G.MessageSoundsEventt.PlaySoundRandom("OnPlayerDodge")
-	end
-	return
-end)
