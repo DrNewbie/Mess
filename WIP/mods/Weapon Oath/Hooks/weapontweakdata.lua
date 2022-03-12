a508bbe19d8de802093ff501651305471937616e
Dr_Newbie <tf2baidonation@gmail.com>
2022-03-13 01:57:55 +0800

    .

--- WIP/mods/Weapon Oath/Hooks/weapontweakdata.lua
+++ WIP/mods/Weapon Oath/Hooks/weapontweakdata.lua
@@ -7,11 +7,6 @@ Hooks:PostHook(WeaponTweakData, "init", Hook1, function(self)
 		if type(__data) == "table" and type(__data.categories) == "table" and type(__data.use_data) == "table" then
 			self[__id].__oath_data = self[__id].__oath_data or {}
 			self[__id].__oath_data.__max_points = self[__id].__oath_data.__max_points or 1*200000
-			self[__id].__oath_data.__oath_link = self[__id].__oath_data.__oath_link or function(is_click, now_rate)
-				if is_click then
-					Steam:overlay_activate("url", "https://iopwiki.com/wiki/Special:Random")
-				end
-			end
 		end
 	end
 end)
