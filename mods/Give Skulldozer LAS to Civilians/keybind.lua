8f275acfa17b4399c07ddf407c57f9106009d1b4
Dr_Newbie <tf2baidonation@gmail.com>
2022-04-10 01:52:52 +0800

    Give Skulldozer LAS to Civilians

---
+++ mods/Give Skulldozer LAS to Civilians/keybind.lua
@@ -0,0 +1,137 @@
+local __attach_line = [[
+		<extension name="spawn_manager" class="ManageSpawnedUnits" >
+			<var name="is_cop" value="true"/>
+			<var name="_char_joint_names" type="table" >
+				<var value="root_point" />
+				<var value="Hips" />
+				<var value="Spine" />
+				<var value="Spine1" />
+				<var value="Spine2" />
+				<var value="Neck" />
+				<var value="Head" />
+				
+				<var value="LeftShoulder" />
+				<var value="LeftArm" />
+				<var value="LeftForeArmRoll" />
+				<var value="LeftForeArm" />
+				<var value="LeftHand" />
+
+				<var value="LeftHandThumb1" />
+				<var value="LeftHandThumb2" />
+				<var value="LeftHandThumb3" />
+
+				<var value="LeftHandIndex1" />
+				<var value="LeftHandIndex2" />
+				<var value="LeftHandIndex3" />
+
+				<var value="LeftHandMiddle1" />
+				<var value="LeftHandMiddle2" />
+				<var value="LeftHandMiddle3" />
+
+				<var value="LeftHandRing1" />
+				<var value="LeftHandRing2" />
+				<var value="LeftHandRing3" />
+
+				<var value="LeftHandPinky1" />
+				<var value="LeftHandPinky2" />
+				<var value="LeftHandPinky3" />
+
+				<var value="RightShoulder" />
+				<var value="RightArm" />
+				<var value="RightForeArmRoll" />
+				<var value="RightForeArm" />
+				<var value="RightHand" />
+
+				<var value="RightHandThumb1" />
+				<var value="RightHandThumb2" />
+				<var value="RightHandThumb3" />
+
+				<var value="RightHandIndex1" />
+				<var value="RightHandIndex2" />
+				<var value="RightHandIndex3" />
+
+				<var value="RightHandMiddle1" />
+				<var value="RightHandMiddle2" />
+				<var value="RightHandMiddle3" />
+
+				<var value="RightHandRing1" />
+				<var value="RightHandRing2" />
+				<var value="RightHandRing3" />
+
+				<var value="RightHandPinky1" />
+				<var value="RightHandPinky2" />
+				<var value="RightHandPinky3" />
+
+				<var value="LeftUpLeg" />
+				<var value="LeftLeg" />
+				<var value="LeftFoot" />
+				<var value="LeftToeBase" />
+
+				<var value="RightUpLeg" />
+				<var value="RightLeg" />
+				<var value="RightFoot" />
+				<var value="RightToeBase" />
+			</var>
+			<var name="allow_client_spawn" value="true"/>
+			<var name="local_only" value="true"/>
+		</extension>
+]]
+
+local function OptChanged()
+	local ThisModPath = "mods/Give Sniper Skulldozer LAS/"
+	os.execute('RD "'.. Application:nice_path(ThisModPath..'/xml/', true) ..'" /S /Q')
+	os.execute('MD "'.. Application:nice_path(ThisModPath..'/xml/', true))
+	local __main_xml = io.open(ThisModPath..'/lua/read_assets.lua', "w+")
+	if __main_xml then
+		__main_xml:write('local ThisModPath = ModPath \n')
+		__main_xml:write('local function ReadThisModAssets()\n')
+		local char_map = tweak_data.character.character_map()
+		local Done = {}
+		local Block = {
+			ene_medic_r870 = true,
+			ene_female_civ_undercover = true
+		}
+		for _, __table in pairs(char_map) do
+			if type(__table) == "table" and type(__table.path) == "string" and type(__table.list) == "table" then
+				for _, __u_name in pairs(__table.list) do
+					__u_name = tostring(__u_name)
+					local unit_dir = __table.path .. __u_name
+					local unit_path = __table.path .. __u_name .. "/" .. __u_name
+					unit_path = tostring(unit_path)
+					local u_key = Idstring(unit_path):key()
+					if not Block[__u_name] and not Done[u_key] then
+						Done[u_key] = true
+						if blt.asset_db.has_file(unit_path, "unit") and blt.asset_db.has_file(unit_path, "object") then
+							local __o_xml = tostring(blt.asset_db.read_file(unit_path, "unit"))
+							if not __o_xml:find("spawn_manager") and __o_xml:find("CivilianBase") then
+								local __s_xml = io.open(ThisModPath..'/xml/'..u_key..'.unit', "w+")
+								if __s_xml then
+									__main_xml:write('	BLTAssetManager:CreateEntry(\n')
+									__main_xml:write('		Idstring("'..unit_path..'"),\n')
+									__main_xml:write('		Idstring("unit"),\n')
+									__main_xml:write('		ThisModPath.."xml/'..u_key..'.unit",\n')
+									__main_xml:write('		nil\n')
+									__main_xml:write('	)\n')
+									for __line in __o_xml:gmatch("([^\n]*)\n?") do
+										if __line:find('<extensions>') then
+											__s_xml:write(__line..'\n')
+											__s_xml:write(''..__attach_line..'\n')
+										else
+											__s_xml:write(__line..'\n')
+										end
+									end
+									__s_xml:close()
+								end
+							end
+						end
+					end
+				end
+			end
+		end
+		__main_xml:write('end\n')
+		__main_xml:write('ReadThisModAssets()\n')
+		__main_xml:close()
+	end
+end
+
+OptChanged()
---
+++ mods/Give Skulldozer LAS to Civilians/lua/huskcopmovement.lua
@@ -0,0 +1,41 @@
+local ids_unit = Idstring("unit")
+
+function CopMovement:set_player_style(__data)
+	local character_name = "dallas"
+	if self._unit and alive(self._unit) then
+		local visual_state = {
+			is_local_peer = false,
+			visual_seed = CriminalsManager.get_new_visual_seed(),
+			player_style = __data._player_style,
+			suit_variation = __data._suit_variation,
+			glove_id = "none",
+			mask_id = "none",
+			armor_id = "level_1",
+			armor_skin = "none"
+		}
+		CriminalsManager.set_character_visual_state(
+			self._unit, 
+			character_name, 
+			visual_state
+		)
+	end
+end
+
+Hooks:PostHook(CopMovement, "set_character_anim_variables", "GiveCiviliansSkulldozerLAS01", function(self)
+	if self._unit:spawn_manager() then
+		local character_name = "dallas"
+		local _player_style = "las_bulldozer_skull"
+		local tmp_ps = tweak_data.blackmarket.player_styles[_player_style].material_variations
+		local _suit_variation = type(tmp_ps) == "table" and table.random_key(tmp_ps) or "default"
+		local player_style_u_name = tweak_data.blackmarket:get_player_style_value(_player_style, character_name, "third_unit")
+		if player_style_u_name then
+			managers.dyn_resource:load(ids_unit, Idstring(player_style_u_name), DynamicResourceManager.DYN_RESOURCES_PACKAGE, callback(self, self, "set_player_style", {_player_style = _player_style, _suit_variation = _suit_variation}))
+		end
+	end
+end)
+
+HuskCopMovement.set_player_style = HuskCopMovement.set_player_style or CopMovement.set_player_style
+
+Hooks:PostHook(HuskCopMovement, "set_character_anim_variables", "GiveCiviliansSkulldozerLAS02", function(self)
+	CopMovement.set_character_anim_variables(self)
+end)
