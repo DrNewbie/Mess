3845f6324929fc14641e3e2fd9049b4cd7fb8cc7
Dr_Newbie <tf2baidonation@gmail.com>
2022-08-15 22:32:51 +0800

    Piggy Bank in Main Menu[3] ; moving speed

--- mods/Piggy Bank in Main Menu/lua/Setup.lua
+++ mods/Piggy Bank in Main Menu/lua/Setup.lua
@@ -1,5 +1,12 @@
-function PBiMM:OptChanged()
+function PBiMM:is_PBiMM_alive()
 	if MenuSceneManager and MenuSceneManager.PBiMM and alive(MenuSceneManager.PBiMM) then
+		return true
+	end
+	return false
+end
+
+function PBiMM:OptChanged()
+	if PBiMM:is_PBiMM_alive() then
 		MenuSceneManager.PBiMM:set_position(Vector3(
 			PBiMM.Options:GetValue("__pos_x"),
 			PBiMM.Options:GetValue("__pos_y"),
@@ -14,11 +21,22 @@ function PBiMM:OptChanged()
 	return
 end
 
+function PBiMM:MovingSpeedChanged()
+	if PBiMM:is_PBiMM_alive() and MenuSceneManager.PBiMM:anim_state_machine() then
+		local current_state_name = MenuSceneManager.PBiMM:anim_state_machine():segment_state(Idstring("base"))
+		if current_state_name then
+			MenuSceneManager.PBiMM:anim_state_machine():set_speed(current_state_name, PBiMM.Options:GetValue("__moving_speed"))
+		end
+	end
+	return
+end
+
 function PBiMM:IsMovingChanged()
-	if MenuSceneManager.PBiMM:damage() then
+	if PBiMM:is_PBiMM_alive() and MenuSceneManager.PBiMM:damage() then
 		if PBiMM.Options:GetValue("__is_moving") then
 			if MenuSceneManager.PBiMM:damage():has_sequence("anim_pig_idle") then
 				MenuSceneManager.PBiMM:damage():run_sequence_simple("anim_pig_idle")
+				PBiMM:MovingSpeedChanged()
 			end
 		else
 			MenuSceneManager:__SpawnPBiMM()
