89abb5cd908d6a1188755e5579b0e9079a67c124
Dr_Newbie <tf2baidonation@gmail.com>
2022-09-24 13:15:39 +0800

    VD-12 Jammed

--- mods/VD-12 Jammed/shotgunbase.lua
+++ mods/VD-12 Jammed/shotgunbase.lua
@@ -3,31 +3,46 @@ local ThisModIds = Idstring(ThisModPath):key()
 local __Name = function(__id)
 	return "VDJ_"..Idstring(tostring(__id).."::"..ThisModIds):key()
 end
-local Hook1 = __Name("Hook1")
-local Hook2 = __Name("Hook2")
-local Hook3 = __Name("Hook3")
+
 local Bool1 = __Name("Bool1")
 local Time1 = __Name("Time1")
 local Speed1 = __Name("Speed1")
 local Smoke1 = __Name("Smoke1")
-local ReloadSpeedMul = 0.15
+local Jammed1 = __Name("Jammed1")
+local ReloadSpeedMul = function ()
+	return 0.15
+end
+local SmokeSpawnRate = function ()
+	if math.random() >= 0.5 then
+		return true
+	end
+	return false
+end
+local IsJammedNow = function (them)
+	if math.random() >= 0.001 and them[Time1] >= 3 then
+		return true
+	end
+	return false
+end
 
-Hooks:PreHook(ShotgunBase, "setup_default", Hook2, function(self)
+Hooks:PreHook(ShotgunBase, "setup_default", __Name("setup_default"), function(self)
 	if tostring(self._name_id) == "sko12" then
 		self[Bool1] = true
+		self[Jammed1] = false
 		self[Time1] = 0
 		self[Speed1] = self._reload or 1
 		self[Smoke1] = {}
 	end
 end)
 
-Hooks:PreHook(ShotgunBase, "_fire_raycast", Hook2, function(self)
+Hooks:PreHook(ShotgunBase, "_fire_raycast", __Name("_fire_raycast"), function(self)
 	if self[Bool1] then
 		self[Time1] = self[Time1] + 1
-		if self[Time1] == 3 then
+		if IsJammedNow(self) then
 			--[[Jammed]]
+			self[Jammed1] = true
 			self:set_ammo_remaining_in_clip(0)
-			self._reload = self._reload * ReloadSpeedMul
+			self._reload = self._reload * ReloadSpeedMul()
 			managers.explosion:play_sound_and_effects(self._unit:position(), math.UP, 100, {
 				camera_shake_max_mul = 4,
 				effect = "effects/particles/explosions/explosion_flash_grenade",
@@ -46,7 +61,7 @@ Hooks:PreHook(ShotgunBase, "_fire_raycast", Hook2, function(self)
 				sound_source = sound_source
 			}), TimerManager:game():time() + 4)
 			local __part = self._parts[table.random_key(self._parts)]
-			if math.random() >= 0.5 and __part and __part.unit and alive(__part.unit) and __part.unit.orientation_object and __part.unit:orientation_object() then
+			if SmokeSpawnRate() and __part and __part.unit and alive(__part.unit) and __part.unit.orientation_object and __part.unit:orientation_object() then
 				table.insert(self[Smoke1] ,World:effect_manager():spawn({
 					effect = Idstring("effects/payday2/particles/weapons/rpg_smoke_trail"),
 					parent = __part.unit:orientation_object()
@@ -56,8 +71,9 @@ Hooks:PreHook(ShotgunBase, "_fire_raycast", Hook2, function(self)
 	end
 end)
 
-Hooks:PreHook(ShotgunBase, "on_reload", Hook3, function(self)
-	if self[Bool1] and self[Time1] >= 3 then
+Hooks:PreHook(ShotgunBase, "on_reload", __Name("on_reload"), function(self)
+	if self[Bool1] and self[Jammed1] then
+		self[Jammed1] = false
 		self[Time1] = 0
 		self._reload = self[Speed1]
 		for __id, __data in pairs(self[Smoke1]) do
