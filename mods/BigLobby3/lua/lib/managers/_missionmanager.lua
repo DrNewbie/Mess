062bddadd907e2bb9b32e86b556ba57dff1463eb
Dr_Newbie <tf2baidonation@gmail.com>
2022-12-07 21:22:53 +0800

    .

--- mods/BigLobby3/lua/lib/managers/_missionmanager.lua
+++
@@ -1,6 +0,0 @@
-Hooks:PostHook(MissionManager, "init", "BigLobbyModifyAddMoreBots", function()
-	if BigLobbyGlobals.allow_more_bots_settings then
-		CriminalsManager.MAX_NR_TEAM_AI = BigLobbyGlobals:num_bot_slots()
-	end
-	CriminalsManager.MAX_NR_CRIMINALS = BigLobbyGlobals:num_player_slots()
-end)
