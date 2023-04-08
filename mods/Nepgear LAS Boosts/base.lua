4a08542cece77f5de93145b7c1d457e84ac02d00
Dr_Newbie <tf2baidonation@gmail.com>
2023-04-08 17:33:36 +0800

    Nepgear LAS Boosts[2]

--- mods/Nepgear LAS Boosts/base.lua
+++ mods/Nepgear LAS Boosts/base.lua
@@ -78,25 +78,42 @@ if PlayerManager and not PlayerManager[is_bool] then
 		end
 		return Ans
 	end
-end
-
-if GroupAIStateBase and not GroupAIStateBase[is_bool] then
-	GroupAIStateBase[is_bool] = true
-	Hooks:PreHook(GroupAIStateBase, "report_criminal_downed", __Name("report_criminal_downed"), function(self, r_unit)
-		if (r_unit:interaction() and r_unit:interaction():active()) or (r_unit:character_damage() and (r_unit:character_damage():need_revive() or r_unit:character_damage():arrested())) then
-			if managers.player and managers.player:Is_LAS_Nepgear() and managers.player:local_player() then
-				local ply_m = managers.player
-				local ply_u = ply_m:local_player()
-				if r_unit ~= ply_u and ply_u:character_damage() then
-					ply_u:character_damage():band_aid_health()
-					ply_u:character_damage():_regenerate_armor()
-					if ply_m:has_active_timer("replenish_grenades") then
-						ply_m:start_timer("replenish_grenades", 0.1, callback(ply_m, ply_m, "_on_grenade_cooldown_end"))
-						managers.hud:set_player_grenade_cooldown({
-							end_time = managers.game_play_central:get_heist_timer() + 0.1,
-							duration = 0.1
-						})
+	
+	local last_down_count = 0
+	
+	Hooks:PreHook(PlayerManager, "update", __Name("loop check player down"), function(self, t, dt)
+		if managers.player and managers.player:Is_LAS_Nepgear() and managers.player:local_player() then
+			local ply_m = managers.player
+			local ply_u = ply_m:local_player()
+			local down_count = 0
+			local __all_player_criminals = managers.groupai:state():all_player_criminals()
+			local __all_AI_criminals = managers.groupai:state():all_AI_criminals()
+			for c_key, c_data in pairs(__all_player_criminals) do
+				local r_unit = c_data.unit
+				if (r_unit:interaction() and r_unit:interaction():active()) or (r_unit:character_damage() and (r_unit:character_damage():need_revive() or r_unit:character_damage():arrested())) then
+					if r_unit ~= ply_u and ply_u:character_damage() then
+						down_count = down_count + 1
 					end
+				end
+			end
+			for c_key, c_data in pairs(__all_AI_criminals) do
+				local r_unit = c_data.unit
+				if (r_unit:interaction() and r_unit:interaction():active()) or (r_unit:character_damage() and (r_unit:character_damage():need_revive() or r_unit:character_damage():arrested())) then
+					if r_unit ~= ply_u and ply_u:character_damage() then
+						down_count = down_count + 1
+					end
+				end
+			end
+			if last_down_count ~= down_count and down_count > 0 then
+				last_down_count = down_count
+				ply_u:character_damage():band_aid_health()
+				ply_u:character_damage():_regenerate_armor()
+				if ply_m:has_active_timer("replenish_grenades") then
+					ply_m:start_timer("replenish_grenades", 0.1, callback(ply_m, ply_m, "_on_grenade_cooldown_end"))
+					managers.hud:set_player_grenade_cooldown({
+						end_time = managers.game_play_central:get_heist_timer() + 0.1,
+						duration = 0.1
+					})
 				end
 			end
 		end
