48e0fa0c8a6c045b2f454c0edf033ee292cf1318
Dr_Newbie <tf2baidonation@gmail.com>
2023-05-20 17:30:48 +0800

    Lowered Weapons[13] , Carry state

--- mods/Lowered Weapons/lua/playerstandard.lua
+++ mods/Lowered Weapons/lua/playerstandard.lua
@@ -3,13 +3,15 @@ local ThisSavePath = SavePath
 local function __Name(__text)
 	return "LLW_"..Idstring(tostring(__text)..ThisModPath):key()
 end
+
 local __dt1 = __Name("__dt1")
+local init1 = __Name("init1")
+local init2 = __Name("init2")
 local bool1 = __Name("bool1")
 local bool2 = __Name("bool2")
-local func1 = __Name("func1")
 local is_LLWepF = type(LLWepF) == "table" and type(LLWepF.Options) == "table" and type(LLWepF.Options.GetValue) == "function"
 
-PlayerStandard[func1] = PlayerStandard[func1] or function(them, weap_base)
+local function __apply_lowered_weapon(them, weap_base)
 	local weapon_id = weap_base:get_name_id()
 	local weapon_tweak_data = tweak_data.weapon[weapon_id]
 	local __camera_base = them._camera_unit:base()
@@ -35,7 +37,7 @@ PlayerStandard[func1] = PlayerStandard[func1] or function(them, weap_base)
 		duration = LLWepF.Options:GetValue("__time_speed")
 	end
 	if disable_custom_wep and weapon_tweak_data.custom then
-		return
+		return them
 	end
 	if type(LLWepF.AddonCfg) == "table" and #(LLWepF.AddonCfg) > 0 then
 		for _, __cfg in pairs(LLWepF.AddonCfg) do
@@ -73,6 +75,7 @@ PlayerStandard[func1] = PlayerStandard[func1] or function(them, weap_base)
 		duration_multiplier, 
 		duration
 	)
+	return them
 end
 
 local function __check_is_aiming_at_enemies_or_civilians(__camera)
@@ -88,96 +91,126 @@ end
 
 local __whisper_mode = __Name("__whisper_mode")
 
-Hooks:PostHook(PlayerStandard, "_update_check_actions", __Name("hook1"), function(self, __t, __dt)
-	if self._camera_unit and alive(self._camera_unit) and self._camera_unit:base() and self._equipped_unit and alive(self._equipped_unit) and self._equipped_unit:base() then
+local function __loop_function_1(them, __t, __dt)
+	if them._camera_unit and alive(them._camera_unit) and them._camera_unit:base() and them._equipped_unit and alive(them._equipped_unit) and them._equipped_unit:base() then
 		local __assault_disable_lowered = is_LLWepF and LLWepF.Options:GetValue("__assault_disable_lowered") or false
 		local is_whisper_mode = managers.groupai:state():whisper_mode()
 		if __assault_disable_lowered and not is_whisper_mode then
-			if (type(self[__whisper_mode]) ~= type(is_whisper_mode)) or (self[__whisper_mode] ~= is_whisper_mode) then
-				self[__whisper_mode] = is_whisper_mode
-				self:_stance_entered(false)
+			if (type(them[__whisper_mode]) ~= type(is_whisper_mode)) or (them[__whisper_mode] ~= is_whisper_mode) then
+				them[__whisper_mode] = is_whisper_mode
+				them:_stance_entered(false)
 			end
-			self[__dt1] = 1
+			them[__dt1] = 1
 		end
-		local __weap_base = self._equipped_unit:base()
-		local __action_forbidden = self._shooting or 
-									self:_is_reloading() or 
-									self:in_steelsight() or 
-									self:_changing_weapon() or 
-									self:_is_meleeing() or 
-									self._use_item_expire_t or 
-									self:_interacting() or 
-									self:_is_throwing_projectile() or 
-									self:_is_deploying_bipod() or 
-									self._menu_closed_fire_cooldown > 0 or 
-									self:is_switching_stances() or 
-									self:_is_cash_inspecting() or --observing the weapon
-									__check_is_aiming_at_enemies_or_civilians(self._unit:camera()) --thanks to Hoppip
-		if not self[__dt1] or __action_forbidden or not __weap_base:start_shooting_allowed() then
-			self[__dt1] = is_LLWepF and LLWepF.Options:GetValue("__time_delay") or 3
-			if self[bool1] then
-				self:_stance_entered(false)
+		local __weap_base = them._equipped_unit:base()
+		local __action_forbidden = them._shooting or 
+									them:_is_reloading() or 
+									them:in_steelsight() or 
+									them:_changing_weapon() or 
+									them:_is_meleeing() or 
+									them._use_item_expire_t or 
+									them:_interacting() or 
+									them:_is_throwing_projectile() or 
+									them:_is_deploying_bipod() or 
+									them._menu_closed_fire_cooldown > 0 or 
+									them:is_switching_stances() or 
+									them:_is_cash_inspecting() or --observing the weapon
+									__check_is_aiming_at_enemies_or_civilians(them._unit:camera()) --thanks to Hoppip
+		if not them[__dt1] or __action_forbidden or not __weap_base:start_shooting_allowed() then
+			them[__dt1] = is_LLWepF and LLWepF.Options:GetValue("__time_delay") or 3
+			if them[bool1] then
+				them:_stance_entered(false)
 			end
-			self[bool1] = false
-			self[bool2] = self[bool2] or {}
+			them[bool1] = false
+			them[bool2] = them[bool2] or {}
 			--auto turn gadget on
-			if self[bool2][self._equipped_unit:key()] then
-				local __last_gadget_idx = self[bool2][self._equipped_unit:key()] or 1
-				self[bool2][self._equipped_unit:key()] = nil
+			if them[bool2][them._equipped_unit:key()] then
+				local __last_gadget_idx = them[bool2][them._equipped_unit:key()] or 1
+				them[bool2][them._equipped_unit:key()] = nil
 				__weap_base:set_gadget_on(__last_gadget_idx, false)
 			end
 		else
-			if LLWepF.ForcedApplyToFov or (self[__dt1] <= 0 and not self[bool1]) then
+			if LLWepF.ForcedApplyToFov or (them[__dt1] <= 0 and not them[bool1]) then
 				LLWepF.ForcedApplyToFov = false
-				self[bool1] = true
+				them[bool1] = true
 				--auto turn gadget off
 				if is_LLWepF and LLWepF.Options:GetValue("__auto_gadget_off") and __weap_base.set_gadget_on and __weap_base:has_gadget() and __weap_base:was_gadget_on() then
-					self[bool2][self._equipped_unit:key()] = __weap_base:current_gadget_index() or 1
+					them[bool2][them._equipped_unit:key()] = __weap_base:current_gadget_index() or 1
 					__weap_base:gadget_off()
 				end
-				self[func1](self, __weap_base)
+				them = __apply_lowered_weapon(them, __weap_base)
 			else
-				self[__dt1] = self[__dt1] - __dt
+				them[__dt1] = them[__dt1] - __dt
 			end		
 		end
 	end
-end)
+	return them
+end
 
-Hooks:PostHook(PlayerStandard, "_update_check_actions", __Name("hook2"), function(self, __t, __dt)
-	if Utils and self._camera_unit and alive(self._camera_unit) and self._camera_unit:base() and self._equipped_unit and alive(self._equipped_unit) and self._equipped_unit:base() then
+local function __loop_function_2(them, __t, __dt)
+	if Utils and them._camera_unit and alive(them._camera_unit) and them._camera_unit:base() and them._equipped_unit and alive(them._equipped_unit) and them._equipped_unit:base() then
 		local too_close_dis = is_LLWepF and LLWepF.Options:GetValue("__too_close") or 50
 		if too_close_dis > 0 then
 			local is_too_close = Utils:GetPlayerAimPos(managers.player:player_unit(), too_close_dis) or nil
-			if is_too_close and not self[bool1] then
+			if is_too_close and not them[bool1] then
 				LLWepF.ForcedApplyToFov = true
 			end
 		end
 	end
-end)
+	return them
+end
 
-Hooks:PostHook(PlayerStandard, "_activate_mover", __Name("hook3"), function(self, __mover, ...)
+local function __loop_function_3(them, __mover)
 	if __mover == PlayerStandard.MOVER_STAND or __mover == PlayerStandard.MOVER_DUCK then
-		self[bool1] = false
-		self[__dt1] = 1
+		them[bool1] = false
+		them[__dt1] = 1
 	end
-end)
+	return them
+end
 
 local function LLWepFLoadAddonCfg()
+	local io_file = file
 	LLWepF.AddonCfg = LLWepF.AddonCfg or {}
-	local configs = file.GetFiles(ThisModPath.."/cfg/")
+	local configs = io_file.GetFiles(ThisModPath.."/cfg/")
 	if type(configs) == "table" then
 		for i, cfg in pairs(configs) do
 			dofile(ThisModPath.."/cfg/"..cfg)
 		end
 	end
-	if file.DirectoryExists(ThisSavePath.."/LLWepF CFG/") then
-		configs = file.GetFiles(ThisSavePath.."/LLWepF CFG/")
+	if io_file.DirectoryExists(ThisSavePath.."/LLWepF CFG/") then
+		configs = io_file.GetFiles(ThisSavePath.."/LLWepF CFG/")
 		if type(configs) == "table" then
 			for i, cfg in pairs(configs) do
 				dofile(ThisSavePath.."/LLWepF CFG/"..cfg)
 			end
 		end
 	end
+end
+
+if PlayerStandard and not PlayerStandard[init1] then
+	PlayerStandard[init1] = true
+	
+	Hooks:PostHook(PlayerStandard, "_update_check_actions", __Name("hook1"), function(self, __t, __dt)
+		self = __loop_function_1(self, __t, __dt)
+		self = __loop_function_2(self, __t, __dt)
+	end)
+
+	Hooks:PostHook(PlayerStandard, "_activate_mover", __Name("hook2"), function(self, __mover, ...)
+		self = __loop_function_3(self, __mover)
+	end)
+end
+
+if PlayerCarry and not PlayerCarry[init2] then
+	PlayerCarry[init2] = true
+	
+	Hooks:PostHook(PlayerCarry, "_update_check_actions", __Name("hook3"), function(self, __t, __dt)
+		self = __loop_function_1(self, __t, __dt)
+		self = __loop_function_2(self, __t, __dt)
+	end)
+
+	Hooks:PostHook(PlayerCarry, "_activate_mover", __Name("hook4"), function(self, __mover, ...)
+		self = __loop_function_3(self, __mover)
+	end)
 end
 
 LLWepFLoadAddonCfg()
