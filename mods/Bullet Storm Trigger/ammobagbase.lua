f8f639ac5d81f4b6bd2e1c8c6a3874c121059c04
Dr_Newbie <tf2baidonation@gmail.com>
2023-07-27 17:11:32 +0800

    Bullet Storm Trigger[3] ; Saw

--- mods/Bullet Storm Trigger/ammobagbase.lua
+++ mods/Bullet Storm Trigger/ammobagbase.lua
@@ -7,25 +7,43 @@ end
 local func1 = __Name("take_ammo")
 local msgr1 = __Name("OnWeaponFired")
 local bullet_storm_hold = __Name("bullet_storm_hold")
+local is_bool = __Name("is_bool")
 
-AmmoBagBase[func1] = AmmoBagBase[func1] or AmmoBagBase.take_ammo
-
-AmmoBagBase[bullet_storm_hold] = AmmoBagBase[func1] or false
-
-function AmmoBagBase:take_ammo(__unit, ...)
-	local __interacted, __bullet_storm = self[func1](self, __unit, ...)
-	if managers.player and __unit and alive(__unit) and __unit == managers.player:player_unit() then
-		if __bullet_storm and type(__bullet_storm) == "number" and __bullet_storm > 0 then
-			AmmoBagBase[bullet_storm_hold] = __bullet_storm
-			__bullet_storm = false
-			managers.player:unregister_message(Message.OnWeaponFired, msgr1)
-			managers.player:register_message(Message.OnWeaponFired, msgr1, function()
-				if managers.player and AmmoBagBase[bullet_storm_hold] and type(AmmoBagBase[bullet_storm_hold]) == "number" and AmmoBagBase[bullet_storm_hold] > 0 then
-					managers.player:add_to_temporary_property("bullet_storm", AmmoBagBase[bullet_storm_hold], 1)
-					AmmoBagBase[bullet_storm_hold] = false				
-				end
-			end)
-		end
+local function __Run()
+	if managers.player and AmmoBagBase and type(AmmoBagBase[bullet_storm_hold]) == "number" and AmmoBagBase[bullet_storm_hold] > 0 then
+		managers.player:add_to_temporary_property("bullet_storm", AmmoBagBase[bullet_storm_hold], 1)
+		AmmoBagBase[bullet_storm_hold] = false				
 	end
-	return __interacted, __bullet_storm
+	return
+end
+
+if AmmoBagBase and not AmmoBagBase[is_bool] then
+	AmmoBagBase[is_bool] = true
+
+	AmmoBagBase[func1] = AmmoBagBase[func1] or AmmoBagBase.take_ammo
+
+	AmmoBagBase[bullet_storm_hold] = AmmoBagBase[func1] or false
+
+	function AmmoBagBase:take_ammo(__unit, ...)
+		local __interacted, __bullet_storm = self[func1](self, __unit, ...)
+		if managers.player and __unit and alive(__unit) and __unit == managers.player:player_unit() then
+			if __bullet_storm and type(__bullet_storm) == "number" and __bullet_storm > 0 then
+				AmmoBagBase[bullet_storm_hold] = __bullet_storm
+				__bullet_storm = false
+				managers.player:unregister_message(Message.OnWeaponFired, msgr1)
+				managers.player:register_message(Message.OnWeaponFired, msgr1, function()
+					pcall(__Run)
+				end)
+			end
+		end
+		return __interacted, __bullet_storm
+	end
+end
+
+if SawHit and not SawHit[is_bool] then
+	SawHit[is_bool] = true
+	
+	Hooks:PostHook(SawHit, "on_collision", __Name("SawHit::on_collision"), function(...)
+		pcall(__Run)
+	end)
 end
