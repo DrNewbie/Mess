0dc733a9af43418f4a1dd8c6a4a33eec90bb5269
Dr_Newbie <tf2baidonation@gmail.com>
2024-09-28 11:38:03 +0800

    Buy Random Weapon[2]

--- mods/Buy Random Weapon/blackmarketgui.lua
+++ mods/Buy Random Weapon/blackmarketgui.lua
@@ -9,16 +9,12 @@ local Hook0 = __Name("_G::")
 if _G[Hook0] then return end
 _G[Hook0] = true
 
-local Hook2 = __Name("populate_weapon_category::")
-local Hook3 = __Name("LocalizationManagerPostInit::")
 local Func1 = __Name("buy random weapon func::")
 local Func2 = __Name("buy random weapon btns::")
-local Func3 = __Name("buy random weapon name::")
+local Func3 = "bm_menu_buy_random_weapon_013ad61fa19195d33c42b5dda6bfb31dcbce8fd3"
 
-Hooks:Add("LocalizationManagerPostInit", Hook3, function()
-	LocalizationManager:add_localized_strings({
-		[Func3] = "Buy Random Weapon"
-	})
+Hooks:Add("LocalizationManagerPostInit", __Name("load loc file"), function(self)
+	self:load_localization_file(ThisModPath .. "loc_en.txt")
 end)
 
 Hooks:PostHook(BlackMarketGui, "_setup", Hook1, function(self, ...)
@@ -35,18 +31,31 @@ Hooks:PostHook(BlackMarketGui, "_setup", Hook1, function(self, ...)
 		if not data.empty_slot or not data.unlocked or type(data.slot) ~= "number" then
 			return
 		end
-		if type(data.category) ~= "string" or (data.category ~= "primaries" and data.category ~= "secondary") then
+		if type(data.category) ~= "string" or (data.category ~= "primaries" and data.category ~= "secondaries") then
 			return
 		end
-		local _weapon_list = tweak_data.statistics._weapon_list
-		if type(_weapon_list) ~= "table" then
+		if type(tweak_data.statistics._weapon_list) ~= "table" then
 			return
 		end
+		local _weapon_list = table.sorted_copy(tweak_data.statistics._weapon_list)
 		local _weapon_list_safe = {}
-		for id, wep_id in pairs(_weapon_list) do
-			if Global.blackmarket_manager.weapons[wep_id] and managers.blackmarket:weapon_unlocked(wep_id) then
+		for _, wep_id in pairs(_weapon_list) do
+			if Global.blackmarket_manager.weapons[wep_id] and managers.blackmarket:weapon_unlocked(wep_id) and managers.money:can_afford_weapon(wep_id) then
 				table.insert(_weapon_list_safe, wep_id)
 			end
+		end
+		if table.empty(_weapon_list_safe) or #_weapon_list_safe <= 0 then			
+			QuickMenu:new(
+				managers.localization:to_upper_text(Func3),
+				managers.localization:text("bm_menu_not_enough_cash"),
+				{
+					{
+						text = managers.localization:text("dialog_cancel"),
+						is_cancel_button = true
+					}
+				}
+			):Show()
+			return
 		end
 		local _weapon_id = table.random(_weapon_list_safe)
 		self._item_bought = true
@@ -64,7 +73,7 @@ Hooks:PostHook(BlackMarketGui, "_setup", Hook1, function(self, ...)
 	}, 10)
 end)
 
-Hooks:PostHook(BlackMarketGui, "populate_weapon_category_new", Hook2, function(self, data, ...)
+Hooks:PostHook(BlackMarketGui, "populate_weapon_category_new", __Name("populate_weapon_category::"), function(self, data, ...)
 	for k, v in pairs(data) do
 		if data[k] and type(v) == "table" and v.unlocked and tostring(json.encode({v = v})):find("ew_buy") then
 			table.insert(data[k], Func2)
