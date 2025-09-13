835bd5a6dd6ef5444001b8bf0724523c95c4c227
Dr_Newbie <tf2baidonation@gmail.com>
2025-09-13 13:06:39 +0800

    Tag Team Through Walls[2]

--- mods/tag team through walls/entry.lua
+++ mods/tag team through walls/entry.lua
@@ -1,36 +1,49 @@
-local old_attempt_tag_team = PlayerManager._attempt_tag_team
-
-function PlayerManager:_attempt_tag_team(...)
-	local ans = old_attempt_tag_team(self, ...)
-	if not ans and not self._coroutine_mgr:is_running("tag_team") then
-		local player = managers.player:player_unit()
-		local criminals = managers.groupai:state():all_criminals()
-		if player and not table.empty(criminals) then
-			local player_eye = player:camera():position()
-			local player_fwd = player:camera():rotation():y()
-			local tagged = nil
-			local tag_distance = managers.player:upgrade_value("player", "tag_team_base").distance * 100			
-			for u_key, u_data in pairs(criminals) do
-				local unit = u_data.unit
-				if alive(unit) and unit:movement() then
-					if mvector3.distance_sq(player_eye, unit:movement():m_pos()) < tag_distance * tag_distance then
-						local cam_fwd = player_fwd
-						local vec = unit:movement():m_pos() - player_eye
-						local dis = mvector3.normalize(vec)
-						local max_angle = math.max(8, math.lerp(10, 30, dis / 1200))
-						local angle = vec:angle(cam_fwd)
-						if angle < max_angle or math.abs(max_angle - angle) < 10 then
-							tagged = unit
-							break
-						end				
-					end
+local function __check_and_run(__them, __player, __criminals)
+	local tagged = nil
+	if __player and not table.empty(__criminals) then
+		local player_eye = __player:camera():position()
+		local player_fwd = __player:camera():rotation():y()
+		local tag_distance = managers.player:upgrade_value("player", "tag_team_base").distance * 100			
+		for u_key, u_data in pairs(__criminals) do
+			local unit = nil
+			if type(u_data.unit) == "userdata" then
+				unit = u_data.unit
+			elseif type(u_data) == "userdata" then
+				unit = u_data
+			end
+			if type(unit) == "userdata" and alive(unit) and unit:position() then
+				if mvector3.distance_sq(player_eye, unit:position()) < tag_distance * tag_distance then
+					local cam_fwd = player_fwd
+					local vec = unit:position() - player_eye
+					local dis = mvector3.normalize(vec)
+					local max_angle = math.max(8, math.lerp(10, 30, dis / 1200))
+					local angle = vec:angle(cam_fwd)
+					if angle < max_angle or math.abs(max_angle - angle) < 10 then
+						tagged = unit
+						break
+					end				
 				end
 			end
-			if tagged and not self._coroutine_mgr:is_running("tag_team") then
-				self:add_coroutine("tag_team", PlayerAction.TagTeam, tagged, player)
-				return true
-			end			
+		end
+		if tagged and not __them._coroutine_mgr:is_running("tag_team") then
+			__them:add_coroutine("tag_team", PlayerAction.TagTeam, tagged, __player)
+			return true
+		end			
+	end
+	return false
+end
+
+Hooks:PostHook(PlayerManager, "_attempt_tag_team", "Tag Team Through Walls", function(self, ...)
+	local ans = Hooks:GetReturn()
+	if not ans and not self._coroutine_mgr:is_running("tag_team") then
+		local player = managers.player:player_unit()
+		local all_criminals = managers.groupai:state():all_criminals()
+		local all_converted_enemies = managers.groupai:state():all_converted_enemies()
+		if __check_and_run(self, player, all_criminals) then
+			return true
+		elseif __check_and_run(self, player, all_converted_enemies) then
+			return true
 		end
 	end
 	return ans
-end
+end)
