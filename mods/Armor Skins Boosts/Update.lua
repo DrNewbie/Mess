9f873ae3f8ee8214d63f3d6d0052bf1a50ff556f
Dr_Newbie <tf2baidonation@gmail.com>
2025-10-31 23:23:11 +0800

    Armor Skins Boosts
    https://modworkshop.net/mod/20721

--- mods/Armor Skins Boosts/Update.lua
+++
@@ -1,9 +0,0 @@
-if not ModCore then
-	log("[ERROR] BeardLib is not installed!")
-else
-	ModCore:new(ModPath .. "Config.xml", false, true):init_modules()
-end
-
-if Announcer then
-	Announcer:AddHostMod('Armor Skins Boosts, (http://modwork.shop/20721)')
-end
