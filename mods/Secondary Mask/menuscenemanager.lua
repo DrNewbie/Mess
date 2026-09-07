5fd5c3bf41de68a471c26090caa2caa089bcd2b5
Dr_Newbie <tf2baidonation@gmail.com>
2026-09-07 23:15:01 +0800

    Secondary Mask[3] ; fix and support more mask

--- mods/Secondary Mask/menuscenemanager.lua
+++ mods/Secondary Mask/menuscenemanager.lua
@@ -1,107 +1,71 @@
-local ids_unit = Idstring("unit")
+local mask_data_ready_to_apply = {}
+local mask_unit_ready_to_remove = {}
 
-function MenuSceneManager:set_character_mask_2_by_id(mask_id, blueprint, unit, peer_id, character_name)
-	self._mask_2_units = self._mask_2_units or {}
-	
-	mask_id = managers.blackmarket:get_real_mask_id(mask_id, peer_id, character_name)
-	
-	local unit_name = managers.blackmarket:mask_unit_name_by_mask_id(mask_id, peer_id, character_name)
-	
+function MenuSceneManager:apply_character_mask_2()
+	if type(mask_data_ready_to_apply) ~= "table" then
+		return
+	end
+	if table.empty(mask_data_ready_to_apply) then
+		return
+	end
+	if not alive(self._character_unit) then
+		return
+	end
+	for this_key, use_this_mask_data in pairs(mask_data_ready_to_apply) do
+		if type(use_this_mask_data) == "table" and not table.empty(use_this_mask_data) then
+			local owner_unit = self._character_unit
+			use_this_mask_data.owner_unit = owner_unit
+			local unit_name = use_this_mask_data.unit_name
+			managers.dyn_resource:load(Idstring("unit"), unit_name, DynamicResourceManager.DYN_RESOURCES_PACKAGE, function()
+				local mask_id = use_this_mask_data.mask_id
+				local blueprint = use_this_mask_data.blueprint
+				local u_name_key = use_this_mask_data.u_name_key
+				local mask_align = owner_unit:get_object(Idstring("Head"))
+				local mask_unit = self:_spawn_mask(unit_name, false, mask_align:position(), mask_align:rotation(), mask_id)
+				if mask_unit and alive(mask_unit) then
+					mask_unit:base():apply_blueprint(blueprint, function()
+						return
+					end)
+					owner_unit:link(mask_align:name(), mask_unit)
+					use_this_mask_data.mask_unit = mask_unit
+					use_this_mask_data.mask_align = mask_align
+					use_this_mask_data.character_name = self._player_character_name
+					use_this_mask_data.peer_id = nil
+					self:update_mask_offset(use_this_mask_data)
+					self:_chk_character_visibility(owner_unit)
+				end
+				mask_data_ready_to_apply[this_key] = nil
+				table.insert(mask_unit_ready_to_remove, mask_unit)
+			end)
+		end
+	end
+end
+
+Hooks:PostHook(MenuSceneManager, "_set_player_character_unit", "2Mask_MenuSceneManager_ReNewUnit", function(self)
+	for this_key, remove_this_mask_unit in pairs(mask_unit_ready_to_remove) do
+		if remove_this_mask_unit and alive(remove_this_mask_unit) then
+			for _, linked_unit in ipairs(remove_this_mask_unit:children()) do
+				linked_unit:unlink()
+				World:delete_unit(linked_unit)
+			end
+			remove_this_mask_unit:unlink()
+			World:delete_unit(remove_this_mask_unit)
+		end
+		mask_unit_ready_to_remove[this_key] = nil
+	end
+	self:add_one_frame_delayed_clbk(callback(self, self, "apply_character_mask_2"))
+end)
+
+function MenuSceneManager:set_character_mask_2_by_id(mask_id, blueprint)
+	mask_id = managers.blackmarket:get_real_mask_id(mask_id)	
+	local unit_name = managers.blackmarket:mask_unit_name_by_mask_id(mask_id)
 	unit_name = Idstring(unit_name)
-
-	local owner_unit = unit or self._character_unit
-	
-	local mask_data = {
+	local owner_unit = self._character_unit
+	table.insert(mask_data_ready_to_apply, {
 		owner_unit = owner_unit,
 		unit_name = unit_name,
 		mask_id = mask_id,
 		blueprint = blueprint,
-		peer_id = peer_id,
-		character_name = character_name,
 		u_name_key = tostring(owner_unit:name():key())
-	}
-	
-	managers.dyn_resource:load(Idstring("unit"), unit_name, DynamicResourceManager.DYN_RESOURCES_PACKAGE, callback(self, self, "clbk_mask_unit_2_loaded", mask_data))
-	
-	owner_unit:base():request_cosmetics_update()
-end
-
-function MenuSceneManager:clbk_mask_unit_2_loaded(mask_data_param, status, asset_type, asset_name)
-	if not alive(mask_data_param.owner_unit) then
-		return
-	end
-	
-	local owner_unit = mask_data_param.owner_unit
-	local unit_name = mask_data_param.unit_name
-	local mask_id = mask_data_param.mask_id
-	local blueprint = mask_data_param.blueprint
-	local u_name_key = mask_data_param.u_name_key
-
-	local mask_align = owner_unit:get_object(Idstring("Head"))
-	local mask_unit = self:_spawn_mask(unit_name, false, mask_align:position(), mask_align:rotation(), mask_id)
-	
-	if mask_unit and alive(mask_unit) then	
-		if blueprint then
-			mask_unit:base():apply_blueprint(blueprint, function ()
-			end)
-		end
-			
-		self._mask_2_units[u_name_key] = {
-			mask_unit = mask_unit,
-			owner_unit = owner_unit,
-			mask_id = mask_id
-		}
-		
-		owner_unit:link(mask_align:name(), mask_unit)
-		
-		mask_data_param.mask_unit = mask_unit
-		mask_data_param.mask_align = mask_align
-		self:update_mask_offset(mask_data_param)
-	end
-end
-
-Hooks:PostHook(MenuSceneManager, "set_character_mask", "2Mask_BlackMarketGui_ReLink", function(self)
-	if type(self._mask_2_units) == "table" then
-		for u_name_key, linked in pairs(self._mask_2_units) do
-			if linked.mask_unit and alive(linked.mask_unit) and not alive(linked.owner_unit) then
-				local do_remove = true
-				for i, o_linked in pairs(self._mask_units) do
-					if o_linked.unit and alive(o_linked.unit) and o_linked.unit:name():key() == u_name_key then						
-						if o_linked.unit and alive(o_linked.unit) then
-							local mask_align = o_linked.unit:get_object(Idstring("Head"))
-							if mask_align then
-								o_linked.unit:link(mask_align:name(), linked.mask_unit)
-								self:update_mask_offset({
-									mask_unit = linked.mask_unit,
-									mask_id = linked.mask_id,
-									mask_align = mask_align,
-									character_name = o_linked.character_name,
-									peer_id = o_linked.peer_id
-								})
-								do_remove = false
-								break
-							end
-						end
-					end
-				end
-				if do_remove then
-					for _, linked_unit in ipairs(linked.mask_unit:children()) do
-						if linked_unit and alive(linked_unit) then
-							linked_unit:unlink()
-							World:delete_unit(linked_unit)
-						end
-					end
-					linked.mask_unit:unlink()
-					World:delete_unit(linked.mask_unit)
-					self._mask_2_units[u_name_key] = nil
-				end
-			end
-		end
-	end
-end)
-
-local SecondaryMask_ModPath = ModPath
-
-if ModCore then
-	ModCore:new(SecondaryMask_ModPath .. "Config.xml", true, true)
+	})
 end
