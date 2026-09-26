63e540ebfb273e89bb673681abd451a6c5f99234
Dr_Newbie <tf2baidonation@gmail.com>
2026-09-26 22:28:55 +0800

    Holdout_Wave_Reward[5] ; use fake item

--- mods/Holdout_Wave_Reward/Lua/missionscriptelement.lua
+++ mods/Holdout_Wave_Reward/Lua/missionscriptelement.lua
@@ -1,9 +1,15 @@
 core:import("CoreMissionScriptElement")
 core:import("CoreClass")
 
+local ThisModPath = ModPath
+
+local __Name = function(__id)
+	return "SS_"..Idstring(tostring(__id).."::"..ThisModPath):key()
+end
+
 if not MissionScriptElement then return end
 
-Hooks:PostHook(MissionScriptElement, "on_executed", "F_"..Idstring("PostHook:MissionScriptElement:on_executed:Holdout Wave Reward"):key(), function(self)
+Hooks:PostHook(MissionScriptElement, "on_executed", __Name("on_executed"), function(self)
 	if managers and managers.skirmish and managers.skirmish:is_skirmish() then
 		if self._editor_name == "hostage_interaction_link" then
 			managers.skirmish:Spawn_Holdout_Wave_Reward()
--- mods/Holdout_Wave_Reward/Lua/skirmishmanager.lua
+++ mods/Holdout_Wave_Reward/Lua/skirmishmanager.lua
@@ -1,7 +1,3 @@
-local __ammo_bag_unit_name = Idstring("units/payday2/equipment/gen_equipment_ammobag/gen_equipment_ammobag")
-local __medic_bag_unit_name = Idstring("units/payday2/equipment/gen_equipment_medicbag/gen_equipment_medicbag")
-local __grenade_crate_unit_name = Idstring("units/payday2/equipment/gen_equipment_grenade_crate/gen_equipment_grenade_crate")
-
 function SkirmishManager:Spawn_Holdout_Wave_Reward()
 	if not self:is_skirmish() then
 		
@@ -9,35 +5,23 @@ function SkirmishManager:Spawn_Holdout_Wave_Reward()
 		if (Network and Network:is_server()) or Global.game_settings.single_player then
 			local __civilians = World:find_units_quick("all", managers.slot:get_mask("civilians"))
 			if __civilians then
-				local __spawn_ammo_bag = function(__pos, __rot)				
-					local __spawn_unit = AmmoBagBase.spawn(__pos + Vector3(100, 0, 0), __rot, 0)
-					if __spawn_unit then
-						table.insert(self.__Wave_Rewards, __spawn_unit)
-					end
+				local __spawn_ammo_bag = function(__pos, __rot)
+					ThisFakeAmmoBagBase.spawn(__pos + Vector3(100, 0, 0), Rotation(), nil, nil, 30)
 					return
 				end
 				local __spawn_medic_bag = function(__pos, __rot)				
-					local __spawn_unit = DoctorBagBase.spawn(__pos + Vector3(-100, 0, 0), __rot, 0)
-					if __spawn_unit then
-						table.insert(self.__Wave_Rewards, __spawn_unit)
-					end
+					ThisFakeDoctorBagBase.spawn(__pos + Vector3(0, 100, 0), Rotation(), nil, 30)
 					return
 				end
 				local __spawn_grenade_crate = function(__pos, __rot)				
-					local __spawn_unit = GrenadeCrateBase.spawn(__pos + Vector3(-100, -100, 0), __rot)
-					if __spawn_unit then
-						table.insert(self.__Wave_Rewards, __spawn_unit)
-						if __spawn_unit:interaction() then
-							__spawn_unit:interaction():interact(managers.player:player_unit())
-						end
-					end
+					ThisFakeGrenadeCrateBase.spawn(__pos + Vector3(-100, 0, 0), Rotation(), 30)
 					return
 				end
 				for __, __unit in pairs(__civilians) do
 					if __unit and alive(__unit) then
-						__spawn_ammo_bag(__unit:position(), __unit:rotation())
-						__spawn_medic_bag(__unit:position(), __unit:rotation())
-						__spawn_grenade_crate(__unit:position(), __unit:rotation())
+						pcall(__spawn_ammo_bag, __unit:position())
+						pcall(__spawn_medic_bag, __unit:position())
+						pcall(__spawn_grenade_crate, __unit:position())
 					end
 				end
 			end
@@ -45,37 +29,3 @@ function SkirmishManager:Spawn_Holdout_Wave_Reward()
 	end
 	return
 end
-
-function SkirmishManager:Remove_Holdout_Wave_Reward()
-	if not self:is_skirmish() then
-	
-	else
-		self.__Wave_Rewards = self.__Wave_Rewards or {}
-		for __, __unit in pairs(self.__Wave_Rewards) do
-			if __unit and alive(__unit) then
-				if __unit:name() == __ammo_bag_unit_name then
-					managers.network:session():send_to_peers_synched("sync_ammo_bag_ammo_taken", __unit, (__unit:base()._ammo_amount or 1) * 2)
-					__unit:base()._ammo_amount = 0
-					__unit:base():_set_empty()
-					World:delete_unit(__unit)
-				elseif __unit:name() == __medic_bag_unit_name then
-					managers.network:session():send_to_peers_synched("sync_doctor_bag_taken", __unit, (__unit:base()._amount or 4) * 2)
-					__unit:base()._amount = 0
-					__unit:base():_set_empty()
-					World:delete_unit(__unit)
-				elseif __unit:name() == __grenade_crate_unit_name then
-					
-				end
-			end
-		end
-		self.__Wave_Rewards = {}
-	end
-end
-
-Hooks:PostHook(SkirmishManager, "on_start_assault", "F_"..Idstring("PostHook:SkirmishManager:on_start_assault:Holdout Wave Reward"):key(), function(self)
-	self:Remove_Holdout_Wave_Reward()
-end)
-
-Hooks:PostHook(SkirmishManager, "init", "F_"..Idstring("PostHook:SkirmishManager:init:Holdout Wave Reward"):key(), function(self)
-	self.__Wave_Rewards = self.__Wave_Rewards or {}
-end)
